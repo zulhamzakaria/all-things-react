743fab1ec71124ebf9a6640ceaeb4c5caa2a8210
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-11-09 12:14:27 +0800

    - CarDetails

--- car-gallery/components/index.tsx
+++ car-gallery/components/index.tsx
@@ -6,6 +6,8 @@ import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import { CarCard } from "./CarCard";
+import CarDetails from "./CarDetails";
+
 export {
   Hero,
   CustomButton,
@@ -15,4 +17,5 @@ export {
   CustomFilter,
   SearchManufacturer,
   CarCard,
+  CarDetails,
 };
