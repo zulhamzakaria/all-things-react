3ecbc564db5896f96fd46c1208e04f11b50e8711
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-11-15 18:27:37 +0800

    - added components

---
+++ nextjs-full-course/app/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+<h1 className='head_text text-center'>
+    Discover and Share.
+    <br className='max-md:hidden' />
+<span className='orange_gradient text-center'>AI powered prompts</span>
+</h1>
+<p className='desc text-center'>
+    Open-source AI prompting
+</p>
+    </section>
+  )
+}
+
+export default Home
