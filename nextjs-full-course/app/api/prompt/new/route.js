5fa383adfa2328ae52ac25fea19abf72cd8582cd
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-11-16 16:03:28 +0800

    - api backend - prompt model - create-prompt create action

---
+++ nextjs-full-course/app/api/prompt/new/route.js
@@ -0,0 +1,23 @@
+import { connectToDatabase } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const POST = async (req, res) => {
+  const { userId, prompt, tag } = await req.JSON();
+  try {
+    await connectToDatabase();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt), {
+      status: 201,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("failed to create a new prompt", {
+      status: "500",
+    });
+  }
+};
---
+++ nextjs-full-course/models/prompt.js
@@ -0,0 +1,19 @@
+import { Schema, model, models } from "mongoose";
+
+const PrompSchema = new Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is required"],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is required"],
+  },
+});
+
+const Prompt = models.Prompt || model("Prompt", PrompSchema);
+export default Prompt;
