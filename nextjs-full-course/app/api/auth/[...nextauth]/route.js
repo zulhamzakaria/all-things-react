5afa8d40337886e18e1544c1f628d9ef05efa72f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-11-16 13:23:54 +0800

    - login using google

--- nextjs-full-course/app/api/auth/[...nextauth]/route.js
+++ nextjs-full-course/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
+import { connectToDatabase } from "@utils/database";
+import User from "@models/user";
+
+const allowedEmail = process.env.ALLOWED_EMAIL;
 
 const handler = NextAuth({
     providers:[GoogleProvider({
@@ -7,13 +11,38 @@ const handler = NextAuth({
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })],
     async session({session}){
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
+        session.user.id = sessionUser._id.toString()
 
+        return session;
     },
     async signIn({profile}){
         try {
-            
+           await connectToDatabase();
+           // check if user exists
+           const userExists = await User.findOne({
+            email: profile.email
+           });
+
+        //    if(!userExists && !allowedEmail.includes(profile.email)){
+        //     return false;
+        //    }
+
+           // otherwise, create new user
+           if(!userExists){
+            await User.create({
+                email: profile.email,
+                username: profile.name.replace(" ","").toLowerCase(),
+                image: profile.picture
+            })
+           }
+
+           return true;
         } catch (error) {
-            
+            console.log(error);
+            return false;
         }
     },
 })
--- nextjs-full-course/components/Nav.jsx
+++ nextjs-full-course/components/Nav.jsx
@@ -5,14 +5,15 @@ import {useState, useEffect} from 'react'
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
   const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
   useEffect(()=>{
-    const setProviders = async () =>{
+    const setUpProviders = async () =>{
       const response = await getProviders();
       setProviders(response)
     }
+    setUpProviders()
   },[])
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
@@ -21,7 +22,7 @@ const Nav = () => {
         <p className='logo_text'>Promppass</p>
       </Link>
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className='black_btn'>
               Create
@@ -30,7 +31,7 @@ const Nav = () => {
               Sign Out
             </button>
             <Link href="/profile">
-              <Image src="/assets/images/logo.svg" width={37} height={37} alt="profile-photo" className='rounded-full'/>
+              <Image src={session?.user.image} width={37} height={37} alt="profile-photo" className='rounded-full'/>
             </Link>
           </div>
         ):<>
@@ -43,9 +44,9 @@ const Nav = () => {
         </>}
       </div>
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
-            <Image src="/assets/images/logo.svg" width={37} height={37} alt="profile-photo" className='rounded-full' 
+            <Image src={session?.user.image} width={37} height={37} alt="profile-photo" className='rounded-full' 
              onClick={() => setToggleDropdown((prev)=>!prev)}/>
             {toggleDropdown && (
               <div className='dropdown'>
