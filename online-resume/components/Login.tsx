662b174fece39fa23aa3a87a5bd7f5e27de686c1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-11-19 21:34:53 +0800

    - navbar login

---
+++ online-resume/components/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <form noValidate>
+
+    </form>
+  )
+}
+
+export default Login
--- online-resume/components/Nav.tsx
+++ online-resume/components/Nav.tsx
@@ -1,19 +1,29 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import Login from "./Login";
 
 const Nav = () => {
+  const [isLoginVisible, setIsLoginVisible] = useState(false);
+  const handleLoginClick = () => {
+    setIsLoginVisible(true);
+  };
   return (
     <nav className="flex-between navbar">
       <div className="flex-1 flex-start gap-10 ">
         <Link href="/">
           <Image
-            src="https://media.tenor.com/Jojpr9QgMLoAAAAd/maxwell-maxwell-spin.gif"
+            src="https://media.tenor.com/d13jPFfU24YAAAAi/maxwell-christmas.gif"
             alt="logo"
-            width={20}
+            width={30}
             height={20}
           />
         </Link>
-        <ul className="xl:flex hidden text-small gap-7"></ul>
+      </div>
+      <div className="flex-center gap-4">
+        <span onClick={handleLoginClick}>log in</span>
+        {isLoginVisible && <Login />}
       </div>
     </nav>
   );
