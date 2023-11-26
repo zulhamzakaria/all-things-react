7affa2ec21d6a2d92dad2d3ad6fbd0388b41b81c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-11-26 20:43:20 +0800

    - texteditor toolbar

--- online-resume/components/ToolBar.tsx
+++ online-resume/components/ToolBar.tsx
@@ -2,6 +2,7 @@
 
 import { Editor } from "@tiptap/react";
 import { Bold, List, Strikethrough, ListOrdered, Heading2 } from "lucide-react";
+import { Toggle } from "./ui/toggle";
 
 type Props = {
   editor: Editor | null;
@@ -11,7 +12,56 @@ const ToolBar = ({ editor }: Props) => {
   if (!editor) {
     return null;
   }
-  return <div>ToolBar</div>;
+  return (
+    <div className="border border-input bg-transparent">
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("heading")}
+        onPressedChange={() =>
+          editor.chain().focus().toggleHeading({ level: 2 }).run()
+        }
+      >
+        <Heading2 className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("bold")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleBold().run();
+        }}
+      >
+        <Bold className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("strike")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleStrike().run();
+        }}
+      >
+        <Strikethrough className="h-4 w-4" />
+      </Toggle>
+
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("bulletList")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleBulletList().run();
+        }}
+      >
+        <List className="h-4 w-4" />
+      </Toggle>
+      <Toggle
+        size="sm"
+        pressed={editor.isActive("orderedList")}
+        onPressedChange={() => {
+          editor.chain().focus().toggleOrderedList().run();
+        }}
+      >
+        <ListOrdered className="h-4 w-4" />
+      </Toggle>
+    </div>
+  );
 };
 
 export default ToolBar;
