2a39b678e715515eb90c288b39efcf9b742db540
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-11-29 19:40:15 +0800

    - summary.tsx SWR

--- online-resume/components/Summary.tsx
+++ online-resume/components/Summary.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import useSWR from "swr";
 
-const Summary = () => {
-  return <div>Summary</div>;
+const fetcher = async () => {
+  const response = await fetch(
+    "https://online-resume-with-minimal-api.azurewebsites.net/api/summaries"
+  );
+  if (!response.ok) {
+    console.log("Failed to get summary data");
+  }
+  return response.json();
+};
+
+const Summary: React.FC = () => {
+  const { data, error, isLoading } = useSWR(
+    "https://online-resume-with-minimal-api.azurewebsites.net/api/summaries",
+    fetcher
+  );
+  if (error) return <div>Failed to get data... (`${error.message}`)</div>;
+  if (isLoading) return <div>Loading...</div>;
+  return <div>{data?.description}</div>;
 };
 
 export default Summary;
