e2e93c4ecc279fd2586ebc86fa761d5303c975eb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-12-07 20:04:00 +0800

    - data render for experiences

--- online-resume/components/Experiences.tsx
+++ online-resume/components/Experiences.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
 const Experiences = () => {
-  const [experiences, setExperiences] = useState("");
+  const [experiences, setExperiences] = useState([]);
   const fetcher = (url: any) => fetch(url).then((res) => res.json());
   const { data, error, isLoading } = useSWR(
     "https://online-resume-with-minimal-api.azurewebsites.net/api/experiences",
@@ -11,12 +11,21 @@ const Experiences = () => {
   useEffect(() => {
     if (!isLoading && data) {
       setExperiences(data);
+      console.log(experiences);
     }
   }, [isLoading, data]);
   if (error) return <div>Failed to get data... (`${error.message}`)</div>;
   if (isLoading) return <div>loading data...</div>;
   return (
-    <div className="pl-5 pt-1 text-sm text-justify w-full">{experiences}</div>
+    <div className="pl-5 pt-1 text-sm text-justify w-full">
+      {experiences.map((experience) => (
+        <div key={experience["id"]}>
+          <p>{experience["companyname"]}</p>
+          <p>{experience["responsibility"]}</p>
+          <p>{experience["id"]}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
