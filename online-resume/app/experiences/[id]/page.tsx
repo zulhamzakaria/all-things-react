0e442002760df3d94ff190c984edfcfca56880d7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-12-13 12:48:16 +0800

    - simple form

--- online-resume/app/experiences/[id]/page.tsx
+++ online-resume/app/experiences/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ExperienceProps } from "@/utils/props";
+import { Paper, TextField } from "@mui/material";
 import { FC, useEffect, useState } from "react";
 import useSWR from "swr";
 
@@ -8,6 +9,9 @@ interface pageProps {
 }
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const UpdateExperience: FC<pageProps> = ({ params }) => {
+  const [companyName, setCompanyName] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [period, setPeriod] = useState("");
   const [experience, setExperience] = useState<ExperienceProps | null>(null);
   const { data, error, isLoading } = useSWR<ExperienceProps>(
     `https://online-resume-with-minimal-api.azurewebsites.net/api/experiences/${params.id}`,
@@ -27,8 +31,48 @@ const UpdateExperience: FC<pageProps> = ({ params }) => {
   }
   return (
     <div>
-      <div>id: {experience?.id}</div>
-      <div>Company Name: {experience?.companyName}</div>
+      <Paper elevation={0} className="mt-2">
+        {/* <div>id: {experience?.id}</div>
+        <div>Company Name: {experience?.companyName}</div> */}
+        <div className="text-end pt-10 bg-blue-900"></div>
+        <div className="text-center pb-6 font-bold text-3xl text-white bg-gradient-to-b from-blue-900 to-blue-950">
+          EXPERIENCE
+        </div>
+        <div className="mr-1 ml-1">
+          <div className="flex items-center flex-col">
+            <TextField
+              id="standard-basic"
+              label="Company Name"
+              variant="outlined"
+              className="mt-5"
+              fullWidth
+              required
+              value={companyName}
+              onChange={(e) => setCompanyName(e.target.value)}
+            />
+          </div>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <TextField
+              id="standard-basic"
+              label="Job Title"
+              variant="outlined"
+              className="mt-5 flex-grow"
+              required
+              value={jobTitle}
+              onChange={(e) => setJobTitle(e.target.value)}
+            />
+            <TextField
+              id="standard-basic"
+              label="Period"
+              variant="outlined"
+              className="mt-5 flex-grow mb-1"
+              required
+              value={period}
+              onChange={(e) => setPeriod(e.target.value)}
+            />
+          </div>
+        </div>
+      </Paper>
     </div>
   );
 };
