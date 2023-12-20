94acc99dfd173e7c3d3a35161d1e4b66add710e5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2023-12-20 17:36:12 +0800

    - slight mods

--- online-resume/components/Forms/LoginForm.tsx
+++ online-resume/components/Forms/LoginForm.tsx
@@ -13,14 +13,11 @@ const LoginForm: React.FC<CloseChildDialogProps> = ({ closeDialog }) => {
   const [password, setPassword] = useState("");
 
   const login = useAuthStore((state) => state.login);
-  const isAuthenticated = useAuthStore((state) => {
-    return state.isAuthenticated;
-  });
+  // const isAuthenticated = useAuthStore((state) => {
+  //   return state.isAuthenticated;
+  // });
   const handleLogin = (event: React.MouseEvent) => {
     login(username, password);
-    // if (isAuthenticated) {
-    //   closeDialog(event);
-    // }
   };
 
   return (
