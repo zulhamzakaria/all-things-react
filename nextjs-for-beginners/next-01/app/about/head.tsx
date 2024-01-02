ef6e8b882870c144bcadf494a03894f1b77f3c14
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-02 21:42:06 +0800

    - head.tsx for metadata

---
+++ nextjs-for-beginners/next-01/app/about/head.tsx
@@ -0,0 +1,12 @@
+function head() {
+  return (
+    <>
+      <title>About Page</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="bla bla bla" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
+
+export default head;
