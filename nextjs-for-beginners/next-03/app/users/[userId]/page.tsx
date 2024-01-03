bd35c4304c201afcaf20a20f0807ed9d641a6f9b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-03 16:12:48 +0800

    - dynamic metadata

--- nextjs-for-beginners/next-03/app/users/[userId]/page.tsx
+++ nextjs-for-beginners/next-03/app/users/[userId]/page.tsx
@@ -2,12 +2,24 @@ import getUser from "@/lib/getUser";
 import getUserPosts from "@/lib/getUserPosts";
 import { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
+import type { Metadata } from "next";
 
 type Params = {
   params: {
     userId: string;
   };
 };
+
+export async function generateMetadata({
+  params: { userId },
+}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId);
+  const user: User = await userData;
+  return {
+    title: user.name,
+    description: `This is the page of ${user.name}`,
+  };
+}
 
 export default async function UserPage({ params: { userId } }: Params) {
   const userData: Promise<User> = getUser(userId);
