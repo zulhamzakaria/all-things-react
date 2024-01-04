39e7707acfaef7db228249803707f8807734727f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-04 10:49:10 +0800

    - search.tsx

---
+++ nextjs-for-beginners/next-04/app/components/Search.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+function Search() {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSearch("");
+    // add search term behind the URL
+    router.push(`/${search}/`);
+  };
+
+  return (
+    <form
+      className="w-50 flex justify-center md:justify-between"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="bg-white p-2 w-80 text-xl rounded-xl"
+        placeholder="Search..."
+      />
+      <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
+        🚀
+      </button>
+    </form>
+  );
+}
+
+export default Search;
