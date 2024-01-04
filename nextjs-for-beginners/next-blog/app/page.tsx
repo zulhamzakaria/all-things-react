95ccba611cbe5ad15c28de19de9bb236e0ff0954
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-04 17:24:47 +0800

    - Posts.tsx component

--- nextjs-for-beginners/next-blog/app/page.tsx
+++ nextjs-for-beginners/next-blog/app/page.tsx
@@ -1,12 +1,15 @@
+import Posts from "./components/Posts";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-6 mx-auto">
+    <main className="flex flex-col items-center justify-between px-6 mx-auto">
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and welcome 👋&nbsp;
         <span className="whitespace-nowrap">
           I'm <span className="font-bold">Balmond</span>
         </span>
       </p>
+      <Posts />
     </main>
   );
 }
