df0b0bfc13380201b61589c4a6ba0dea1960923a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-04 10:27:07 +0800

    - stylings

--- nextjs-for-beginners/next-04/app/components/Navbar.tsx
+++ nextjs-for-beginners/next-04/app/components/Navbar.tsx
@@ -2,14 +2,10 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <>
-      <div>
-        <nav className="bg-slate-600 p-4 flex justyify-between flex-col md:flex-row top-0 drop-shadow-xl">
-          <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
-            <Link href="/">WikkiRockket</Link>
-          </h1>
-        </nav>
-      </div>
-    </>
+    <nav className="bg-slate-600 p-4 flex justyify-between flex-col md:flex-row top-0 drop-shadow-xl">
+      <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+        <Link href="/">WikkiRockket 🚀✨</Link>
+      </h1>
+    </nav>
   );
 }
