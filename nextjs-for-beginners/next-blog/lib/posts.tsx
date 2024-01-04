c88bb41c203960d8c42663a1bec416cf46b3cb08
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-04 20:14:12 +0800

    - npm i remark remark-html - getPostData(id:string)

--- nextjs-for-beginners/next-blog/lib/posts.tsx
+++ nextjs-for-beginners/next-blog/lib/posts.tsx
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "blogposts");
 
@@ -21,4 +23,23 @@ export function getSortedPostsData() {
   });
 
   return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+export async function getPostData(id: string) {
+  const fullpath = path.join(postsDirectory, `${id}.md`);
+  const filecontents = fs.readFileSync(fullpath, "utf8");
+  const matterResult = matter(filecontents);
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+
+  const contentHtml = processedContent.toString();
+  const blogPostWithHtml: BlogPost & { contentHtml: string } = {
+    id,
+    title: matterResult.data.title,
+    date: matterResult.data.date,
+    contentHtml,
+  };
+
+  return blogPostWithHtml;
 }
