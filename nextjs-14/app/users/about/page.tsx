6aa95658f9a7aed308380417737a6f84ea577b67
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-20 18:30:38 +0800

    - nested routes

---
+++ nextjs-14/app/users/about/page.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-l from-fuchsia-950 to-rose-950">
+      <p className="text-5xl">AboutUsers.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ nextjs-14/app/users/page.tsx
@@ -0,0 +1,9 @@
+const Users = () => {
+  return (
+    <div className="flex items-center justify-center pt-10 h-screen bg-gradient-to-br from-purple-950 to-blue-950">
+      <p className="text-5xl">Users.</p>
+    </div>
+  );
+};
+
+export default Users;
