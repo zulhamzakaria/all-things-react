0ca6ce2723855f6689151ad39ae9b49efb57bdde
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-20 18:14:05 +0800

    - routing

---
+++ nextjs-14/app/about/page.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-cyan-950">
+      <p className="text-8xl">About.</p>
+    </div>
+  );
+};
+
+export default About;
