bf5b729e54f9e77733b6ba31d4d33dac001b51ce
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-21 22:32:56 +0800

    - backend POST

--- nextjs-14/app/api/users/route.tsx
+++ nextjs-14/app/api/users/route.tsx
@@ -1,5 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 export function GET() {
   return NextResponse.json({ result: "hello" });
+}
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  let { email, password } = await req.body.json();
+
+  if (!email || !password) {
+    return NextResponse.json({ error: "Incomplete data" }, { status: 400 });
+  }
+
+  return NextResponse.json({ success: "record added" });
 }
