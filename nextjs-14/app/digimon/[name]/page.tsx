7862e972b35562e29dc586b098292fb6045384db
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-21 12:35:57 +0800

    -redirect

---
+++ nextjs-14/app/digimon/[name]/page.tsx
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+
+interface PageProps {
+  params: { name: string };
+}
+
+//USE useRouter() HOOK INSTEAD for TS
+
+const Page = ({ params }: PageProps) => {
+  return (
+    <div className="h-screen flex items-center justify-center text-5xl bg-gradient-to-b from-yellow-500 to-orange-500">
+      <p className="border bg-gradient-to-tr from-cyan-500 to-blue-950 py-2 px-5">
+        {params.name}
+      </p>
+    </div>
+  );
+};
+
+export default Page;
