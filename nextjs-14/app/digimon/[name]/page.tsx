87745cd750ba6513dfc9eac491eac7f7ed0c2028
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-21 12:38:24 +0800

    - it works?

--- nextjs-14/app/digimon/[name]/page.tsx
+++ nextjs-14/app/digimon/[name]/page.tsx
@@ -4,9 +4,11 @@ interface PageProps {
   params: { name: string };
 }
 
-//USE useRouter() HOOK INSTEAD for TS
-
 const Page = ({ params }: PageProps) => {
+  if (params.name === "pikachu") {
+    redirect("/");
+  }
+
   return (
     <div className="h-screen flex items-center justify-center text-5xl bg-gradient-to-b from-yellow-500 to-orange-500">
       <p className="border bg-gradient-to-tr from-cyan-500 to-blue-950 py-2 px-5">
