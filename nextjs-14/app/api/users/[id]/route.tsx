e5f82afd4bd6d3eb4c57d158e0cc7a76319fd640
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-22 14:21:46 +0800

    - move to nextjs TS tut

--- nextjs-14/app/api/users/[id]/route.tsx
+++ nextjs-14/app/api/users/[id]/route.tsx
@@ -7,3 +7,13 @@ export async function GET(_: any, res: any) {
   const user = users.filter((u) => u.id === id);
   return NextResponse.json({ user });
 }
+
+// login
+export async function POST(req: any, res: any) {
+  let { name, age } = await req.json();
+  const { id } = await res.params;
+
+  const user = users.find((u) => u.id === id);
+
+//   const { name: Name = "Unknown", age: Age = "0" } = user;
+}
