9a0feaa4788480b47a847018c7187afd3b1fb8c8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-24 13:58:48 +0800

    - footer

--- fullstack-n3xtjs/components/Footer.tsx
+++ fullstack-n3xtjs/components/Footer.tsx
@@ -1,5 +1,83 @@
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+};
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const FooterColumn = ({ title, links }: ColumnProps) => (
+    <div className="footer_column">
+      <h4 className="font-semibold">{title}</h4>
+      <ul className="flex flex-col gap-2 font-normal">
+        {links.map((l) => (
+          <Link href="/" key={l}>
+            {l}
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="./logo-purple.svg"
+            alt="footer-logo"
+            width={115}
+            height={38}
+          />
+          <p className="font-normal mt-5 max-w-xs text-start text-sm">
+            To showcase the projects
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[1].title}
+              links={footerLinks[1].links}
+            />
+            <FooterColumn
+              title={footerLinks[2].title}
+              links={footerLinks[2].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[3].title}
+            links={footerLinks[3].links}
+          />
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[4].title}
+              links={footerLinks[4].links}
+            />
+            <FooterColumn
+              title={footerLinks[5].title}
+              links={footerLinks[5].links}
+            />
+          </div>
+          <FooterColumn
+            title={footerLinks[6].title}
+            links={footerLinks[6].links}
+          />
+        </div>
+      </div>
+      <div className="flexBetween footer_copyright">
+        <p>@2024 galeria. All rights reserved.</p>
+        <p className="text-gray">
+          <span className="text-black font-semibold">9999+</span> projects
+          submitted
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
