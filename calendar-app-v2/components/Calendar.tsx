e90ce4992f4637e6e8ebe53604d9b324dd430598
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-02-22 12:10:00 +0800

    - installed dayjs

--- calendar-app-v2/components/Calendar.tsx
+++ calendar-app-v2/components/Calendar.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+"use client";
+import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 const Calendar = () => {
   return (
     <div>
       <h1>Calendar Commponent</h1>
-      <div></div>
+      <div>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DateCalendar />
+        </LocalizationProvider>
+      </div>
     </div>
   );
 };
