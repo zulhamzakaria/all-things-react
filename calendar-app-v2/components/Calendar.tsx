dedced2a1cf8c85bb899e7d101895a160afee7eb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-02-22 12:00:59 +0800

    - installed MUI date time picker

--- calendar-app-v2/components/Calendar.tsx
+++ calendar-app-v2/components/Calendar.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div>
+      <h1>Calendar Commponent</h1>
+      <div></div>
+    </div>
+  );
 };
 
 export default Calendar;
