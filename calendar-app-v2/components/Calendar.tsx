6bc85b886fd52a2c386d74356cff5d1e55577612
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-02-23 19:00:57 +0800

    tomfoolery

--- calendar-app-v2/components/Calendar.tsx
+++ calendar-app-v2/components/Calendar.tsx
@@ -2,17 +2,24 @@
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { DatePicker } from "@mui/x-date-pickers";
 
 const Calendar = () => {
   return (
     <div>
       <div>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar
+          {/* <DateCalendar
             className="bg-white text-red-900"
-            views={["day", "month"]}
-            openTo="month"
-            monthsPerRow={3}
+            views={["day"]}
+            fixedWeekNumber={10}
+          /> */}
+          <DatePicker
+            className="text-white"
+            views={["day"]}
+            fixedWeekNumber={10}
+            showDaysOutsideCurrentMonth
+            sx={{ width: 600, height: 400 }}
           />
         </LocalizationProvider>
       </div>
