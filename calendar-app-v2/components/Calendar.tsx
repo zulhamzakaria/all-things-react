99285c6a894cbdc7fce85e063301e00258571f4d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-02-26 19:13:58 +0800

    - foolin around

--- calendar-app-v2/components/Calendar.tsx
+++ calendar-app-v2/components/Calendar.tsx
@@ -2,10 +2,20 @@
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { useState } from "react";
 
 const Calendar = () => {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+  };
   return (
-    <div>
+    <div className="flex flex-col">
+      <div>
+        {selectedDate && (
+          <p>Selected Date: {selectedDate.toLocaleDateString()}</p>
+        )}
+      </div>
       <div>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DateCalendar
