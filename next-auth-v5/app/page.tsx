785b4da58067e758981196ffbc7869dba04e47f7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-03-29 19:59:49 +0800

    - shadcn custom

--- next-auth-v5/app/page.tsx
+++ next-auth-v5/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p className="font-semibold">Hello Auth!</p>;
+  return <Button variant={"custom"}>Click Me!</Button>;
 }
