4ac2ec9a37e6c829062d8eb6f1f1f448a7ee5458
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-03-31 17:39:07 +0800

    - globals.css

--- next-auth-v5/app/page.tsx
+++ next-auth-v5/app/page.tsx
@@ -2,8 +2,15 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant={"custom"} size={"lg"}>
-      Click Me!
-    </Button>
+    <main
+      className="flex h-full flex-col items-center justify-center 
+    bg-[radial-gradient(ellipse_at_top, _var(--tw-gradient-stops))] from-sky-400 to-blue-800"
+    >
+      <div className="space-y-6">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          Auth
+        </h1>
+      </div>
+    </main>
   );
 }
