de1a39b7b589024b55b1e9e41bb9c4314682462b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-02 12:22:34 +0800

    - imports

--- next-auth-v5/components/auth/login-form.tsx
+++ next-auth-v5/components/auth/login-form.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
 import CardWrapper from "./card-wrapper";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <CardWrapper
       headerLabel="Welcome Back!"
