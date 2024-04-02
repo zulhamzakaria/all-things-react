66b3fca2fccea58921b23f3ad7fbb806d2be2982
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-02 12:37:22 +0800

    - form message

--- next-auth-v5/schemas/index.ts
+++ next-auth-v5/schemas/index.ts
@@ -1,7 +1,8 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string()
+  email: z.string().email({
+    message: "Email is required.",
+  }),
+  password: z.string(),
 });
-
