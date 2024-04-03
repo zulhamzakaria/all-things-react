a26e672d733afd999cb657406d0aa381b138f25c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-03 14:47:04 +0800

    - server side fields validation

--- next-auth-v5/actions/login.ts
+++ next-auth-v5/actions/login.ts
@@ -1,5 +1,12 @@
 "use server";
 
-export const login = (values: any) => {
-  console.log(values);
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+
+export const login = (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid fields." };
+  }
+  return { success: "Email sent!" };
 };
