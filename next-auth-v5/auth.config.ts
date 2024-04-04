4d92b0863ba7c317122cb9ccf2d3ce526c2918f6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-04 20:00:54 +0800

    - config for logged in user

--- next-auth-v5/auth.config.ts
+++ next-auth-v5/auth.config.ts
@@ -1,6 +1,25 @@
-import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+import bcrypt from "bcrypt";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) {
+            return null;
+          }
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (passwordMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
