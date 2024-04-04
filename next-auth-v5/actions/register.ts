c40c951f9d495b141c268a9eeeccf8caa22c51ed
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-04 14:18:23 +0800

    - slight mod

--- next-auth-v5/actions/register.ts
+++ next-auth-v5/actions/register.ts
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 import * as z from "zod";
 import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -14,11 +15,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const { email, password, name } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findFirst({
-    where: {
-      email,
-    },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
     return { error: "Email already taken." };
