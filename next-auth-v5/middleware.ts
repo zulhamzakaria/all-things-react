d0b31050a9c7b136f7c09e7fccaab459ffb3eed4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-04 17:13:26 +0800

    - middleware matcher

--- next-auth-v5/middleware.ts
+++ next-auth-v5/middleware.ts
@@ -3,5 +3,5 @@ import { auth } from "@/auth";
 export default auth((req) => {});
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
