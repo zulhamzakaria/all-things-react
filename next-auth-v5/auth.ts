f59c6211abdd7e7bcb489deae70a7dfe63fbb589
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-04 20:13:38 +0800

    - auth.ts

--- next-auth-v5/auth.ts
+++ next-auth-v5/auth.ts
@@ -6,6 +6,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut,
 } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
