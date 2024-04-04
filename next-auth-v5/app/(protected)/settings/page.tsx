0ae337730ce3c667a12f418c82cc149e059a9a61
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-04 19:14:13 +0800

    - slight change

--- next-auth-v5/app/(protected)/settings/page.tsx
+++ next-auth-v5/app/(protected)/settings/page.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { auth } from "@/auth";
 
-const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+const SettingsPage = async () => {
+  const session = await auth();
+  return <div>{JSON.stringify(session)}</div>;
 };
 
 export default SettingsPage;
