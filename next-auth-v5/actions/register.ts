7116c0f6b20f60d8529451b40a280aef6680c687
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-04 14:07:59 +0800

    - register action

--- next-auth-v5/actions/register.ts
+++ next-auth-v5/actions/register.ts
@@ -1,12 +1,37 @@
 "use server";
 
+import { db } from "@/lib/db";
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { error } from "console";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid fields." };
   }
-  return { success: "Email sent!" };
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already taken." };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "Account created!" };
 };
