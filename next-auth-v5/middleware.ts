98a894c90896ea168d112fbbf84ddc55e55772b1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-04 18:55:33 +0800

    - middleware

--- next-auth-v5/middleware.ts
+++ next-auth-v5/middleware.ts
@@ -1,6 +1,8 @@
 import { auth } from "@/auth";
 
-export default auth((req) => {});
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
