f6866224dbf2acb39d0ebb72158c954e6a6926dc
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-05 09:27:00 +0800

    - mod

--- next-auth-v5/middleware.ts
+++ next-auth-v5/middleware.ts
@@ -17,21 +17,21 @@ export default auth((req) => {
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
-    return;
+    return undefined;
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    return;
+    return undefined;
   }
 
   if (!isLoggedIn && !isPublicRoute) {
     // return Response.redirect(new URL("/auth/login", nextUrl));
   }
 
-  return;
+  return undefined;
 });
 
 export const config = {
