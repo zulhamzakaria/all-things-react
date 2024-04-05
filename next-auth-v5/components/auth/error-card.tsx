08dabed380c8c2e1a38886a0c10d33c54d7cfb1f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-05 16:21:48 +0800

    - error card

--- next-auth-v5/components/auth/error-card.tsx
+++ next-auth-v5/components/auth/error-card.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
+import Header from "@/components/auth/header";
+import BackButton from "./back-button";
+import { CardHeader, Card, CardFooter } from "@/components/ui/card";
 const ErrorCard = () => {
-  return <div>Error Card</div>;
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label="Oops, something went wrong!" />
+      </CardHeader>
+      <CardFooter>
+        <BackButton label="Back to login" href="/auth/login" />
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default ErrorCard;
