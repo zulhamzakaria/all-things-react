ca17abd86a919d4c66c1c9cdf18ea4296751d305
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-05 09:31:44 +0800

    - aaaahhhhhhh

--- next-auth-v5/auth.ts
+++ next-auth-v5/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { getUserById } from "./data/user";
 
 export const {
   handlers: { GET, POST },
@@ -17,6 +18,17 @@ export const {
       return session;
     },
     async jwt({ token }) {
+      //logged out
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     },
   },
--- next-auth-v5/middleware.ts
+++ next-auth-v5/middleware.ts
@@ -17,21 +17,21 @@ export default auth((req) => {
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
-    return undefined;
+    return;
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    return undefined;
+    return;
   }
 
   if (!isLoggedIn && !isPublicRoute) {
     // return Response.redirect(new URL("/auth/login", nextUrl));
   }
 
-  return undefined;
+  return;
 });
 
 export const config = {
