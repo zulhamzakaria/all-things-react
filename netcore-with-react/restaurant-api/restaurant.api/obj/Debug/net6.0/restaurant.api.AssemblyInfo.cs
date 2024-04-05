b775b01d850de083a43d8e26d152252b7290fe8a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-05 10:04:02 +0800

    - verifiedEmail condition

--- netcore-with-react/restaurant-api/restaurant.api/obj/Debug/net6.0/restaurant.api.AssemblyInfo.cs
+++ netcore-with-react/restaurant-api/restaurant.api/obj/Debug/net6.0/restaurant.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("restaurant.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d7c85a5d3b3363a9a84accaa82d7aa7313cd2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+594f579faf9a607c7ee08b82bdbed4966ce92ca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("restaurant.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("restaurant.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
