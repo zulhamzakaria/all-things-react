a09d1e0f94dc133a77b9eecb3808415249d5b2f0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-05 16:37:26 +0800

    - slight mod

--- next-auth-v5/components/auth/login-form.tsx
+++ next-auth-v5/components/auth/login-form.tsx
@@ -42,8 +42,8 @@ export const LoginForm = () => {
     setSuccess("");
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        // setSuccess(data?.success);
       });
     });
   };
