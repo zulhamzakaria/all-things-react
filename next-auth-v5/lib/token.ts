e9fd26837035818150d899bf6e9c1442350762a7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-17 21:33:14 +0800

    - generateVerificationToken

--- next-auth-v5/lib/token.ts
+++ next-auth-v5/lib/token.ts
@@ -5,13 +5,23 @@ import { v4 as uuid } from "uuid";
 export const generateVerificationToken = async (email: string) => {
   const token = uuid();
   // get 1 hour
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const expires_at = new Date(new Date().getTime() + 3600 * 1000);
   const existingToken = await getVerificationTokenByEmail(email);
   if (existingToken) {
     await db.verificationToken.delete({
       where: {
-        id: existingToken,
+        id: existingToken.id,
       },
     });
   }
+
+  const verificationToken = await db.verificationToken.create({
+    data: {
+      email,
+      token,
+      expires_at,
+    },
+  });
+
+  return verificationToken;
 };
