9004f276744de7c4857f61721916a405a02e2391
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-17 21:18:30 +0800

    -verification-token endpoints?

---
+++ next-auth-v5/data/verification-token.ts
@@ -0,0 +1,24 @@
+import { db } from "@/lib/db";
+
+export const getVerificationTokenByEmail = async (email: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findFirst({
+      where: {
+        email,
+      },
+    });
+  } catch {
+    return null;
+  }
+};
+export const getVerificationTokenByToken = async (token: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findUnique({
+      where: {
+        token,
+      },
+    });
+  } catch {
+    return null;
+  }
+};
