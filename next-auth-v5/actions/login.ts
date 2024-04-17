c048c2226f674cca085a096577294f9292b39b7a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-17 21:46:59 +0800

    - condition for blocking the login if the email hasnt been verified

--- next-auth-v5/actions/login.ts
+++ next-auth-v5/actions/login.ts
@@ -5,6 +5,8 @@ import { LoginSchema } from "@/schemas";
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/route";
 import { AuthError } from "next-auth";
+import { generateVerificationToken } from "@/lib/token";
+import { getUserByEmail } from "@/data/user";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -12,6 +14,20 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid fields." };
   }
   const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: "Invalid credentials!" };
+  }
+
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email
+    );
+    return { success: "Confirmation email sent" };
+  }
+
   try {
     await signIn("credentials", {
       email,
