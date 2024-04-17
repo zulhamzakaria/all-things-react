945d607634d9c27520458af8509d3f09f92dd412
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-17 22:12:38 +0800

    - email verification implementation for register.ts

--- next-auth-v5/actions/register.ts
+++ next-auth-v5/actions/register.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/token";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -31,6 +32,8 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   });
 
   const verificationToken = await generateVerificationToken(email);
+
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
   return { success: "Confirmation email sent!" };
 };
