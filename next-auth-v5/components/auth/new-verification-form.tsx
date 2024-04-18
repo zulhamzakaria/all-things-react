618f28fa19627a64c63b0fc9a93ed5cdd6894ad9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-18 15:39:34 +0800

    - implement newVerification action to new-verification-form

--- next-auth-v5/components/auth/new-verification-form.tsx
+++ next-auth-v5/components/auth/new-verification-form.tsx
@@ -3,15 +3,30 @@
 import CardWrapper from "./card-wrapper";
 import { BeatLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { newVerification } from "@/actions/new-verification";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
 
 const NewVerificationForm = () => {
   const searchParams = useSearchParams();
-
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
   const token = searchParams.get("tokens");
 
   const onSubmit = useCallback(() => {
-    console.log(token);
+    if (!token) {
+      setError("Missing token");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong");
+      });
   }, [token]);
 
   useEffect(() => {
@@ -26,6 +41,8 @@ const NewVerificationForm = () => {
     >
       <div className="flex items-center w-full justify-center">
         <BeatLoader />
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
