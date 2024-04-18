ea224492fc2f02a95bc3674b11b721ccf379ef5d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-18 15:18:19 +0800

    - verification form

--- next-auth-v5/components/auth/new-verification-form.tsx
+++ next-auth-v5/components/auth/new-verification-form.tsx
@@ -1,13 +1,32 @@
+"use client";
+
 import CardWrapper from "./card-wrapper";
-import {BeatLoader} from "react-spinners"
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect } from "react";
+
 const NewVerificationForm = () => {
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get("tokens");
+
+  const onSubmit = useCallback(() => {
+    console.log(token);
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
   return (
     <CardWrapper
       headerLabel="Confirming your verification"
       backButtonHref="/auth/login"
       backButtonLabel="Back to login"
     >
-      <div className="flex items-center w-full justify-center"></div>
+      <div className="flex items-center w-full justify-center">
+        <BeatLoader />
+      </div>
     </CardWrapper>
   );
 };
