a1325f7b4f990cd6ea6e75bc931b84a3c7c6a578
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-18 21:38:48 +0800

    - reset server action

---
+++ next-auth-v5/actions/reset.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { ResetSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import * as z from "zod";
+
+export const reset = async (values: z.infer<typeof ResetSchema>) => {
+  const validatedFields = ResetSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid email" };
+  }
+
+  const { email } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser) {
+    return { error: "Email not found" };
+  }
+  
+  return { success: "Reset email sent" };
+};
