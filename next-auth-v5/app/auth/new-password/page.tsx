ffbe4b6642f6feb9a407a191358266e06abcbffe
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-19 14:09:38 +0800

    - new-password page + new-password-form

--- next-auth-v5/app/auth/new-password/page.tsx
+++ next-auth-v5/app/auth/new-password/page.tsx
@@ -1,7 +1,8 @@
+import { NewPasswordForm } from "@/components/auth/new-password-form";
 import React from "react";
 
 const NewPasswordPage = () => {
-  return <div>New Password Page</div>;
+  return <NewPasswordForm />;
 };
 
 export default NewPasswordPage;
