46b1305113ca01f1113dc8cffcd2293b8ccfd58c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-20 22:18:41 +0800

    - user-button

--- next-auth-v5/components/auth/user-button.tsx
+++ next-auth-v5/components/auth/user-button.tsx
@@ -5,9 +5,31 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
-
+import { FaUser } from "react-icons/fa";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { LogoutButton } from "./logout-button";
+import { ExitIcon } from "@radix-ui/react-icons";
 
 export const UserButton = () => {
-  return <div>User Button</div>;
+  const user = useCurrentUser();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user?.image || ""} />
+          <AvatarFallback className="bg-green-900">
+            <FaUser className="text-white" />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="end">
+        <LogoutButton>
+          <DropdownMenuItem>
+            <ExitIcon className="h-4 w-4 mr-2"/>
+            Logout</DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
