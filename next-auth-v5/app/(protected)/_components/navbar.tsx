6ba5710cce6b03c9ae30fb0f57155bf7a4ae8c50
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-20 21:51:26 +0800

    - (protected) navbar stylingzzz

--- next-auth-v5/app/(protected)/_components/navbar.tsx
+++ next-auth-v5/app/(protected)/_components/navbar.tsx
@@ -1,5 +1,38 @@
 "use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
 export const Navbar = () => {
-  return <div>Navbar component</div>;
+  const pathName = usePathname();
+
+  return (
+    <div className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm">
+      <div className="flex gap-x-2">
+        <Button
+          asChild
+          variant={pathName === "/server" ? "default" : "outline"}
+        >
+          <Link href={"/server"}>Server</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathName === "/Client" ? "default" : "outline"}
+        >
+          <Link href={"/client"}>client</Link>
+        </Button>
+        <Button asChild variant={pathName === "/admin" ? "default" : "outline"}>
+          <Link href={"/admin"}>Admin</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathName === "/settings" ? "default" : "outline"}
+        >
+          <Link href={"/settings"}>Settings</Link>
+        </Button>
+      </div>
+      <p>Usr button</p>
+    </div>
+  );
 };
