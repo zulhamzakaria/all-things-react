2a80f16ce47fe0771d4a449c64d5866f67c0c010
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-20 13:44:03 +0800

    - login-form slight mod

--- next-auth-v5/components/auth/login-form.tsx
+++ next-auth-v5/components/auth/login-form.tsx
@@ -143,7 +143,7 @@ export const LoginForm = () => {
           <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button disabled={isPending} type="submit" className="w-full">
-            Login
+            {showTwoFactor ? "Confirm" : "Login"}
           </Button>
         </form>
       </Form>
