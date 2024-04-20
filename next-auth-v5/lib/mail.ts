edb6b643c85f2b35e242f02bf480ff6a79b41800
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-20 13:10:40 +0800

    - mail sending capability for 2FA token

--- next-auth-v5/lib/mail.ts
+++ next-auth-v5/lib/mail.ts
@@ -23,3 +23,12 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
     html: `<p>Click <a href "${resetLink}">here</a> to reset ur password.</p>`,
   });
 };
+
+export const sentTwoFactorTokenEmail = async (email: string, token: string) => {
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "2FA Code",
+    html: `<p>Your 2FA Code: ${token}</p>`,
+  });
+};
