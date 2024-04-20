378bcdad9d1ffeeb92c75fec5901a3f534c186da
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-20 13:29:20 +0800

    - add 2FA checker to the Login schema

--- next-auth-v5/schemas/index.ts
+++ next-auth-v5/schemas/index.ts
@@ -7,7 +7,9 @@ export const LoginSchema = z.object({
   password: z.string().min(1, {
     message: "Password is required.",
   }),
+  code:z.optional(z.string())
 });
+
 export const ResetSchema = z.object({
   email: z.string().email({
     message: "Email is required.",
