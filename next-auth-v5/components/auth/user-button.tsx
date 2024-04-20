ca9600eba04e5e6dc907c3767858fd7c362378ba
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-20 22:09:51 +0800

    - user-button component

---
+++ next-auth-v5/components/auth/user-button.tsx
@@ -0,0 +1,13 @@
+"use client";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
+
+export const UserButton = () => {
+  return <div>User Button</div>;
+};
