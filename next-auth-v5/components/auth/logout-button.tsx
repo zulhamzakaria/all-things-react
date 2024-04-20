448ffede4f1e3979ec580c94d2d05875b7234717
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-20 22:00:46 +0800

    - logout-button component - logout server action

---
+++ next-auth-v5/components/auth/logout-button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { logout } from "@/actions/logout";
+
+interface LogoutButtonProps {
+  children?: React.ReactNode;
+}
+
+export const LogoutButton = ({ children }: LogoutButtonProps) => {
+  const onClick = () => {
+    logout();
+  };
+
+  return (
+    <span onClick={onClick} className="curson-pointer">
+      {children}
+    </span>
+  );
+};
