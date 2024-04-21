0f1b8f395d88fe6ee365e65793321f8fc83dabb0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-21 10:41:58 +0800

    - next-auth module

--- next-auth-v5/components/user-info.tsx
+++ next-auth-v5/components/user-info.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+// agnostic component ; depends on whether the parent is a client or server page
+import ExtendedUser from "@/next-auth"
 
+interface UserInfoProps{
+    user?: ExtendedUser
+}
 const UserInfoPage = () => {
   return <div>User Info Page</div>;
 };
