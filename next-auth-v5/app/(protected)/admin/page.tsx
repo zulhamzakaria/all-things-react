cbe377abf7400057d8bf9500202a20980e60fc8b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-21 18:12:37 +0800

    - AdminPage stylings

--- next-auth-v5/app/(protected)/admin/page.tsx
+++ next-auth-v5/app/(protected)/admin/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
+import { RoleGate } from "@/components/auth/role-gate";
+import { FormSuccess } from "@/components/form-success";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { useCurrentRole } from "@/hooks/use-current-role";
+import { UserRole } from "@prisma/client";
 import React from "react";
 
 const AdminPage = () => {
-  const role = useCurrentRole();
   return (
     <Card className="w-[600px]">
       <CardHeader>
         <p className="text-2xl font-semibold text-center">🧑‍💼Admin</p>
       </CardHeader>
       <CardContent className="space-y-4">
-        
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message="Welcome, Admin" />
+        </RoleGate>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium ">Admin-only API route.</p>
+          <Button>Click to test.</Button>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium ">Admin-only Server Action.</p>
+          <Button>Click to test.</Button>
+        </div>
       </CardContent>
     </Card>
   );
