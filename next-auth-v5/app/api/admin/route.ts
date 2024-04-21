f1fd0e435dd83c0b6b292a0bc96e14a7f83123f7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-21 18:20:55 +0800

    - role check for Admin route API

--- next-auth-v5/app/api/admin/route.ts
+++ next-auth-v5/app/api/admin/route.ts
@@ -1,5 +1,11 @@
+import { currentUserRole } from "@/lib/auth";
+import { UserRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const role = await currentUserRole();
+  if (role === UserRole.ADMIN) {
+    return new NextResponse(null, { status: 200 });
+  }
   return new NextResponse(null, { status: 403 });
 }
