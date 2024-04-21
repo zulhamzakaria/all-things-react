fa82e9b9360da590d2b1166fc668e84b29bc08ad
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-21 17:54:16 +0800

    - lib for getting userRole ( usable on a server-side page)

--- next-auth-v5/lib/auth.ts
+++ next-auth-v5/lib/auth.ts
@@ -4,3 +4,7 @@ export const currentUser = async () => {
   const session = await auth();
   return session?.user;
 };
+export const currentUserRole = async () => {
+  const session = await auth();
+  return session?.user?.role;
+};
