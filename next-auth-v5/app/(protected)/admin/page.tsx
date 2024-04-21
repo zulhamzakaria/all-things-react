bd52823f05366e2c6baa73c6a99bbb29160e5789
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-21 17:59:52 +0800

    - AdminPage stylingzzz

--- next-auth-v5/app/(protected)/admin/page.tsx
+++ next-auth-v5/app/(protected)/admin/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useCurrentRole } from "@/hooks/use-current-role";
 import React from "react";
 
 const AdminPage = () => {
   const role = useCurrentRole();
-  return <div>{role}</div>;
+  return (
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">🧑‍💼Admin</p>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
