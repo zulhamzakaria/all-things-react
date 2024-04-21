83f4525c1e1b04a7dae0bea7ab7931895f5a8947
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-21 10:45:21 +0800

    - apply next-auth module to userinfopage (for getting user + role)

--- next-auth-v5/components/user-info.tsx
+++ next-auth-v5/components/user-info.tsx
@@ -1,11 +1,19 @@
 // agnostic component ; depends on whether the parent is a client or server page
-import ExtendedUser from "@/next-auth"
+import { ExtendedUser } from "@/next-auth";
+import { Card, CardHeader } from "./ui/card";
 
-interface UserInfoProps{
-    user?: ExtendedUser
+interface UserInfoProps {
+  user?: ExtendedUser;
+  label: string;
 }
-const UserInfoPage = () => {
-  return <div>User Info Page</div>;
+const UserInfoPage = ({ user, label }: UserInfoProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">{label}</p>
+      </CardHeader>
+    </Card>
+  );
 };
 
 export default UserInfoPage;
