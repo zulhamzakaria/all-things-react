fe5870c102f07cf79d922855529a58a9fb0fcfa0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-23 14:06:21 +0800

    - fleshin out settingsPage

--- next-auth-v5/app/(protected)/settings/page.tsx
+++ next-auth-v5/app/(protected)/settings/page.tsx
@@ -1,22 +1,53 @@
 "use client";
 
-import { useTransition } from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { SettingsSchema } from "@/schemas";
 import { useSession } from "next-auth/react";
 import { settings } from "@/actions/settings";
+import { Input } from "@/components/ui/input";
+import { useTransition, useState } from "react";
 import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import {
+  Form,
+  FormItem,
+  FormLabel,
+  FormField,
+  FormControl,
+  FormMessage,
+  FormDescription,
+} from "@/components/ui/form";
 
 const SettingsPage = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
   const { update } = useSession();
   const [isPending, startTransition] = useTransition();
 
-  const onClick = () => {
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      name: "defaultName",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
     startTransition(() => {
-      settings({
-        name: "New name",
-      }).then(() => {
-        update();
-      });
+      settings(values)
+        .then((data) => {
+          if (data.error) {
+            setError(data.error);
+          }
+          if (data.success) {
+            update();
+            setSuccess(data.success);
+          }
+        })
+        .catch(() => {
+          setError("somn wrong");
+        });
     });
   };
   return (
@@ -25,9 +56,29 @@ const SettingsPage = () => {
         <p className="text-2xl font-semibold text-center">⚙️Settings</p>
       </CardHeader>
       <CardContent>
-        <Button disabled={isPending} onClick={onClick}>
-          Update name
-        </Button>
+        <Form {...form}>
+          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="John Doe"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button type="submit">Save</Button>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
