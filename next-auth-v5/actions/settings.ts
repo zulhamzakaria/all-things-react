a90d0b70de3cbf4c49bc0b2fd25284de91797a7e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-23 13:18:26 +0800

    - setttings server action

---
+++ next-auth-v5/actions/settings.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import * as z from "zod";
+import { db } from "@/lib/db";
+import { currentUser } from "@/lib/auth";
+import { getUserById } from "@/data/user";
+import { SettingsSchema } from "@/schemas";
+import { FormSuccess } from "@/components/form-success";
+
+export const settings = async (values: z.infer<typeof SettingsSchema>) => {
+  const user = await currentUser();
+  if (!user) {
+    return { error: "Unauthorized" };
+  }
+  const dbUser = await getUserById(user.id);
+
+  if (dbUser === null) {
+    return { error: "User not found" };
+  }
+
+  if (!user) {
+    return { error: "Unauthorized" };
+  }
+
+  await db.user.update({
+    where: { id: dbUser.id },
+    data: {
+      ...values,
+    },
+  });
+
+  return { success: "Settings updated." };
+};
