fb76678767ef4437dd4b407bf066bf808d1a490c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-23 16:28:22 +0800

    - condition for putting new emaisl thats already in use - generate new token for new email - send newly generated token - compare entered password with the one inside db - hash new password - assigned hashedPassword to the password and set undefined to newPassword

--- next-auth-v5/actions/settings.ts
+++ next-auth-v5/actions/settings.ts
@@ -2,9 +2,12 @@
 
 import * as z from "zod";
 import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
 import { currentUser } from "@/lib/auth";
-import { getUserById } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 import { SettingsSchema } from "@/schemas";
+import { generateVerificationToken } from "@/lib/token";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
@@ -27,6 +30,35 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     values.password = undefined;
     values.newPassword = undefined;
     values.isTwoFactorEnabled = undefined;
+  }
+
+  if (values.email && values.email !== user.email) {
+    const existingUser = await getUserByEmail(values.email);
+    if (existingUser && existingUser.id !== user.id) {
+      return { error: "Email already in used" };
+    }
+
+    const verificationToken = await generateVerificationToken(values.email);
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+
+    return { success: "Verification email sent" };
+  }
+
+  if (values.password && values.newPassword && dbUser.password) {
+    const passwordMatch = await bcrypt.compare(
+      values.password,
+      dbUser.password
+    );
+
+    if (!passwordMatch) {
+      return { error: "Incorrect password" };
+    }
+    const hashedPassword = await bcrypt.hash(values.newPassword, 10);
+    values.password = hashedPassword;
+    values.newPassword = undefined;
   }
 
   await db.user.update({
