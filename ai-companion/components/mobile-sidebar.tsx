3aa304d972709321d169e4fc0efa3bbad6abd6e8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-24 17:48:58 +0800

    - mobile-sidebar component stylings

--- ai-companion/components/mobile-sidebar.tsx
+++ ai-companion/components/mobile-sidebar.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Menu } from "lucide-react";
+import Sidebar from "./sidebar";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
 const MobileSidebar = () => {
-  return <div>mobile-sidebar</div>;
+  return (
+    <div>
+      <Sheet>
+        <SheetTrigger className="md:hidden pr-4">
+          <Menu />
+        </SheetTrigger>
+        <SheetContent side={"left"} className="p-0 bg-secondary pt-10 w-32">
+          <Sidebar />
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
 };
 
 export default MobileSidebar;
