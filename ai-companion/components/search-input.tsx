1ba31d8f8067f4fadc67a7a8e68bed237a7e80b1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-24 21:42:34 +0800

    - apply debounce hook to search-input - useEffect for getting the url and routes to it

--- ai-companion/components/search-input.tsx
+++ ai-companion/components/search-input.tsx
@@ -3,7 +3,9 @@
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/use-debounce";
+import queryString from "query-string";
 
 const SearchInput = () => {
   const router = useRouter();
@@ -12,11 +14,36 @@ const SearchInput = () => {
   const name = searchParams.get("name");
 
   const [value, setValue] = useState(name || "");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId: categoryId,
+    };
+    const url = queryString.stringifyUrl(
+      {
+        url: window.location.href,
+        query: query,
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+    router.push(url);
+  }, [debouncedValue, router, categoryId]);
 
   return (
     <div className="relative">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
-      <Input placeholder="search..." className="pl-10 bg-primary/10" />
+      <Input
+        placeholder="search..."
+        className="pl-10 bg-primary/10"
+        onChange={onChange}
+        value={value}
+      />
     </div>
   );
 };
