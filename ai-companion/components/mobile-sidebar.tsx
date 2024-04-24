f2d22c3c8e8c3765a09184b78975f0fd9af90fb9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-24 17:44:10 +0800

    - basic mobilesidebar component - import mobilesidebar from navbar

---
+++ ai-companion/components/mobile-sidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MobileSidebar = () => {
+  return <div>mobile-sidebar</div>;
+};
+
+export default MobileSidebar;
--- ai-companion/components/navbar.tsx
+++ ai-companion/components/navbar.tsx
@@ -7,6 +7,7 @@ import { Poppins } from "next/font/google";
 import { UserButton } from "@clerk/nextjs";
 import { Menu, Sparkles } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
+import MobileSidebar from "./mobile-sidebar";
 
 const font = Poppins({
   weight: "600",
@@ -17,7 +18,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <MobileSidebar />
         <Link href="/">
           <h1
             className={cn(
