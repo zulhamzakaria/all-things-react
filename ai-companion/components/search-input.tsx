b2cf1029d06ed6f4495c94bc74e8566d46604d40
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-24 21:24:54 +0800

    - added router and searchParams (for getting categoryId and name from url)

--- ai-companion/components/search-input.tsx
+++ ai-companion/components/search-input.tsx
@@ -2,8 +2,17 @@
 
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 const SearchInput = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const categoryId = searchParams.get("categoryId");
+  const name = searchParams.get("name");
+
+  const [value, setValue] = useState(name || "");
+
   return (
     <div className="relative">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
