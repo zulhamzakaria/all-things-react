306a09b36a7e1684b6f17d65be3f7a4cdf8f3534
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-24 16:34:06 +0800

    - set Navbar component to client side - Navbar component styling - npx shadcn-ui@latest add button

--- ai-companion/components/navbar.tsx
+++ ai-companion/components/navbar.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { Poppins } from "next/font/google";
+import { UserButton } from "@clerk/nextjs";
+import { Menu, Sparkles } from "lucide-react";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <Menu className="block md:hidden" />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            companion-ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex itmes-center gap-x-3">
+        <Button size={"sm"}>
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+        </Button>
+        <UserButton />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
