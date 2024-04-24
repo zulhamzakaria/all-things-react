b4e1d4469fd5fd45a96be439c3cfe892e9016da4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-24 17:09:27 +0800

    - basic Sidebar component - import Sidebar from root layout

--- ai-companion/app/(root)/layout.tsx
+++ ai-companion/app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -6,7 +7,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="h-full">
       <Navbar />
       <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
-        Sidebar
+        <Sidebar />
       </div>
       <main className="md:pl-20 pt-16 h-full">{children}</main>
     </div>
