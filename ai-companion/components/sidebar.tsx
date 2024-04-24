97f6c18c5cae318db0f1c4cf09a4334f3154c171
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-24 17:37:08 +0800

    - sidebar items - stylings - navigations

--- ai-companion/components/sidebar.tsx
+++ ai-companion/components/sidebar.tsx
@@ -1,10 +1,56 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { Home, Plus, Settings } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
 const Sidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const onNavigate = (url: string, pro: boolean) => {
+    return router.push(url);
+  };
+
+  const routes = [
+    {
+      href: "/",
+      icon: Home,
+      label: "Home",
+      pro: false,
+    },
+    {
+      href: "/companion/new",
+      icon: Plus,
+      label: "Create",
+      pro: true,
+    },
+    {
+      href: "/settings",
+      icon: Settings,
+      label: "Settings",
+      pro: false,
+    },
+  ];
   return (
     <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
-      <div className="p-3 flex-1 justify-center">
-        <div className="space-y-2">Routes</div>
+      <div className="p-3 flex flex-1 justify-center">
+        <div className="space-y-2">
+          {routes.map((route) => (
+            <div
+              onClick={() => onNavigate(route.href, route.pro)}
+              key={route.href}
+              className={cn(
+                "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
+                pathname === route.href && "bg-primary/10 text-primary"
+              )}
+            >
+              <div className="flex flex-col gap-y-2 items-center flex-1">
+                <route.icon className="h-5 w-5" />
+                {route.label}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
