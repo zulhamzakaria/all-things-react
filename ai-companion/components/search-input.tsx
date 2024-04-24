fa09dc72782c398b567450a8918a9444e8acafc6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-24 21:18:18 +0800

    - searchinput stylings

--- ai-companion/components/search-input.tsx
+++ ai-companion/components/search-input.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+
 const SearchInput = () => {
-  return <div>Search Input</div>;
+  return (
+    <div className="relative">
+      <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+      <Input placeholder="search..." className="pl-10 bg-primary/10" />
+    </div>
+  );
 };
 
 export default SearchInput;
