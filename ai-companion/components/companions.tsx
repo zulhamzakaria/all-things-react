92beb19e504821afef18910cca08bd125747da84
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-25 21:02:08 +0800

    - interface of an array of companions data

--- ai-companion/components/companions.tsx
+++ ai-companion/components/companions.tsx
@@ -1,6 +1,26 @@
-import React from "react";
+import { Companion } from "@prisma/client";
+import Image from "next/image";
 
-const Companions = () => {
+interface CompanionsProps {
+  data: (Companion & {
+    _count: {
+      messages: number;
+    };
+  })[];
+}
+
+const Companions = ({ data }: CompanionsProps) => {
+  if (data.length === 0) {
+    return (
+      <div className="pt-10 flex flex-col items-center justify-center space-y-3">
+        <div className="relative w-60 h-60">
+          <Image fill className="grayscale" alt="empty" src="/empty.png" />
+        </div>
+        <p className="text-sm text-muted-foreground">No companions found.</p>
+      </div>
+    );
+  }
+
   return <div>Companions component</div>;
 };
 
