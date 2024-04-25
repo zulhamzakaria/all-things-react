a6a49f4139082a970d3bd4a0792de7a05055d7e2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-25 22:32:48 +0800

    - chatclient props - stylings

--- ai-companion/app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ ai-companion/app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,9 +1,22 @@
-"use client"
+"use client";
 
-const ChatClient = () => {
-  return (
-    <div>Chat Client</div>
-  )
+import { Companion, Message } from "@prisma/client";
+
+interface ChatClientProps {
+  companion: Companion & {
+    messages: Message[];
+    _count: {
+      messages: number;
+    };
+  };
 }
 
-export default ChatClient
+const ChatClient = ({ companion }: ChatClientProps) => {
+  return (
+    <div className="flex flex-col h-full space-y-2">
+      ChatHeader companion=companion
+    </div>
+  );
+};
+
+export default ChatClient;
