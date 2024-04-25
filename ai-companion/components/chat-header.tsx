501c3ab71d36567e85dfe1fde7f72535d94ae5de
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-25 22:43:49 +0800

    - chat header stylings - back button

--- ai-companion/components/chat-header.tsx
+++ ai-companion/components/chat-header.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { Companion, Message } from "@prisma/client";
+import { Button } from "./ui/button";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface ChatHeaderProps {
   companion: Companion & {
@@ -11,8 +14,17 @@ interface ChatHeaderProps {
   };
 }
 
-const ChatHeader = ({companion}: ChatHeaderProps) => {
-  return <div>Chat Header</div>;
+const ChatHeader = ({ companion }: ChatHeaderProps) => {
+  const router = useRouter();
+  return (
+    <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
+      <div className="flex gap-x-2 items-center">
+        <Button onClick={() => router.back()} size="icon" variant={"ghost"}>
+          <ChevronLeft className="h-8 w-8" />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ChatHeader;
