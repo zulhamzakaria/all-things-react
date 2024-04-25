6d6462e8309c14fbaaf2f99ee0cb84edd9e68c7c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-25 16:28:26 +0800

    - import imageupload component from companion form

--- ai-companion/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ ai-companion/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -12,6 +12,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
+import { ImageUpload } from "@/components/image-upload";
 
 interface CompanionFormProps {
   initialData: Companion | null;
@@ -78,7 +79,13 @@ const CompanionForm = ({ initialData, categories }: CompanionFormProps) => {
             name="src"
             render={({ field }) => (
               <FormItem className="flex flex-col items-center justify-center space-y-4">
-                <FormControl>Image Upload Component</FormControl>
+                <FormControl>
+                  <ImageUpload
+                    disabled={isLoading}
+                    onChange={field.onChange}
+                    value={field.value}
+                  />
+                </FormControl>
                 <FormMessage />
               </FormItem>
             )}
