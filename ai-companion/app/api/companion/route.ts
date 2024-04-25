28f3e83206651f30e574619cd0c71e043ef8b606
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-25 20:04:39 +0800

    - api endpoint for creating companion

---
+++ ai-companion/app/api/companion/route.ts
@@ -0,0 +1,44 @@
+import prismaDb from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instructions, seed, categoryId } = body;
+
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Halt", { status: 401 });
+    }
+
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !instructions ||
+      !seed ||
+      !categoryId
+    ) {
+      return new NextResponse("Incomplete data", { status: 400 });
+    }
+
+    const companion = await prismaDb.companion.create({
+      data: {
+        categoryId,
+        userId: user.id,
+        userName: user.firstName,
+        src,
+        name,
+        description,
+        instructions,
+        seed,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.error("companion post", error);
+    return new NextResponse("Internal Error.", { status: 500 });
+  }
+}
