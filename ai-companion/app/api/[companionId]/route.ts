4c0513316c6259f9a77d00e469d46067f3db6b33
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-25 20:17:33 +0800

    - api enpoint for updating companion

---
+++ ai-companion/app/api/[companionId]/route.ts
@@ -0,0 +1,54 @@
+import prismaDb from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { companionId: string } }
+) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instructions, seed, categoryId } = body;
+
+    if (!params.companionId) {
+      return new NextResponse("Missing Companion Id", { status: 400 });
+    }
+
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Halt", { status: 401 });
+    }
+
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !instructions ||
+      !seed ||
+      !categoryId
+    ) {
+      return new NextResponse("Incomplete data", { status: 400 });
+    }
+
+    const companion = await prismaDb.companion.update({
+      where: {
+        id: params.companionId,
+      },
+      data: {
+        categoryId,
+        userId: user.id,
+        userName: user.firstName,
+        src,
+        name,
+        description,
+        instructions,
+        seed,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.error("companion patch", error);
+    return new NextResponse("Internal Error.", { status: 500 });
+  }
+}
