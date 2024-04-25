504b46f4febb60c88803827d6b06e633b649be76
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-25 14:17:56 +0800

    - added zod (for form schema. works kinda like c# model attribute) - companion form form

--- ai-companion/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ ai-companion/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,14 +1,77 @@
 "use client";
 
+import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
 
 interface CompanionFormProps {
   initialData: Companion | null;
   categories: Category[];
 }
 
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+  description: z.string().min(1, {
+    message: "Name is required",
+  }),
+  instructions: z.string().min(200, {
+    message: "Instruction at minimum requires 200 chars",
+  }),
+  seed: z.string().min(200, {
+    message: "Seed requires 200 chars at minimum ",
+  }),
+  src: z.string().min(1, {
+    message: "Image is required",
+  }),
+  categoryId: z.string().min(1, {
+    message: "Category is required",
+  }),
+});
+
 const CompanionForm = ({ initialData, categories }: CompanionFormProps) => {
-  return <div>Companion Form</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      description: "",
+      instructions: "",
+      seed: "",
+      src: "",
+      categoryId: undefined,
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
+      <Form {...form}>
+        <form
+          className="space-y-8 pb-10"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <div className="space-y-2 w-full col-span-2">
+            <div>
+              <h3 className="text-lg font-medium">General Information</h3>
+              <p className="text-sm text-muted-foreground">
+                General information about ur companion
+              </p>
+            </div>
+            <Separator className="bg-primary/10" />
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default CompanionForm;
