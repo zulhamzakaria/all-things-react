2dd5b8ce09bfa1a366a15b5991a7b6915300d167
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-26 15:43:48 +0800

    - chatmessages prrops - chatmessages form

--- ai-companion/components/chat-messages.tsx
+++ ai-companion/components/chat-messages.tsx
@@ -1,5 +1,17 @@
 "use client";
 
-export const ChatMessages = () => {
-  return <div>Chat messages!</div>;
+import { Companion } from "@prisma/client";
+
+interface ChatMessagesProps {
+  messages: any[];
+  isLoading: boolean;
+  companion: Companion;
+}
+
+export const ChatMessages = ({
+  messages = [],
+  isLoading,
+  companion,
+}: ChatMessagesProps) => {
+  return <div className="flex-1 overflow-y-auto pr-4">Chat messages!</div>;
 };
