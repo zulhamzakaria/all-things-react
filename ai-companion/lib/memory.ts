3da0da2ffb548c4edccd44f9ae4383f2adf158cb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-26 22:00:13 +0800

    - memory service for AI - npm i pinecone-client - end all things. unusable libs - botched clerks login - moving to latest project

---
+++ ai-companion/lib/memory.ts
@@ -0,0 +1,17 @@
+import { Redis } from "@upstash/redis";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { PineconeClient } from "pinecone-client";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+
+export type CompanionKey = {
+  companionName: string;
+  modelName: string;
+  userId: string;
+};
+
+export class MemoryManager {
+  private static instance: MemoryManager;
+  private history: Redis;
+  private vectorDBClient: PineconeClient;
+}
