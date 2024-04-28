6d64ddce4cfa2486cd57277a4a1690e0c4936b07
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-29 00:34:05 +0800

    - basic org sidebar - import org sidebar from layout.tsx

---
+++ miro-clone/app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrganizationSidebar = () => {
+  return <div>Org sidebar...</div>;
+};
+
+export default OrganizationSidebar;
--- miro-clone/app/(dashboard)/layout.tsx
+++ miro-clone/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "./_components/sidebar";
+import OrganizationSidebar from "./_components/org-sidebar";
 
 const LayoutPage = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -7,7 +8,7 @@ const LayoutPage = ({ children }: { children: React.ReactNode }) => {
       <Sidebar />
       <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
-          Org sidebar...
+          <OrganizationSidebar />
           <div className="h-full flex-1">{children}</div>
         </div>
       </div>
