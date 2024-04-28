d835a0f07bc1a33cc5ab366594834af0d899f329
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-29 00:36:45 +0800

    - org sidebar stylings

--- miro-clone/app/(dashboard)/_components/org-sidebar.tsx
+++ miro-clone/app/(dashboard)/_components/org-sidebar.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
 
 const OrganizationSidebar = () => {
-  return <div>Org sidebar...</div>;
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+      Org sidebar...
+    </div>
+  );
 };
 
 export default OrganizationSidebar;
