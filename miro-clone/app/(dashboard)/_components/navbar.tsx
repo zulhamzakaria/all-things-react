9a7acce41dace966657c731e420f34716196dd41
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-29 00:40:53 +0800

    - basic navbar comp - import navbar from layout

---
+++ miro-clone/app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-100">Navbar...</div>
+  );
+};
+
+export default Navbar;
--- miro-clone/app/(dashboard)/layout.tsx
+++ miro-clone/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "./_components/sidebar";
 import OrganizationSidebar from "./_components/org-sidebar";
+import Navbar from "./_components/navbar";
 
 const LayoutPage = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -9,7 +10,10 @@ const LayoutPage = ({ children }: { children: React.ReactNode }) => {
       <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
           <OrganizationSidebar />
-          <div className="h-full flex-1">{children}</div>
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
         </div>
       </div>
     </main>
