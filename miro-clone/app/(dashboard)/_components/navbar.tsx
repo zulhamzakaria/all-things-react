178655b67b7abaf963583b3fd5fd6115ef296206
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-29 00:45:49 +0800

    - added clerk UserButton to navbar

--- miro-clone/app/(dashboard)/_components/navbar.tsx
+++ miro-clone/app/(dashboard)/_components/navbar.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { UserButton } from "@clerk/nextjs";
+
 const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-100">Navbar...</div>
+    <div className="flex items-center gap-x-4 p-5 bg-green-100">
+      <div className="hidden lg:flex lg:flex-1"></div>
+      <UserButton />
+    </div>
   );
 };
 
