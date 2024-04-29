0074d5e42f618ee1bd04f9acfd0422cc65899c29
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-29 12:21:13 +0800

    - changed new-button to be a client side component - stylings - added clerk CreateOrganization - set CreateOrganization routing prop to hash

--- miro-clone/app/(dashboard)/_components/sidebar/new-button.tsx
+++ miro-clone/app/(dashboard)/_components/sidebar/new-button.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+
+import { Plus } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 const NewButton = () => {
-  return <div>NewButton Component</div>;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="aspect-square">
+          <button className="bg-white/25 h-full w-full rounded-lg flex items-center justify-center opacity-60 hover:opacity-100 transition">
+            <Plus className="text-orange-500" />
+          </button>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+        <CreateOrganization routing="hash" />
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default NewButton;
