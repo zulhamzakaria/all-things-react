2b99988e8107c3dc89907ffb1a554d25a9184d4e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-29 22:58:40 +0800

    - props for board-card

--- miro-clone/app/(dashboard)/_components/board-card/index.tsx
+++ miro-clone/app/(dashboard)/_components/board-card/index.tsx
@@ -1,5 +1,25 @@
 "use client";
 
-export const BoardCard = () => {
+interface BoardCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  authorId: string;
+  authorName: string;
+  createdAt: number;
+  orgId: string;
+  isFavourite: boolean;
+}
+
+export const BoardCard = ({
+  id,
+  title,
+  imageUrl,
+  authorId,
+  authorName,
+  createdAt,
+  orgId,
+  isFavourite,
+}: BoardCardProps) => {
   return <div>Board Card!</div>;
 };
