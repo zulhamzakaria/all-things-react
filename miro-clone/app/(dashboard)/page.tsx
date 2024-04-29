111f6ea0981f0de9026d2e4eebd5fc049169c6cb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-29 16:04:20 +0800

    - added props for getting data from params/url based on searchParams

--- miro-clone/app/(dashboard)/page.tsx
+++ miro-clone/app/(dashboard)/page.tsx
@@ -2,10 +2,18 @@
 import { useOrganization } from "@clerk/nextjs";
 import EmptyOrg from "./_components/empty-org";
 
-const DashboardPage = () => {
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
   const { organization } = useOrganization();
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {JSON.stringify(searchParams)}
       {!organization ? <EmptyOrg /> : <p>Board List!</p>}
     </div>
   );
