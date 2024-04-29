7ef945634fd233647187bdf87a60c9e65c735fa1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-29 16:00:41 +0800

    - added condition to the dashboard rootpage to only display EmptyOrg component is theres no organization selected. else, display board list

--- miro-clone/app/(dashboard)/page.tsx
+++ miro-clone/app/(dashboard)/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+"use client";
+import { useOrganization } from "@clerk/nextjs";
 import EmptyOrg from "./_components/empty-org";
 
 const DashboardPage = () => {
+  const { organization } = useOrganization();
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
-      <EmptyOrg />
+      {!organization ? <EmptyOrg /> : <p>Board List!</p>}
     </div>
   );
 };
