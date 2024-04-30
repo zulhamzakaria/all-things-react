55ce4170a39992bcae624d9cb9a9f401e4f5f6d9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-30 23:34:09 +0800

    - mods

--- miro-clone/app/board/[boardId]/_components/toolbar.tsx
+++ miro-clone/app/board/[boardId]/_components/toolbar.tsx
@@ -2,9 +2,13 @@ export const Toolbar = () => {
   return (
     <div className="absolute top-[400px] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div className="flex-grow">Pencil</div>
-        <div className="flex-grow">Pencil</div>
-        <div className="flex-grow">Pencil</div>
+        <div>Pencil</div>
+        <div>Pencil</div>
+        <div>Pencil</div>
+      </div>
+      <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
+        <div>Undo</div>
+        <div>Redo</div>
       </div>
     </div>
   );
