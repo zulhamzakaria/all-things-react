d3f8ba49a58b7131101fc87df71680597a5d607f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-30 23:17:01 +0800

    - basic board participants component - import participants component from canvas

--- miro-clone/app/board/[boardId]/_components/canvas.tsx
+++ miro-clone/app/board/[boardId]/_components/canvas.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { Info } from "./info";
+import { Participants } from "./participants";
 
 export const Canvas = () => {
-  return <main className="h-full w-full relative bg-neutral-100 touch-none">
-    <Info/>
-  </main>;
+  return (
+    <main className="h-full w-full relative bg-neutral-100 touch-none">
+      <Info />
+      <Participants />
+    </main>
+  );
 };
