0a6cff7860058079fd853cc0c2b0803d6cb2ee3b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-30 22:21:35 +0800

    - assign board.isFavorite to BoardCard component isFavorite

--- miro-clone/app/(dashboard)/_components/board-list.tsx
+++ miro-clone/app/(dashboard)/_components/board-list.tsx
@@ -65,7 +65,7 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
             authorName={board.authorName}
             createdAt={board._creationTime}
             orgId={board.orgId}
-            isFavourite={false}
+            isFavourite={board.isFavorite}
           />
         ))}
       </div>
