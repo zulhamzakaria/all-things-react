25cae3c7878905e55f55d66bc58f97f706e2e26e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-30 22:03:54 +0800

    - added favorite, unfavorite api calls to board-carrd index

--- miro-clone/app/(dashboard)/_components/board-card/index.tsx
+++ miro-clone/app/(dashboard)/_components/board-card/index.tsx
@@ -9,6 +9,9 @@ import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Actions } from "@/components/actions";
 import { MoreHorizontal } from "lucide-react";
+import { useAPIMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 interface BoardCardProps {
   id: string;
   title: string;
@@ -30,6 +33,23 @@ export const BoardCard = ({
   orgId,
   isFavourite,
 }: BoardCardProps) => {
+  const { mutate: onFavorite, pending: pendingFavorite } = useAPIMutation(
+    api.board.favorite
+  );
+  const { mutate: onUnfavorite, pending: pendingUnfavorite } = useAPIMutation(
+    api.board.unfavorite
+  );
+
+  const toggleFavorite = () => {
+    if (isFavourite) {
+      onUnfavorite({ id }).catch(() => {
+        toast.error("Failed to unfavorite");
+      });
+    } else {
+      onFavorite({ id, orgId }).catch(() => toast.error("Failed to favorite"));
+    }
+  };
+
   const { userId } = useAuth();
   const authorLabel = userId === authorId ? "You" : authorName;
   const createdAtLabel = formatDistanceToNow(createdAt, {
@@ -53,8 +73,8 @@ export const BoardCard = ({
           title={title}
           authorLabel={authorLabel}
           createdAtLabel={createdAtLabel}
-          onClick={() => {}}
-          disabled={false}
+          onClick={toggleFavorite}
+          disabled={pendingFavorite || pendingUnfavorite}
         />
       </div>
     </Link>
