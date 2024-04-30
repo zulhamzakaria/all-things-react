a6bed7665178a94e08c8736854f51c174e89fbee
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-30 15:49:26 +0800

    - fleshing out rename-modal

--- miro-clone/components/modals/rename-modal.tsx
+++ miro-clone/components/modals/rename-modal.tsx
@@ -10,9 +10,19 @@ import {
   DialogFooter,
   DialogTitle,
 } from "../ui/dialog";
+import { useEffect, useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export const RenameModal = () => {
   const { isOpen, onClose, initialValues } = useRenameModal();
+  const [title, setTitle] = useState(initialValues.title);
+
+  useEffect(() => {
+    setTitle(initialValues.title);
+  }, [initialValues.title]);
+
+  const onSubmit = () => {};
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -21,6 +31,24 @@ export const RenameModal = () => {
           <DialogTitle>Edit board title</DialogTitle>
         </DialogHeader>
         <DialogDescription>Enter a new title for this board</DialogDescription>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <Input
+            required
+            disabled={false}
+            maxLength={60}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Board title"
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="button" variant={"outline"}>Cancel</Button>
+            </DialogClose>
+            <Button disabled={false} type="submit">
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
