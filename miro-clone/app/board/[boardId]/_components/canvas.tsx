b79228e52262a6952492d8d937f06209b573bf5b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-30 23:12:32 +0800

    - basic board info component - import info component from canvas - info stylings

--- miro-clone/app/board/[boardId]/_components/canvas.tsx
+++ miro-clone/app/board/[boardId]/_components/canvas.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Info } from "./info";
+
 export const Canvas = () => {
   return <main className="h-full w-full relative bg-neutral-100 touch-none">
-    Canvas
+    <Info/>
   </main>;
 };
