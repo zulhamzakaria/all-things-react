87b903240be884b294cafb14371b0eb20d4f9876
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-30 22:38:15 +0800

    - allow for get enpoint to return data based on search query

--- miro-clone/convex/boards.ts
+++ miro-clone/convex/boards.ts
@@ -2,17 +2,30 @@ import { v } from "convex/values";
 import { query } from "./_generated/server";
 
 export const getAll = query({
-  args: { orgId: v.string() },
+  args: { orgId: v.string(), search: v.optional(v.string()) },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       throw new Error("Halt! Unauthorized access");
     }
-    const boards = await ctx.db
-      .query("boards")
-      .withIndex("by_org", (g) => g.eq("orgId", args.orgId))
-      .order("desc")
-      .collect();
+
+    const title = args.search as string;
+    let boards = [];
+
+    if (title) {
+      boards = await ctx.db
+        .query("boards")
+        .withSearchIndex("search_title", (q) =>
+          q.search("title", title).eq("orgId", args.orgId)
+        )
+        .collect();
+    } else {
+      boards = await ctx.db
+        .query("boards")
+        .withIndex("by_org", (g) => g.eq("orgId", args.orgId))
+        .order("desc")
+        .collect();
+    }
 
     const boardsWithFavoriteRelation = boards.map((board) => {
       return ctx.db
