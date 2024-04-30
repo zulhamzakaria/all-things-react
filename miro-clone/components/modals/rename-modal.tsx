a68123ba80dc998972d03279e6b2ce8f31cccdbb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-04-30 15:25:06 +0800

    rename modal component for updating board title

---
+++ miro-clone/components/modals/rename-modal.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useRenameModal } from "@/store/use-rename-modal";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogClose,
+  DialogFooter,
+  DialogTitle,
+} from "../ui/dialog";
+
+export const RenameModal = () => {
+  const { isOpen, onClose, initialValues } = useRenameModal();
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit board title</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>Enter a new title for this board</DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+};
