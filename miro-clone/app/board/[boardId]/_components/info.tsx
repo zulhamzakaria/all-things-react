e09f42c6816a09a057f0ad120a59d568bb19ad92
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-01 15:32:46 +0800

    - fetch current board - added boardId props fro Info component to canvas - info stylings

--- miro-clone/app/board/[boardId]/_components/info.tsx
+++ miro-clone/app/board/[boardId]/_components/info.tsx
@@ -1,11 +1,29 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
-export const Info = () => {
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export const Info = ({ boardId }: { boardId: string }) => {
+  const data = useQuery(api.board.get, { id: boardId as Id<"boards"> });
+
+  if (!data) return <InfoSkeleton />;
+
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      Info
+      <Button className="px-2">
+        <Image src="/logo.png" alt="info" height={40} width={40} />
+      </Button>
     </div>
   );
 };
