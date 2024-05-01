85ea234008edc3340a52a00f984d5210f5665150
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-01 22:09:54 +0800

    - cursor compoennt

---
+++ miro-clone/app/board/[boardId]/_components/cursor.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { memo } from "react";
+import { MousePointer2 } from "lucide-react";
+import { connectionIdToColor } from "@/lib/utils";
+import { useOther } from "@/liveblocks.config";
+
+interface CursorProps {
+  connectionId: number;
+}
+
+export const Cursor = memo(({ connectionId }: CursorProps) => {
+  const info = useOther(connectionId, (user) => user?.info);
+  const cursor = useOther(connectionId, (user) => user.presence.cursor);
+
+  const name = info?.name || "some dude from Sales";
+
+  if (!cursor) {
+    return null;
+  }
+
+  return <p>Cursor!</p>;
+});
+
+Cursor.displayName = "Cursor";
