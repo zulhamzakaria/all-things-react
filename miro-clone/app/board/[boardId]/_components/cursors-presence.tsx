4ba4025988d95352dae33d53d453ba5f0e641a2d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-01 22:00:10 +0800

    - added Cursors variable that has all the Cursor based on connectionId - calls the Cursors from CursorsPresence

--- miro-clone/app/board/[boardId]/_components/cursors-presence.tsx
+++ miro-clone/app/board/[boardId]/_components/cursors-presence.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { memo } from "react";
+import { useOthersConnectionIds } from "@/liveblocks.config";
+
+const Cursors = () => {
+  const ids = useOthersConnectionIds();
+  return (
+    <>
+      {ids.map((connectionId) => (
+        <Cursor key={connectionId} connectionId={connectionId} />
+      ))}
+    </>
+  );
+};
 
 export const CursorsPresence = memo(() => {
   return (
     <>
-      <p>cursors</p>
+      <Cursors/>
     </>
   );
 });
