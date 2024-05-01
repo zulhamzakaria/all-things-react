e8e1def97a2e2954eda7a3589b124edd81edf1fb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-01 15:05:47 +0800

    - marked boardid loading component with useClient since info componet is client side

--- miro-clone/app/board/[boardId]/_components/info.tsx
+++ miro-clone/app/board/[boardId]/_components/info.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Skeleton } from "@/components/ui/skeleton";
 
 export const Info = () => {
@@ -11,7 +13,7 @@ export const Info = () => {
 Info.Skeleton = function InfoSkeleton() {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]">
-      <Skeleton className="h-full w-full bg-muted-400"/>
+      <Skeleton className="h-full w-full bg-muted-400" />
     </div>
   );
 };
--- miro-clone/app/board/[boardId]/_components/loading.tsx
+++ miro-clone/app/board/[boardId]/_components/loading.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Loader } from "lucide-react";
 
 import { Info } from "./info";
@@ -8,9 +10,9 @@ export const Loading = () => {
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none flex items-center justify-center">
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
-      <Info.Skeleton/>
-      <Participants.Skeleton/>
-      <Toolbar.Skeleton/>
+      <Info.Skeleton />
+      <Participants.Skeleton />
+      <Toolbar.Skeleton />
     </main>
   );
 };
