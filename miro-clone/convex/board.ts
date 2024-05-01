739a8b466ba0178c1c52a8c35801b0971d84c993
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-01 14:22:40 +0800

    - api endpoint for getting board based on id

--- miro-clone/convex/board.ts
+++ miro-clone/convex/board.ts
@@ -1,4 +1,4 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
 const images = [
@@ -156,6 +156,14 @@ export const unfavorite = mutation({
 
     await ctx.db.delete(existingFavorite._id);
 
+    return board;
+  },
+});
+
+export const get = query({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const board = ctx.db.get(args.id);
     return board;
   },
 });
