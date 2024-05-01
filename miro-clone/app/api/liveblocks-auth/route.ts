9fab7497f956d7b2e96178d8fe186cb10283747d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-01 14:43:16 +0800

    - api endpoint for liveblocks

---
+++ miro-clone/app/api/liveblocks-auth/route.ts
@@ -0,0 +1,42 @@
+import { ConvexHttpClient } from "convex/browser";
+import { Liveblocks } from "@liveblocks/node";
+import { api } from "@/convex/_generated/api";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+const liveblocks = new Liveblocks({
+  secret:
+    "sk_prod_AJCHSggnmUD0TG5nep6sgIKsJu5MGIXe1UcLCzhnJYdUpxkrnLm0a_PAC6RVTLz3",
+});
+
+export async function POST(request: Request) {
+  const authorization = await auth();
+  const user = await currentUser();
+
+  if (!authorization || !user) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const { room } = await request.json();
+  const board = await convex.query(api.board.get, { id: room });
+
+  if (board?.orgId !== authorization.orgId) {
+    return new Response("Unauthorized");
+  }
+
+  const userInfo = {
+    name: user.firstName || "some dude from Sales",
+    picture: user.imageUrl,
+  };
+
+  const session = liveblocks.prepareSession(user.id, { userInfo });
+
+  if (room) {
+    session.allow(room, session.FULL_ACCESS);
+  }
+
+  const { status, body } = await session.authorize();
+
+  return new Response(body, { status });
+}
