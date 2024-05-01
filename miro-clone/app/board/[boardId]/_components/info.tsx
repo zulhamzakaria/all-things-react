55d642e7ddeee80d03d59c923e77522bfc405f3c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-01 13:34:39 +0800

    - skeleton to info.tsx for holding the canvas tools - import info skeleton from boardid loading component

--- miro-clone/app/board/[boardId]/_components/info.tsx
+++ miro-clone/app/board/[boardId]/_components/info.tsx
@@ -1,7 +1,17 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export const Info = () => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
       Info
+    </div>
+  );
+};
+
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]">
+      <Skeleton className="h-full w-full bg-muted-400"/>
     </div>
   );
 };
