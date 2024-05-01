2be0d1481aa3dc62d47b917c3fbb6669db826b0b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-01 16:03:55 +0800

    - boardid infor component stylings  set button asChild if ure putitng Link inside it

--- miro-clone/app/board/[boardId]/_components/info.tsx
+++ miro-clone/app/board/[boardId]/_components/info.tsx
@@ -8,6 +8,11 @@ import Image from "next/image";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Hint } from "@/components/hint";
+import { useRenameModal } from "@/store/use-rename-modal";
+import { Actions } from "@/components/actions";
+import { Menu } from "lucide-react";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -15,15 +20,49 @@ const font = Poppins({
 });
 
 export const Info = ({ boardId }: { boardId: string }) => {
+  const { onOpen } = useRenameModal();
+
   const data = useQuery(api.board.get, { id: boardId as Id<"boards"> });
 
   if (!data) return <InfoSkeleton />;
 
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      <Button className="px-2">
-        <Image src="/logo.png" alt="info" height={40} width={40} />
-      </Button>
+      <Hint label="Back to boards" side="bottom" sideOffset={10}>
+        <Button asChild variant={"board"} className="px-2">
+          <Link href="/">
+            <Image src="/logo.png" alt="info" height={40} width={40} />
+            <span
+              className={cn(
+                " font-semibold text-xl ml-2 text-black",
+                font.className
+              )}
+            >
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          variant={"board"}
+          className=" text-base font-normal px-2"
+          onClick={() => onOpen(data._id, data.title)}
+        >
+          {data.title}
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Actions id={data._id} title={data.title} side="bottom" sideOffset={10}>
+        <div>
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button size={"icon"} variant={"board"}>
+              <Menu />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   );
 };
@@ -34,4 +73,8 @@ export const InfoSkeleton = () => {
       <Skeleton className="h-full w-full bg-muted-400" />
     </div>
   );
+};
+
+const TabSeparator = () => {
+  return <div className=" text-neutral-300 px-1.5">|</div>;
 };
