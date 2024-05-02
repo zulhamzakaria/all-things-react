6ffd92aa921ac2a8ca724e35063e798b092414da
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-02 15:00:35 +0800

    - show users handles - get corner

--- miro-clone/app/board/[boardId]/_components/selection-box.tsx
+++ miro-clone/app/board/[boardId]/_components/selection-box.tsx
@@ -38,6 +38,24 @@ export const SelectionBox = memo(
           width={bounds.width}
           height={bounds.height}
         />
+        {isShowingHandles && (
+          <>
+            <rect
+              className=" fill-white stroke-1 stroke-blue-500"
+              x={0}
+              y={0}
+              style={{
+                cursor: "nwse-resize",
+                width: `${HANDLE_WIDTH}px`,
+                height: `${HANDLE_WIDTH}px`,
+                transform: `translate(${bounds.x - HANDLE_WIDTH / 2}px,${bounds.y - HANDLE_WIDTH / 2}px)`,
+              }}
+              onPointerDown={(e) => {
+                e.stopPropagation();
+              }}
+            />
+          </>
+        )}
       </>
     );
   }
