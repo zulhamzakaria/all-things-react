4cfa9baac9352510cef52f1aa18d501eab800c51
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-03 12:35:21 +0800

    - delete layer hook

---
+++ miro-clone/hooks/use-delete-layers.ts
@@ -0,0 +1,18 @@
+import { useSelf, useMutation } from "@/liveblocks.config";
+
+export const useDeleteLayers = () => {
+  const selection = useSelf((me) => me.presence.selection);
+
+  return useMutation(({ storage, setMyPresence }) => {
+    const liveLayer = storage.get("layers");
+    const liveLayerId = storage.get("layerIds");
+    for (const id of selection) {
+      liveLayer.delete(id);
+      const idx = liveLayerId.indexOf(id);
+      if (idx !== -1) {
+        liveLayerId.delete(idx);
+      }
+    }
+    setMyPresence({ selection: [] }, { addToHistory: true });
+  }, []);
+};
