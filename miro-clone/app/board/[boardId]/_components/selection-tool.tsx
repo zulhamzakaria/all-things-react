dc32f83d514f227b384a47ce6b05052a9ae35bba
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-03 12:43:34 +0800

    - apply delete layer

--- miro-clone/app/board/[boardId]/_components/selection-tool.tsx
+++ miro-clone/app/board/[boardId]/_components/selection-tool.tsx
@@ -5,6 +5,10 @@ import { useMutation, useSelf } from "@/liveblocks.config";
 import { Camera, Color } from "@/types/canvas";
 import { memo } from "react";
 import { ColorPicker } from "./color-picker";
+import { useDeleteLayers } from "@/hooks/use-delete-layers";
+import { Button } from "@/components/ui/button";
+import { Hint } from "@/components/hint";
+import { Trash2 } from "lucide-react";
 
 interface SelectionToolProps {
   camera: Camera;
@@ -35,6 +39,8 @@ export const SelectionTool = memo(
       [selection, setLastUsedColor]
     );
 
+    const deleteLayer = useDeleteLayers();
+
     return (
       <div
         className="absolute p-3 rounded-xl bg-white shadow-sm border flex select-none"
@@ -43,6 +49,13 @@ export const SelectionTool = memo(
         }}
       >
         <ColorPicker onChange={setFill} />
+        <div className=" flex items-center pl-2 ml-2 border-l border-neutral-200">
+          <Hint label="Delete">
+            <Button variant={"board"} size={"icon"} onClick={deleteLayer}>
+              <Trash2 />
+            </Button>
+          </Hint>
+        </div>
       </div>
     );
   }
