303704e22106e334231a337c5693ec5baf1f107b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-07 13:56:37 +0800

    - Hello component - for vite, need to manually import the component. ctrl+. doesnt work - use cap for component to differentiate between a component and html tag

---
+++ react-masterclass/src/components/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+  return <h1>Hello from the underworld</h1>;
+}
+
+export default Hello;
--- react-masterclass/src/main.jsx
+++ react-masterclass/src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Hello from "./components/Hello";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Hello />
+  </React.StrictMode>
+);
