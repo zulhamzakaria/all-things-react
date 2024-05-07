122b1d1d26cf7f03afaf1fcb6980bc6fbf9e0f75
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-07 21:55:20 +0800

    - condiitonal rendition using element var

--- react-masterclass/src/components/ConditionalComponent.jsx
+++ react-masterclass/src/components/ConditionalComponent.jsx
@@ -3,10 +3,13 @@ import ConditionTrue from "./ConditionTrue";
 
 export default function ConditionalComponent() {
   const display = true;
+  let messageOne = <h1>Message 1</h1>;
+  let messageTwo = <h1>Message 2</h1>;
   return (
     <div>
       {/* {display ? <h3>conditional component</h3> : <h3>code erryday</h3>} */}
-      {display ? <ConditionTrue /> : <ConditionFalse />}
+      {/* {display ? <ConditionTrue /> : <ConditionFalse />} */}
+      {display ? messageOne : messageTwo}
     </div>
   );
 }
