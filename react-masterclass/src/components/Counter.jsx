d68d8afc04a0ed8e076a6c5e60e9f80086173577
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-07 23:13:49 +0800

    - two states

--- react-masterclass/src/components/Counter.jsx
+++ react-masterclass/src/components/Counter.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function Counter() {
   const [counter, setCounter] = useState(0);
+  const [increaseBy, setIncreaseBy] = useState(0);
 
   const handleClick = () => {
     setCounter(counter + 1);
@@ -10,12 +11,21 @@ export default function Counter() {
     if (counter <= 0) return;
     setCounter(counter - 1);
   };
+  const incrementBy = () => {
+    setIncreaseBy(increaseBy + counter);
+  };
   return (
     <div>
       {counter}
       <div />
       <button onClick={handleClick}>Increase!</button>
       <button onClick={decrement}>Decrease!</button>
+      <div />
+      <div />
+      <div />
+      current value: {increaseBy}
+      <div />
+      <button onClick={incrementBy}>Increase!</button>
     </div>
   );
 }
