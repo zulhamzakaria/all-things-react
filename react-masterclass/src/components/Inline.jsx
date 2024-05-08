71d76087267100a1b147b145cd9fb58d3a76201b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-08 12:11:20 +0800

    - inline styling - use style={{color:blue}}

---
+++ react-masterclass/src/components/Inline.jsx
@@ -0,0 +1,9 @@
+export default function InlineComponent() {
+  return (
+    <div>
+      <h1 style={{ color: "blueviolet", fontFamily: "cursive" }}>
+        Inline stylings
+      </h1>
+    </div>
+  );
+}
