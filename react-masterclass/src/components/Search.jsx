3f469d40a2c0008967faf5c5daa1d1f16de8a5b1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-08 21:42:25 +0800

    - hooks in react - kinda like functions but can only be called inside react component i.e export default function Search() {   const [query, setQuery] = useState("");   ...   } - useEffect hook allows for syncing react conponent with an external system/API - u can call a function thats insiede a useEffect manually within the useEffect itself - useEffect is triggered during page refresh - use the useEffect dependency array/list to trigger the useEffect

--- react-masterclass/src/components/Search.jsx
+++ react-masterclass/src/components/Search.jsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from "react";
+
 export default function Search() {
-  return <div>Search for recipe</div>;
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    // if (query === "") return;
+  }, [query]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for recipe"
+        value={query}
+      ></input>
+    </div>
+  );
 }
