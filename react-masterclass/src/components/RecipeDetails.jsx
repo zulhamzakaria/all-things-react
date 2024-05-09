29de0b675143f120c3e415d9fe9d57304e0df3dc
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-09 14:41:43 +0800

    - api call for details - take note that useEffect gets triggered during page refresh; meaning the URL is incomplete due to the absence of foodId - either set a condition or define a default id - useEffect runs after every render

--- react-masterclass/src/components/RecipeDetails.jsx
+++ react-masterclass/src/components/RecipeDetails.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function RecipeDetails({ foodId }) {
-  return <div>Recipe Details</div>;
+  const [food, setFood] = useState({});
+  const URL = `some api url${foodId}`;
+  const API_KEY = "some api key";
+
+  useEffect(() => {
+    async function fetchRecipeDetails() {
+      if (foodId === 0) {
+        return;
+      }
+      try {
+        const response = await fetch(`${URL}?apiKey=${API_KEY}`);
+        const result = await response.json();
+        setFood(result);
+      } catch (error) {
+        console.error("somn wrong...", error);
+      }
+    }
+    fetchRecipeDetails();
+  }, [foodId]);
+
+  return (
+    <div>
+      {food.image}
+      {food.title}
+    </div>
+  );
 }
