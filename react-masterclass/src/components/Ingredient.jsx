eac0949d313b740673faed69b7d8cbab2dbdccb5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-09 16:54:43 +0800

    - ingredient stylings

--- react-masterclass/src/components/Ingredient.jsx
+++ react-masterclass/src/components/Ingredient.jsx
@@ -1,13 +1,18 @@
+import styles from "./ingredient.module.css";
+
 export default function Ingredient({ ingredient }) {
   return (
-    <div>
-      <div>
+    <div className={styles.ingredient_container}>
+      <div className={styles.image_container}>
         <img src={`food_image_url${ingredient.image}`} alt="food_image.jpeg" />
       </div>
-      <div>
-        <h3>
-          {ingredient.amount} {ingredient.unit} of {ingredient.name}
-        </h3>
+      <div className={styles.name_container}>
+        <div>
+          <p className={styles.amount}>
+            {ingredient.amount} {ingredient.unit}
+          </p>
+          of <p className={styles.name}>{ingredient.name}</p>
+        </div>
       </div>
     </div>
   );
