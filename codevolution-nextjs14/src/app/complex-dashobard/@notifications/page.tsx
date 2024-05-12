610de06fb6bbe663c238ff4551f38b4a9f71d644
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-12 22:58:09 +0800

    - parallel routes - renders simultaneous pages within the same layout - use slots (@folderName) - slots can be defined as ReactNode inside the props so theres no need to import it like components - slot DOESNT affect routing i.e CANNOT be navigated to - each slot MAY have its own error and loading states

---
+++ codevolution-nextjs14/src/app/complex-dashobard/@notifications/page.tsx
@@ -0,0 +1,5 @@
+import Card from "@/components/card";
+
+export default function NotificationsPage() {
+  return <Card>Notifications page</Card>;
+}
---
+++ codevolution-nextjs14/src/app/complex-dashobard/layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function ComplexDashboardLayout({
+  children,
+  users,
+  revenue,
+  notifications,
+}: {
+  children: React.ReactNode;
+  users: React.ReactNode;
+  revenue: React.ReactNode;
+  notifications: React.ReactNode;
+}) {
+  return (
+    <>
+      <div>{children}</div>;
+      <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>{users}</div>
+          <div>{revenue}</div>
+        </div>
+        <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
+      </div>
+    </>
+  );
+}
