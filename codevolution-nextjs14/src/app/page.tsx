467bf21101722957a53b0c7000ab5dd635d0eac5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-12 20:42:15 +0800

    - UI Navigation - next/link Link component

--- codevolution-nextjs14/src/app/page.tsx
+++ codevolution-nextjs14/src/app/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h2>Homepage!</h2>;
+  return (
+    <div>
+      <h2>Homepage!</h2>
+      <Link href="/about" className=" p-4">
+        About
+      </Link>{" "}
+      {" | "}
+      <Link href="/blog" className=" p-4">
+        Blog
+      </Link>
+    </div>
+  );
 }
--- codevolution-nextjs14/src/app/products/page.tsx
+++ codevolution-nextjs14/src/app/products/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function ProductList() {
   return (
     <>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
+      <h2>
+        <Link href="/products/1">Product 1</Link>
+      </h2>
       <h2>Product 2</h2>
       <h2>Product 3</h2>
     </>
