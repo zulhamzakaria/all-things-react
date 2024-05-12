97151b7e12d1c15750be1d0066d033cffcad6d54
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-12 16:27:20 +0800

    - layout

--- codevolution-nextjs14/src/app/layout.tsx
+++ codevolution-nextjs14/src/app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header style={{ backgroundColor: "lightblue", padding: "1rem" }}>
+          Root Layout Header
+        </header>
+        {children}
+        <footer style={{ backgroundColor: "navajowhite" }}>
+          Root Layout Footer
+        </footer>
+      </body>
     </html>
   );
 }
