db068ae2e73318c25b52bba14226c4f0c79bb7e4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-13 17:13:54 +0800

    URL Query Parameters ✨ - get request base query string

--- codevolution-nextjs14/src/app/comments/route.ts
+++ codevolution-nextjs14/src/app/comments/route.ts
@@ -1,7 +1,16 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-  return Response.json(comments);
+export async function GET(req: NextRequest) {
+  const queryString = req.nextUrl.searchParams;
+  // search for the specific string 'query'
+  const query = queryString.get("query");
+
+  const filteredComment = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+
+  return Response.json(filteredComment);
 }
 
 export async function POST(req: Request) {
