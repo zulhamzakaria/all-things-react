0ef9b1f4ee8d025a482c48e8cef7586075e249ec
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-13 17:19:07 +0800

    Redirects in Route Handlers - useful for conditional code execution i.e id > comments.length - if (parseInt(params.id) > comments.length) {     redirect("/comments");   }

--- codevolution-nextjs14/src/app/comments/[id]/route.ts
+++ codevolution-nextjs14/src/app/comments/[id]/route.ts
@@ -1,9 +1,14 @@
 import { comments } from "../data";
+import { redirect } from "next/navigation";
 
 export async function GET(
   _req: Request,
   { params }: { params: { id: string } }
 ) {
+  if (parseInt(params.id) > comments.length) {
+    redirect("/comments");
+  }
+
   const comment = comments.find(
     (comment) => comment.id === parseInt(params.id)
   );
