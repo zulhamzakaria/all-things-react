bd7bf16723234d9f1e1e5085dd42d18b650e5d8e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-13 15:36:05 +0800

    - post endpoint - json body => {"text":"some comment"}

--- codevolution-nextjs14/src/app/comments/route.ts
+++ codevolution-nextjs14/src/app/comments/route.ts
@@ -3,3 +3,16 @@ import { comments } from "./data";
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(req: Request) {
+  const comment = await req.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(comments), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  });
+}
