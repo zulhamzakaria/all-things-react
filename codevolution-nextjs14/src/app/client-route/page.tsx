6e5f21146e18e666cd9ff38c4f6e344ab9f1df34
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-14 00:07:03 +0800

    Server Only Page - import server-only package and it will trigger compile time error if it's being imported by a client page - "Error:   × You're importing a component that needs server-only..."

--- codevolution-nextjs14/src/app/client-route/page.tsx
+++ codevolution-nextjs14/src/app/client-route/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+// import { serverSideFunction } from "@/utils/server-utils";
+
 export default function ClientOnlyPage() {
+  //   const result = serverSideFunction();
   return <div>This is a client-only component</div>;
 }
--- codevolution-nextjs14/src/utils/server-utils.ts
+++ codevolution-nextjs14/src/utils/server-utils.ts
@@ -1,3 +1,5 @@
+import "server-only";
+
 export const serverSideFunction = () => {
   console.log("huh?");
   return "server result";
