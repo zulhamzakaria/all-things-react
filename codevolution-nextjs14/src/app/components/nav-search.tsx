2a0570b1b6ea1fe69bd1132b4886a318cce9b7f7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-14 16:59:35 +0800

    Client Component Placement - server component CAN import a client component - place 'use client' as low as possible in the components tree

---
+++ codevolution-nextjs14/src/app/components/nav-search.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
+export default function NavSearch() {
+  const [search, setSearch] = useState("");
+
+  return <div>Nav search input</div>;
+}
---
+++ codevolution-nextjs14/src/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+import NavLinks from "./nav-links";
+import NavSearch from "./nav-search";
+
+export default function Navbar() {
+  return (
+    <div>
+      <NavLinks />
+      <NavSearch />
+    </div>
+  );
+}
