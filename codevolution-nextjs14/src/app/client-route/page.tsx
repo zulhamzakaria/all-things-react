fb2eeb1d6f6753686b05285de0eccaf9383dc3b0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-14 15:35:19 +0800

    3rd Party Packages - server component CAN import client component

--- codevolution-nextjs14/src/app/client-route/page.tsx
+++ codevolution-nextjs14/src/app/client-route/page.tsx
@@ -1,8 +1,33 @@
 "use client";
 
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 // import { serverSideFunction } from "@/utils/server-utils";
 
 export default function ClientOnlyPage() {
   //   const result = serverSideFunction();
-  return <div>This is a client-only component</div>;
+
+  const settings = { dots: true };
+
+  return (
+    <div className="image-slider-container">
+      <h1>This is a client-only component</h1>
+      <Slider {...settings}>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+        <div>
+          <img src="https://picsum.photos/400/200" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
