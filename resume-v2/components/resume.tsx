72ed06cf6a5a62e97ba31f29240d97d094bad1f0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-16 07:46:49 +0800

    - stylings

--- resume-v2/components/resume.tsx
+++ resume-v2/components/resume.tsx
@@ -1,6 +1,6 @@
 const ResumePage = () => {
   return (
-    <div className=" h-[auto] w-[900px] bg-slate-50 rounded-sm shadow-sm">
+    <div className=" h-[auto] w-[900px] bg-slate-50 rounded-sm shadow-sm pt-10 pb-10 pl-5 pr-5">
       This is the resume body
     </div>
   );
