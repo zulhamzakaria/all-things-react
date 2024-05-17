797b1f9abf836ac2cbda6304df45024a6eb6343d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-17 21:09:25 +0800

    - middleware protected routes

--- resume-v2/middleware.ts
+++ resume-v2/middleware.ts
@@ -1,6 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const protectedRoutes = createRouteMatcher(["/some-link"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoutes(req)) auth().protect();
+});
 
 // Stop Middleware running on static files
 export const config = {
