31b992b0fa1ef81af020a2fc826b2e3321bdf9ca
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-17 20:11:46 +0800

    - resume icon - navbar stylings

--- resume-v2/components/navbar.tsx
+++ resume-v2/components/navbar.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export const NavBar = () => {
   return (
     <nav className=" flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-      Navbar
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/resume-icon.png"
+          alt="logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className=" text-[26px] font-extrabold text-white max-sm:hidden">
+          Resume
+        </p>
+      </Link>
     </nav>
   );
   //   clerk, about
