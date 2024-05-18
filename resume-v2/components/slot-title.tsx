7c566034c457a3ddc3b6e24bdeed000be8169b0b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-18 21:35:41 +0800

    - stylings

--- resume-v2/components/slot-title.tsx
+++ resume-v2/components/slot-title.tsx
@@ -1,8 +1,23 @@
+import { SignedIn } from "@clerk/nextjs";
+import { PenIcon } from "lucide-react";
+
 const SlotTitle = ({ title }: { title?: string }) => {
   return (
-    <div className=" w-full font-bold text-slate-950 border-solid border-b-4 border-red-600 mb-2 ">
-      {title?.toLocaleUpperCase()}
-    </div>
+    <>
+      <div className=" w-full flex-between font-bold text-slate-950 border-solid border-b-4 border-red-600 mb-2 ">
+        <p>{title?.toLocaleUpperCase()}</p>
+        <div>
+          <SignedIn>
+            <PenIcon
+              className=" cursor-pointer"
+              onClick={() => alert("click")}
+              width={15}
+              height={15}
+            />
+          </SignedIn>
+        </div>
+      </div>
+    </>
   );
 };
 
