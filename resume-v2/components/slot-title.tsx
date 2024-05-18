2492010005cef4c77187821ff377e2b50c6783c8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-18 21:37:49 +0800

    - stylings

--- resume-v2/components/slot-title.tsx
+++ resume-v2/components/slot-title.tsx
@@ -5,8 +5,8 @@ const SlotTitle = ({ title }: { title?: string }) => {
   return (
     <>
       <div className=" w-full flex-between font-bold text-slate-950 border-solid border-b-4 border-red-600 mb-2 ">
-        <p>{title?.toLocaleUpperCase()}</p>
-        <div>
+        <p className="flex items-center gap-1">{title?.toLocaleUpperCase()}</p>
+        <div className=" flex-between gap-5">
           <SignedIn>
             <PenIcon
               className=" cursor-pointer"
