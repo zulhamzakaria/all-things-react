55cdf7f6bf3d44ec1a80ad278e5f31e17dcab82e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-19 22:37:21 +0800

    - qqqq

--- resume-v2/app/dashboard/layout1.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ResumeLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className=" bg-red-200">{children}</div>;
-};
-
-export default ResumeLayout;
--- resume-v2/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const DashboardPage = () => {
-  return <div>cock</div>;
-};
-
-export default DashboardPage;
