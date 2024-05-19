84b82133544129d9d16777649f313a1b2ace99d0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-19 23:51:14 +0800

    - error?

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className=" justify-center flex items-center h-screen font-extrabold">
-      {/* <h1>App Page</h1> */}
+      <h1>App Page</h1>
     </div>
   );
 }
