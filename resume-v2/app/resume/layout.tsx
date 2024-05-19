0c85d66ac357d73aa3d30a914ccbe6d1300359ef
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-19 22:18:57 +0800

    - aaaaa

--- resume-v2/app/resume/layout.tsx
+++ resume-v2/app/resume/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ResumeLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return <div></div>;
 };
 
 export default ResumeLayout;
