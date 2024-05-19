a9ce3870e2226d5a89e07d68d774d1b813bfb62f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-20 00:04:10 +0800

    - it works for app page actually. need to style it properly

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -3,7 +3,8 @@ import Dashboard from "./dashboard/page";
 export default function Home() {
   return (
     <div className=" justify-center flex items-center h-screen font-extrabold">
-      <h1>App Page</h1>
+      <h1>App Page!
+      </h1>
     </div>
   );
 }
