7e1d6fd31dafddb2545254a031d63f8c74207447
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-19 23:43:06 +0800

    - flex, justify-center to set item to be dead center

--- test-app/app/dashboard/layout.tsx
+++ test-app/app/dashboard/layout.tsx
@@ -6,8 +6,8 @@ const layout = ({
   details: React.ReactNode;
 }) => {
   return (
-    <div className=" h-screen items-center w-auto bg-amber-50 flex">
-      <div className="w-[595px] bg-slate-600 items-center text-center">
+    <div className=" h-screen w-auto bg-amber-50 flex justify-center">
+      <div className="w-[595px] h-[calc(100vh-50px)] bg-slate-600 items-center text-center flex-col mt-[50px]">
         {children}
         {details}
       </div>
