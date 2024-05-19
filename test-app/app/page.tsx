5298f3573b6e1c984b1cd39edf603a021e2a48f2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-19 23:56:08 +0800

    - no slots for app page?

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -1,7 +1,10 @@
+import Dashboard from "./dashboard/page";
+
 export default function Home() {
   return (
     <div className=" justify-center flex items-center h-screen font-extrabold">
       <h1>App Page</h1>
+      <Dashboard />
     </div>
   );
 }
