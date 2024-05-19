8b356c98a91f1c14c800984576ab605264953a25
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-19 23:55:04 +0800

    - revert

--- test-app/app/layout.tsx
+++ test-app/app/layout.tsx
@@ -11,17 +11,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  appdetails,
 }: Readonly<{
   children: React.ReactNode;
-  appdetails: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        {appdetails}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
