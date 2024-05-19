f1311e8a3accb2795eb3a406b3101653aa6417ae
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-19 23:08:54 +0800

    - copied compex-dashobard

---
+++ test-app/app/complex-dashobard/@notifications/archive/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/card";
+import Link from "next/link";
+
+export default function ArchivedNotificationsPage() {
+  return (
+    <Card>
+      <div>Archived Notifications page</div>
+      <Link href="/complex-dashobard">Default</Link>
+    </Card>
+  );
+}
