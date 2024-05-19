d921fcfbfa9dbc85e230888d57e7433339e568b5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-19 23:57:54 +0800

    - dss

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className=" justify-center flex items-center h-screen font-extrabold">
       <h1>App Page</h1>
-      <Dashboard />
     </div>
   );
 }
