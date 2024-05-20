c8633ef0b323cfda0e1b00301a949cd16265b1ab
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-20 20:29:30 +0800

    - zzz

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -2,9 +2,8 @@ import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
-    <div className=" justify-center flex items-center h-screen font-extrabold">
-      <h1>App Page!
-      </h1>
+    <div className=" justify-center flex items-center font-extrabold">
+      <h1>App Page!</h1>
     </div>
   );
 }
