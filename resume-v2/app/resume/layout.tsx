efb2e6d56c35d5094f9e715caddc58180102a683
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-20 21:00:55 +0800

    - resume layout page

---
+++ resume-v2/app/resume/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ResumeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <p>ResumeLayout</p>
+      {children}
+    </div>
+  );
+};
+
+export default ResumeLayout;
