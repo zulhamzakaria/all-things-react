343bfeea8209ad5dffc75d714a95012c97eedfa8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-20 21:13:34 +0800

    - stylings

--- resume-v2/app/resume/page.tsx
+++ resume-v2/app/resume/page.tsx
@@ -1,7 +1,7 @@
 export default function Resume() {
   return (
     <div className=" flex flex-col items-center justify-between p-24">
-      Resume!
+      Resume! is this the page?
     </div>
   );
 }
