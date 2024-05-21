651bdd7e7ab378d20e013812b908ca1410cfa911
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-21 19:27:11 +0800

    - zzz

--- resume-v2/components/slot-title.tsx
+++ resume-v2/components/slot-title.tsx
@@ -6,13 +6,13 @@ const SlotTitle = ({ title }: { title?: string }) => {
     <>
       <div className=" w-full flex-between font-bold text-slate-950 border-solid border-b-4 border-red-600 mb-2 ">
         <p className="flex items-center gap-1">{title?.toLocaleUpperCase()}</p>
-        <div className=" flex-between gap-5">
+        <div className=" flex-between gap-4">
           <SignedIn>
             <PenIcon
               className=" cursor-pointer"
               onClick={() => alert("click")}
-              width={15}
-              height={15}
+              width={20}
+              height={20}
             />
           </SignedIn>
         </div>
