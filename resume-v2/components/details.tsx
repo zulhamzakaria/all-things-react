274babbe0ac6f694f2da350282c818c74750fc06
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-21 20:13:58 +0800

    - swr

--- resume-v2/components/details.tsx
+++ resume-v2/components/details.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+"use client";
+
+import useSWR from "swr";
+
+interface DetailsProps {
+  name: string;
+  phone: string;
+  email: string;
+  jobtitle: string;
+}
 
 const DetailsPage = () => {
+  const { data } = useSWR("/details");
+
+  if (!data) {
+    console.log("no data");
+  } else {
+    console.log(data);
+  }
+
   return <div>Calls from API!</div>;
 };
 
