f3539c91da8f8d40a259eb658eb791d6ae5de600
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-21 13:35:55 +0800

    - data sheet

---
+++ resume-v2/data.ts
@@ -0,0 +1,164 @@
+export const details = {
+  name: "Mohd Zulham Bin Zakaria",
+  phone: "+60109185300",
+  email: "mohd.zulham.zakaria@gmail.com",
+  jobtitle: "Senior Application Developer",
+};
+
+export const summary = {
+  summary:
+    "Experienced Senior Application Developer wanting to learn more about System Design and Architecture ",
+};
+
+export const skills = [
+  ".net framework",
+  ".net core",
+  "c#",
+  "sql server",
+  "mysql",
+  "vb.net",
+  "propduct development",
+  "product deployment",
+  "web api",
+  "mvc",
+  "next js",
+  "typescript",
+];
+
+export const education = {
+  institution: "Universiti Malaysia Terengganu, Kuala Terengganu",
+  major: "Information Technology, Software Engineering",
+};
+
+export const experiences = [
+  {
+    period: "2009-01 - 2011-09",
+    title: "Application Developer",
+    company: "Edaran IT Services Sdn Bhd, Desa Pandan",
+    responsibilities: [
+      {
+        task: "Worked closely with other team members in such tasks as troubleshooting and debugging.",
+      },
+      {
+        task: "Participated with clients in discussion meetings.",
+      },
+      {
+        task: "Developed and implemented personalized client solutions using programming language expertise.",
+      },
+      {
+        task: "Developed and implemented personalized client solutions using programming language expertise.",
+      },
+      {
+        task: "Worked on Applications: - MARA portal, Reporting module for eFarmasi, ePerhilitan, Sales Tracking System (Internal)",
+      },
+    ],
+  },
+  {
+    period: "2011-10 ~ 2014-09",
+    title: "Application Developer",
+    company: "Strateq Sdn Bhd, Petaling Jaya",
+    responsibilities: [
+      {
+        task: "Worked closely with other team members in such tasks as troubleshooting and debugging.",
+      },
+      {
+        task: "Resolved system test and validation problems to provide normal program functioning.",
+      },
+      {
+        task: "Created software applications for Windows systems.",
+      },
+      {
+        task: "Developed and implemented personalized client solutions using programming language expertise.",
+      },
+      {
+        task: "Used ASP.NET, JavaScript and SQL Server to develop new applications.",
+      },
+      {
+        task: "Collaborated with multidisciplinary teams to design and implement new technology features.",
+      },
+      {
+        task: "Participated with clients in discussion meetings.",
+      },
+      {
+        task: "Worked on Applications: - SIRIM eSCIS",
+      },
+    ],
+  },
+  {
+    period: "2014-10 ~ 2022-02",
+    title: "Senior Application Developer",
+    company: "Strateq Sdn Bhd, Petaling Jaya",
+    responsibilities: [
+      {
+        task: "Managed software development and support using formal specifications, data flow diagrams and other design techniques.",
+      },
+      {
+        task: "Reviewed existing programs to refine, reduce operating time and improve techniques.",
+      },
+      {
+        task: "Analysed and developed technical solutions to meet business requirements.",
+      },
+      {
+        task: "Built databases and table structures for web applications.",
+      },
+      {
+        task: "Implemented SVN and AzureDevOps for version control.",
+      },
+      {
+        task: "Estimated work hours and tracked progress.",
+      },
+      {
+        task: "Contributed ideas and suggestions in team meetings and delivered updates on deadlines, designs and enhancements.",
+      },
+      {
+        task: "Designed, developed, modified and debugged programs.",
+      },
+      {
+        task: "Application setup/deployment to the servers.",
+      },
+      {
+        task: "Worked on Applications: - Various versions of Strateq Procurement Systems (HLB, HLISB, SEH/SEHMY, Petronas and PAMB), Payment file generators (PAMB, HLB, and HLISB), Billing System (Internal), e-HR (Internal), API(Internal), Claim Reports (Internal), SUNS System File Generator.",
+      },
+    ],
+  },
+  {
+    period: "2022-11 ~ 2023-10",
+    title: ".Net Developer (C#)",
+    company: "Pulse Eight Malaysia Sdn Bhd, Petaling Jaya",
+    responsibilities: [
+      {
+        task: "Worked closely with Customer Support team for maintaining in-houses web applications.",
+      },
+      {
+        task: "Attended scrum meeting every day for updates.",
+      },
+      {
+        task: "Used Jira for ticketing.",
+      },
+      {
+        task: "Use GitHub for code source control.",
+      },
+      {
+        task: "Tasked with creation of few proof of concepts application.",
+      },
+      {
+        task: "Develop new features for in-houses web app to enhance the applications capability.",
+      },
+      {
+        task: "Worked on/maintained applications: Product Management System (PMS) and Monitoring System.",
+      },
+      {
+        task: "Developed a wrapper API for Directed Acyclic Graph that’s used by the Engineering personnel.",
+      },
+      {
+        task: "POC: Created API for UserProfile that interacts with the existing PMS MVC application. This is a part of upgrading work for PMS.",
+      },
+      {
+        task: "POC: GUI test automation application with Playwright and Speccflow.",
+      },
+      {
+        task: "POC: Application for testing out RabbitMQ (direct queue) and MongoDB. Both MongoDB and RabbitMQ were running inside Docker containers. ",
+      },
+    ],
+  },
+];
