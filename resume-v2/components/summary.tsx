fcb22930869e0579e35e098d8719fb8549042d20
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-22 20:59:45 +0800

    - swr for SummaryPage

--- resume-v2/components/summary.tsx
+++ resume-v2/components/summary.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import SlotTitle from "./slot-title";
+import useSWR from "swr";
+import LoadingCard from "./loading-card";
+import Card from "./card";
+
+interface SummaryProps {
+  summary: string;
+}
+
+export const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const SummaryPage = () => {
+  const { data, error } = useSWR<SummaryProps>("/summary", fetcher);
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
+  if (!data) {
+    return <LoadingCard />;
+  }
+
   return (
     <div>
       <SlotTitle title="SUMMARY" />
+      <Card>{data.summary}</Card>
     </div>
   );
 };
