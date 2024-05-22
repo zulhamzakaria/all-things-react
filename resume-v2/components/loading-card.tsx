4c1432e98636bec629d525823539a18b9bbc12fa
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-22 15:07:37 +0800

    - loading car component

---
+++ resume-v2/components/loading-card.tsx
@@ -0,0 +1,13 @@
+const LoadingCard = ({ children }: { children: React.ReactNode }) => {
+  const CardStyle = {
+    margin: "10px",
+    boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+    border: "1px solid #ddd",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  return <div style={CardStyle}>{children}</div>;
+};
+
+export default LoadingCard;
