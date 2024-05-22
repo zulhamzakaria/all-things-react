60d2ea8869ea50dc1625c57a2fb1a95ac6f16f27
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-22 15:19:49 +0800

    - zzz

--- resume-v2/components/loading-card.tsx
+++ resume-v2/components/loading-card.tsx
@@ -1,7 +1,6 @@
 const LoadingCard = ({ children }: { children: React.ReactNode }) => {
   const CardStyle = {
     padding: "61px",
-    // margin: "10px",
     boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
     border: "1px solid #ddd",
     display: "flex",
