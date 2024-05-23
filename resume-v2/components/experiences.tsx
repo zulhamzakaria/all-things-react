c1e1080b81c4881e6534ccee94b57caaaae11d7e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-23 19:54:39 +0800

    - swr for experiences

--- resume-v2/components/experiences.tsx
+++ resume-v2/components/experiences.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import SlotTitle from "./slot-title";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const ExperiencesPage = () => {
+  const { data, error } = useSWR("/experiences", fetcher);
+
   return (
     <div>
       <SlotTitle title="Experiences" />
