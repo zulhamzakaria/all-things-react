da0c446d6147db62d2d9344222cf0e04e0cf2a71
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-23 15:58:39 +0800

    - swr fro skills page

--- resume-v2/components/skills.tsx
+++ resume-v2/components/skills.tsx
@@ -1,12 +1,22 @@
+import useSWR from "swr";
 import SlotTitle from "./slot-title";
+import LoadingCard from "./loading-card";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const SkillsPage = () => {
-  return (
+  const { data, error } = useSWR("/skills", fetcher);
+
+  if (error) return <h1>{error}</h1>;
+
+  return data ?(
     <div>
       <SlotTitle title="Skills" />
-      SkillsPage
+      {data.skills}
     </div>
-  );
+  ): (<div>
+    <LoadingCard/>
+  </div>);
 };
 
 export default SkillsPage;
