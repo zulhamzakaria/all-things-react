d7ab120c9702ecaa9b975525393f33b90c983f5b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-23 16:06:59 +0800

    - iteration

--- resume-v2/components/skills.tsx
+++ resume-v2/components/skills.tsx
@@ -1,22 +1,36 @@
 import useSWR from "swr";
 import SlotTitle from "./slot-title";
 import LoadingCard from "./loading-card";
+import { skills } from "@/data";
+
+interface SkillsProps {
+  skills: {
+    id: number;
+    skill: string;
+  }[];
+}
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const SkillsPage = () => {
-  const { data, error } = useSWR("/skills", fetcher);
+  const { data, error } = useSWR<SkillsProps>("/skills", fetcher);
+
+  console.log(data);
 
   if (error) return <h1>{error}</h1>;
 
-  return data ?(
+  return data ? (
     <div>
       <SlotTitle title="Skills" />
-      {data.skills}
+      {skills.map((skill) => (
+        <p key={skill.id}>{skill.skill}</p>
+      ))}
     </div>
-  ): (<div>
-    <LoadingCard/>
-  </div>);
+  ) : (
+    <div>
+      <LoadingCard />
+    </div>
+  );
 };
 
 export default SkillsPage;
