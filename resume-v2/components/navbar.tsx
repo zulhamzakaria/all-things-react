dabc53392a73124b276c2a1342021d6076fdd456
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-26 12:48:39 +0800

    - login button

--- resume-v2/components/navbar.tsx
+++ resume-v2/components/navbar.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import MobileNav from "./mobile-nav";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 export const NavBar = () => {
+  const routeTo = useRouter();
   return (
     <nav className=" flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
@@ -14,12 +18,21 @@ export const NavBar = () => {
           height={32}
           className="max-sm:size-10"
         />
-        <p className=" text-[26px] font-extrabold text-white max-sm:hidden">
+        <p className=" text-[26px] font-extrabold text-white max-sm:hidden font-mono">
           Resume
         </p>
       </Link>
 
       <div className="flex-between gap-5">
+        <SignedOut>
+          <div
+            className="text-white max-sm:hidden font-mono"
+            onClick={() => routeTo.push("/sign-in")}
+          >
+            login
+          </div>
+        </SignedOut>
+
         <SignedIn>
           <UserButton />
         </SignedIn>
