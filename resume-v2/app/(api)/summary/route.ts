9ac385d1b058315e7704e200378321d89cf27b83
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-29 23:33:29 +0800

    - patch endpoint for summary?

--- resume-v2/app/(api)/summary/route.ts
+++ resume-v2/app/(api)/summary/route.ts
@@ -3,3 +3,11 @@ import { summary } from "@/data";
 export async function GET() {
   return Response.json(summary);
 }
+
+export async function PATCH(req: Request) {
+  const body = await req.json();
+  const { userId, summary } = body;
+  const data = summary.find((summary.userId = userId));
+  data.summary = summary;
+  console.log(data.summary);
+}
