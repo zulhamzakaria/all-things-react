71b31d9fe72b2010c939c354dede8e5c2eee80a8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-30 07:25:35 +0800

    - stylings

--- resume-v2/components/skills.tsx
+++ resume-v2/components/skills.tsx
@@ -1,8 +1,9 @@
 import useSWR from "swr";
 import SlotTitle from "./slot-title";
 import LoadingCard from "./loading-card";
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { Button } from "./ui/button";
+import { PencilIcon } from "lucide-react";
 
 interface SkillsProps {
   skills: {
@@ -23,12 +24,30 @@ const SkillsPage = () => {
       <SlotTitle title="Skills" />
       <div>
         {data.skills.map((skill) => (
-          <span
-            key={skill.id}
-            className=" mt-1 bg-slate-200 text-slate-950 w-auto mr-1 rounded-full py-1 px-2 text-sm font-semibold inline-block"
-          >
-            {skill.skill}
-          </span>
+          <>
+            <SignedOut>
+              <span
+                key={skill.id}
+                className=" mt-1 bg-slate-200 text-slate-950 w-auto mr-1 rounded-full py-1 px-2 text-sm font-semibold inline-block"
+              >
+                {skill.skill}
+              </span>
+            </SignedOut>
+            <SignedIn>
+              <span
+                key={skill.id}
+                className=" mt-1 bg-slate-200 text-slate-950 w-auto rounded-l-full py-1 px-2 text-sm font-semibold inline-block"
+              >
+                {skill.skill}
+              </span>
+              <Button className=" bg-blue-400 h-7 w-1 rounded-none text-white">
+                e
+              </Button>
+              <Button className=" bg-red-600 text-white rounded-r-full h-7 w-1 mr-2">
+                x
+              </Button>
+            </SignedIn>
+          </>
         ))}
       </div>
     </div>
