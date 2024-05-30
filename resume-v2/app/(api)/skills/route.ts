09ab1b3b205e98e7fdfa82ee360d8a1bbd00abe4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-30 21:03:05 +0800

    - skill POST endpoint

--- resume-v2/app/(api)/skills/route.ts
+++ resume-v2/app/(api)/skills/route.ts
@@ -1,5 +1,20 @@
 import { skills } from "@/data";
+import { headers } from "next/headers";
+import { json } from "stream/consumers";
 
 export async function GET() {
   return Response.json(skills);
+}
+
+export async function POST(req: Request) {
+  const skill = await req.json();
+  const newSkill = {
+    id: skills.skills.length + 1,
+    skill: skill,
+  };
+  skills.skills.push(newSkill);
+  return new Response(JSON.stringify(skills.skills), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  });
 }
