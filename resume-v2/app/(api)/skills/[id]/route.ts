01a2c75e44e63ba386cc3b742824cfb5e364e57e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-01 08:53:33 +0800

    - Skill Patch endpoint

--- resume-v2/app/(api)/skills/[id]/route.ts
+++ resume-v2/app/(api)/skills/[id]/route.ts
@@ -4,6 +4,16 @@ export async function GET(
   _req: Request,
   { params }: { params: { id: string } }
 ) {
-  const skill = skills.find((skill) => skill.id === parseInt(params.id));
+  const skill = skills.skills.find((skill) => skill.id === parseInt(params.id));
   return Response.json(skill);
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: number } }
+) {
+  const { skill } = await req.json();
+  const index = skills.skills.findIndex((skill) => skill.id === params.id);
+  skills.skills[index].skill = skill;
+  return Response.json(skills.skills[index]);
 }
