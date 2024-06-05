11d23877a9b1daf5f6a6965c3801b5b80fc9ae41
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-05 19:55:44 +0800

    - experience update endpoint

--- resume-v2/app/(api)/experiences/[id]/route.ts
+++ resume-v2/app/(api)/experiences/[id]/route.ts
@@ -1,11 +1,23 @@
-import { experiences } from "@/data";
+import { experiences as exps } from "@/data";
 
 export async function GET(
   _req: Request,
   { params }: { params: { id: string } }
 ) {
-  const experience = experiences.experiences.find(
+  const experience = exps.experiences.find(
     (exp) => exp.id === parseInt(params.id)
   );
   return Response.json(experience);
+}
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { exp: updatedExp } = await req.json();
+  const index = exps.experiences.findIndex(
+    (exp) => exp.id === parseInt(params.id)
+  );
+  exps.experiences[index] = updatedExp;
+  return Response.json(exps.experiences);
 }
