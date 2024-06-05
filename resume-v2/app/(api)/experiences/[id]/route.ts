ea47c45f5f1c4462fdeadd2437c5cbaf2037c7ce
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-05 20:25:24 +0800

    -dont need to deconstruct the body?

--- resume-v2/app/(api)/experiences/[id]/route.ts
+++ resume-v2/app/(api)/experiences/[id]/route.ts
@@ -14,10 +14,11 @@ export async function PUT(
   req: Request,
   { params }: { params: { id: string } }
 ) {
-  const { exp: updatedExp } = await req.json();
+  const updatedExp = await req.json();
   const index = exps.experiences.findIndex(
     (exp) => exp.id === parseInt(params.id)
   );
+  console.log(updatedExp);
   exps.experiences[index] = { ...exps.experiences[index], ...updatedExp };
-  return Response.json(exps.experiences);
+  return Response.json(exps.experiences[index]);
 }
