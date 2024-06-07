bc12eb61ca8bd6739bd9d7e837d9162761be4eaf
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-07 20:25:55 +0800

    - exps POST endpoint

--- resume-v2/app/(api)/experiences/route.ts
+++ resume-v2/app/(api)/experiences/route.ts
@@ -1,5 +1,18 @@
-import { experiences } from "@/data";
+import { experiences as exps } from "@/data";
 
 export async function GET() {
-  return Response.json(experiences);
+  return Response.json(exps);
+}
+
+export async function POST(req: Request) {
+  const { exp } = await req.json();
+
+  const newId = exps.experiences.length + 1;
+  const newExp = { id: newId, ...exp };
+  exps.experiences.push(newExp);
+
+  return new Response(JSON.stringify(exps.experiences), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  });
 }
