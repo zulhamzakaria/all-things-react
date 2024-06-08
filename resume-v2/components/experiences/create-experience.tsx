f18e37a4d31939af5e1189071293ecb2673361c9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-08 20:29:29 +0800

    - stuffs

--- resume-v2/components/experiences/create-experience.tsx
+++ resume-v2/components/experiences/create-experience.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const CreateExperience = () => {
-  return <div>basic-create-experience</div>;
+  const { data, error } = useSWR(`/experiences`, fetcher);
+
+  if (error) return <div>error fetching data</div>;
+
+  return data ? <div>basic-create-experience</div> : "loading...";
 };
 
 export default CreateExperience;
