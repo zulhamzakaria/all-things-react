867a904b18b7c22cd38496c10ad6c43bc43fe1cb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-08 20:48:56 +0800

    - debugging - added props and assign that as type to the useState since cant directly access the data content

--- resume-v2/components/experiences/create-experience.tsx
+++ resume-v2/components/experiences/create-experience.tsx
@@ -2,11 +2,23 @@ import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
+interface ExperiencesProps {
+  experiences: {
+    id: number;
+    period: string;
+    title: string;
+    company: string;
+    responsibilities: {
+      task: string;
+    }[];
+  }[];
+}
 
 const CreateExperience = () => {
   const { data, error } = useSWR(`/experiences`, fetcher);
 
-  const [experiences, setExperiences] = useState([]);
+  const [experiences, setExperiences] =
+    useState<ExperiencesProps["experiences"]>(data);
   if (error) return <div>error fetching data</div>;
 
   useEffect(() => {
