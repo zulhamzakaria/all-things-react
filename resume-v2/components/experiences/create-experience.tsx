1c21404a8ddfd849f260a6a0237368a82f7a307e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-08 20:33:55 +0800

    - useEffect, useState

--- resume-v2/components/experiences/create-experience.tsx
+++ resume-v2/components/experiences/create-experience.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
@@ -6,9 +6,20 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const CreateExperience = () => {
   const { data, error } = useSWR(`/experiences`, fetcher);
 
+  const [experiences, setExperiences] = useState([]);
   if (error) return <div>error fetching data</div>;
 
-  return data ? <div>basic-create-experience</div> : "loading...";
+  useEffect(() => {
+    if (data) {
+      setExperiences(data);
+    }
+  }, [data]);
+
+  return data ? (
+    <div>basic-create-experience {experiences.length}</div>
+  ) : (
+    "loading..."
+  );
 };
 
 export default CreateExperience;
