435f0de464b46bf95ead2e7fc0655fa3dfa46381
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-09 14:43:27 +0800

    - add experience basic form

--- resume-v2/components/experiences/create-experience.tsx
+++ resume-v2/components/experiences/create-experience.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 import { Button } from "../ui/button";
 import { toast } from "sonner";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { PlusIcon, XIcon } from "lucide-react";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 interface ExperiencesProps {
@@ -16,9 +19,17 @@ interface ExperiencesProps {
   }[];
 }
 
+interface Task {
+  task: string;
+}
+
 const CreateExperience = () => {
   const { data, mutate, error } = useSWR(`/experiences`, fetcher);
 
+  const [company, setCompany] = useState("");
+  const [title, setTitle] = useState("");
+  const [period, setPeriod] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [experiences, setExperiences] = useState<
     ExperiencesProps["experiences"]
   >([]);
@@ -66,9 +77,92 @@ const CreateExperience = () => {
   }
 
   return experiences ? (
-    <div>
-      <Button onClick={handleAddExperience}>add exp</Button>
-    </div>
+    <>
+      <div className=" flex flex-col">
+        <div className=" grid grid-cols-4 gap-4 mb-4 ">
+          <div className=" col-span-3 ">
+            <Label htmlFor="company" className=" font-sans mb-2">
+              Company
+            </Label>
+            <Input
+              id="company"
+              value={company}
+              onChange={(e) => {
+                setCompany(e.target.value);
+              }}
+            />
+          </div>
+          <div className=" col-span-1 ">
+            <Label htmlFor="period" className=" font-sans mb-2">
+              Period
+            </Label>
+            <Input
+              id="period"
+              value={period}
+              onChange={(e) => {
+                setPeriod(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div className=" mb-4">
+          <div className=" flex flex-col">
+            <Label htmlFor="title" className=" font-sans mb-2">
+              Title
+            </Label>
+            <Input
+              id="title"
+              className="w-full"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div>
+          <div className=" flex flex-col">
+            <Label className=" font-sans mb-2">Responsibilities</Label>
+          </div>
+        </div>
+        <div className=" flex flex-col">
+          {tasks.map((task, index) => (
+            <div key={index}>
+              <div className=" flex flex-row mb-1">
+                <div className=" flex items-center justify-center rounded-l-lg bg-slate-800 text-white w-10">
+                  <Label htmlFor={`task-${index}`} className=" font-sans">
+                    {index + 1}.
+                  </Label>
+                </div>
+                <Input
+                  id={`task-${index}`}
+                  value={task.task}
+                  onChange={(e) => {}}
+                  className=" rounded-r-none"
+                />
+                <Button
+                  type="button"
+                  onClick={() => {}}
+                  className=" bg-rose-500 text-white rounded-l-none"
+                >
+                  <XIcon />
+                </Button>
+              </div>
+            </div>
+          ))}
+          <div className=" w-full flex justify-center ">
+            <Button
+              type="button"
+              onClick={()=>{}}
+              className=" items-center my-2 text-gray-950 inline-flex hover:bg-emerald-500 hover:text-white font-mono font-semibold"
+            >
+              <PlusIcon className="mr-2" />
+              add responsibility
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
   ) : (
     "loading..."
   );
