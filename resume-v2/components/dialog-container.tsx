73a4d8c6ae907f98e0df4d7813ee13047396d2dc
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-09 19:56:22 +0800

    - reusable dialog container

--- resume-v2/components/dialog-container.tsx
+++ resume-v2/components/dialog-container.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Separator } from "./ui/separator";
 
 interface DialogContainerProps {
   children: [
@@ -9,7 +20,28 @@ interface DialogContainerProps {
 }
 
 const DialogContainer = ({ children }: DialogContainerProps) => {
-  return <div></div>;
+  const [buttonChild, pageChild] = React.Children.toArray(children);
+
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>{buttonChild}</DialogTrigger>
+        <DialogContent className=" bg-amber-50">
+          <DialogHeader>
+            <DialogTitle></DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <Separator />
+          <div>{pageChild}</div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button>save</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
 
 export default DialogContainer;
