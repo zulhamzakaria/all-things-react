825f75b630548167be7612b15c92ce86d98c57d0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-11 16:08:51 +0800

    WIP

--- resume-v2/components/educations/create-education.tsx
+++ resume-v2/components/educations/create-education.tsx
@@ -1,6 +1,18 @@
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { EducationSchema } from "@/schemas/education";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormItem,
+  FormLabel,
+  FormField,
+  FormControl,
+  FormMessage,
+  FormDescription,
+} from "@/components/ui/form";
+import { useState } from "react";
+import { educations } from "@/data";
 
 interface CreateEducationProps {
   institution: string;
@@ -8,6 +20,18 @@ interface CreateEducationProps {
 }
 
 const CreateEducation = () => {
+  const [createEducation, setCreateEducation] = useState<
+    CreateEducationProps[]
+  >([]);
+
+  const form = useForm<z.infer<typeof EducationSchema>>({
+    resolver: zodResolver(EducationSchema),
+  });
+
+  const handleAddEducation = () => {
+    setCreateEducation([...createEducation, { institution: "", major: "" }]);
+  };
+
   return <div>Create Education page!</div>;
 };
 
