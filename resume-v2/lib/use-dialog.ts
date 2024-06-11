e9af814439851fac2f2b61e0c76119d37c0bbfba
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-11 22:24:12 +0800

    - use dialog with id

--- resume-v2/lib/use-dialog.ts
+++ resume-v2/lib/use-dialog.ts
@@ -1,17 +1,45 @@
 import { create } from "zustand";
 
-interface DialogProps {
+interface DialogState {
   isOpen: boolean;
-  onOpen: () => void;
-  onClose: () => void;
-  data: any;
-  setData(data: any): void;
 }
 
-export const useDialog = create<DialogProps>((set) => ({
-  isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
-  data: {},
-  setData: (data) => set({ data: { data } }),
+interface DialogStore {
+  dialogs: Record<string, DialogState>;
+  onOpen: (id: string) => void;
+  onClose: (id: string) => void;
+}
+
+export const useDialog = create<DialogStore>((set) => ({
+  dialogs: {},
+  onOpen: (id) =>
+    set((state) => ({
+      dialogs: {
+        ...state.dialogs,
+        [id]: { ...(state.dialogs[id] || {}), isOpen: true },
+      },
+    })),
+  onClose: (id) =>
+    set((state) => ({
+      dialogs: {
+        ...state.dialogs,
+        [id]: { ...(state.dialogs[id] || {}), isOpen: false },
+      },
+    })),
 }));
+
+// interface DialogProps {
+//   isOpen: boolean;
+//   onOpen: () => void;
+//   onClose: () => void;
+//   data: any;
+//   setData(data: any): void;
+// }
+
+// export const useDialog = create<DialogProps>((set) => ({
+//   isOpen: false,
+//   onOpen: () => set({ isOpen: true }),
+//   onClose: () => set({ isOpen: false }),
+//   data: {},
+//   setData: (data) => set({ data: { data } }),
+// }));
