af771555ef1667ff295c32d165fe58c6521f6129
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-13 13:35:18 +0800

    - debugging

--- resume-v2/app/(api)/education/route.tsx
+++ resume-v2/app/(api)/education/route.tsx
@@ -8,8 +8,7 @@ export async function GET() {
 
 export async function POST(req: Request) {
   const educationsArray = await req.json();
-
-  educationsArray.array.forEach((education: any) => {
+  educationsArray.forEach((education: any) => {
     const newId = educations.length + 1;
     const newEdu = { id: newId, ...education };
     educations.push(newEdu);
