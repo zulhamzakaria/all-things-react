8162c99a28fe84a776fd4825d5c1ace2868f7adc
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-13 20:16:16 +0800

    - Educaiotn DELETE endpoint

--- resume-v2/app/(api)/education/[id]/route.ts
+++ resume-v2/app/(api)/education/[id]/route.ts
@@ -7,3 +7,14 @@ export async function GET(
   const education = educations.find((edu) => edu.id === parseInt(params.id));
   return Response.json(education);
 }
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { id: string } }
+) {
+  const index = educations.findIndex((edu) => edu.id === parseInt(params.id));
+  if (index !== -1) {
+    educations.splice(index, 1);
+  }
+  return Response.json(educations);
+}
