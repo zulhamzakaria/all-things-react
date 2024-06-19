5d74707125fda6bc7fd0f6924aabddab47cd7e50
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-19 21:14:49 +0800

    - WIP

--- resume-v2/components/educations/edit-education.tsx
+++ resume-v2/components/educations/edit-education.tsx
@@ -1,6 +1,36 @@
+import { useDialog } from "@/lib/use-dialog";
+import { EducationSchema } from "@/schemas/education";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+import useSWR, { mutate } from "swr";
+import { z } from "zod";
+
+interface EditEducationProps {
+  institution: string;
+  major: string;
+}
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const EditEducation = () => {
+  const { data } = useSWR("/education", fetcher);
+  const { onClose } = useDialog();
+
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<z.infer<typeof EducationSchema>>({
+    resolver: zodResolver(EducationSchema),
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "educations",
+  });
+
   return <h1>Edit Education</h1>;
 };
 
