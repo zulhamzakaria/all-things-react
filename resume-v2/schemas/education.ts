35b6272e263ccb930a7fd4a990351c0ffe02c786
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-23 00:05:38 +0800

    - updated schema

--- resume-v2/schemas/education.ts
+++ resume-v2/schemas/education.ts
@@ -1,10 +1,16 @@
 import { z } from "zod";
 
 const EducationEntrySchema = z.object({
-  institution: z.string().min(1, { message: "Institution is required." }),
+  institution: z.string().min(1, { message: "Institution is required" }),
   major: z.string().optional(),
 });
 
 export const EducationSchema = z.object({
   educations: z.array(EducationEntrySchema),
+});
+
+export const EditEducationScheme = z.object({
+  id: z.number(),
+  institution: z.string().min(1, { message: "Institution is required" }),
+  major: z.string().optional(),
 });
