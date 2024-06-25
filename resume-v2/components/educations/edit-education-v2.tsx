2f869412fc911f004ac7371056b4baaea89d2f65
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-26 00:53:12 +0800

    - basic form +  zod  handleSubmit

--- resume-v2/components/educations/edit-education-v2.tsx
+++ resume-v2/components/educations/edit-education-v2.tsx
@@ -1,7 +1,27 @@
+import { EditEducationSchema, EducationSchema } from "@/schemas/education";
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "../ui/button";
 
 const EditEducationV2 = () => {
-  return <div>EditEducationV2</div>;
+  const { handleSubmit } = useForm<z.infer<typeof EditEducationSchema>>({
+    resolver: zodResolver(EditEducationSchema),
+  });
+
+  const onSubmit = async (values: z.infer<typeof EditEducationSchema>) => {
+    alert("button clicked");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        EditEducationV2
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
 };
 
 export default EditEducationV2;
