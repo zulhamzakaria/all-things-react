fd461640cdccf626da273213871a796aa9d8079e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-29 20:35:17 +0800

    - wip

--- resume-v2/app/(api)/summary/route.ts
+++ resume-v2/app/(api)/summary/route.ts
@@ -1,24 +1,8 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { getHandler } from "./get";
-import { useUser } from "@clerk/nextjs";
+import { summary } from "@/data";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { user } = useUser();
-  const url = process.env.API_URL;
-  try {
-    switch (req.method) {
-      case "GET":
-        var result = getHandler(user!.id, url!);
-        return res.status(200).json(result);
-    }
-  } catch (e) {
-    res.status(500).json("Error!");
-  }
+export async function GET() {
+  return Response.json(summary);
 }
-
-// export async function GET() {
-//   return Response.json(summary);
-// }
 
 export async function PATCH(req: Request) {
   const body = await req.json();
