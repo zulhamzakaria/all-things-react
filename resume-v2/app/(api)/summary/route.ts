cfc2653b22eaa66ab45f62d1fbb6343e0ee7d0ff
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-06-29 21:35:08 +0800

    - ebugging

--- resume-v2/app/(api)/summary/route.ts
+++ resume-v2/app/(api)/summary/route.ts
@@ -1,16 +1,16 @@
 import { summary } from "@/data";
-import { NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 // https://online-resume-with-minimal-api.azurewebsites.net/api/summaries/user01
 const url = `${process.env.API_URL}/summaries`;
 
-export async function GET(res: NextApiResponse) {
+export async function GET(_req: NextApiRequest) {
   const fullUrl = `${url}/user01`;
 
   const response = await fetch(fullUrl);
   const result = await response.json();
 
-  return res.status(200).json({ userId: "user01", description: "string" });
+  return Response.json(summary);
 
   // if (result) {
   //return Response.json(result);
