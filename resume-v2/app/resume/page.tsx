58176265fe84eb63672c3b1865a72f9f20c7f76e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-01 20:51:45 +0800

    - debugging

--- resume-v2/app/resume/page.tsx
+++ resume-v2/app/resume/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Resume() {
+  const params = useSearchParams();
+  const query = params.get("userId");
+
+  useEffect(() => {
+    alert(JSON.stringify(query));
+  }, [query]);
+
   return (
     <div className=" flex flex-col items-center justify-between p-24">
       Resume! is this the page?
