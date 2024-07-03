eb35bdf127a1be0387c33864d57b0be079487822
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 16:40:07 +0800

    - extends Models.Document if u want to access metadata (interface todo extends Models.Document) - reduce() : for sorting? - whats happennniinggg

--- trello-with-ai/lib/getTodosGroupedByColumn.ts
+++ trello-with-ai/lib/getTodosGroupedByColumn.ts
@@ -5,5 +5,28 @@ export const getTodosGroupedByColumn = async () => {
     process.env.NEXT_PUBLIC_DATABASE_ID!,
     process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
   );
-  console.log(data);
+
+  const todos = data.documents;
+
+  const solumns = todos.reduce((acc, todo) => {
+    if (!acc.get(todo.status)) {
+      acc.set(todo.status, {
+        id: todo.status,
+        todos: [],
+      });
+    }
+
+    acc.get(todo.status)!.todos.push({
+      $id: todo.$id,
+      $createdAt: todo.$createdAt,
+      title: todo.title,
+      status: todo.status,
+      // set the image value basd on the returned data
+      // use spread to update the value
+      // data is stored as JSON object
+      ...(todo.image && { image: JSON.parse(todo.image) }),
+    });
+
+    return acc;
+  }, new Map<TypedColumn, Column>());
 };
