4c56e5fcfd37c7afd72f76bb45e89e63fe400f5e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 13:33:38 +0800

    - fleshin out header - object-contain: image doesnt stretch

--- trello-with-ai/components/header.tsx
+++ trello-with-ai/components/header.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
-  return <div>header component</div>;
+  return (
+    <div>
+      <Image
+        src="https://links.papareact.com/c2cdd5"
+        alt="logo"
+        width={300}
+        height={100}
+        className="w-44 md:w-56 pb-10 md:pd-0 object-contain"
+      />
+    </div>
+  );
 };
 
 export default Header;
