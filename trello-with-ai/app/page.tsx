b58e0ebd767b60726578bd1a8945d1b12fa0f31c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 13:23:03 +0800

    - basic header component - inport header from app page.tsx

--- trello-with-ai/app/page.tsx
+++ trello-with-ai/app/page.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/header";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Header />
       <h1>Trello with AI</h1>
     </main>
   );
