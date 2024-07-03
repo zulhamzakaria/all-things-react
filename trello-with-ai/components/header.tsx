00ddfd8d5339935600143507cf38b6aea4ec5d8f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 14:16:16 +0800

    - header (cont...) - flex-1: allows for the div to take the whole remaining space

--- trello-with-ai/components/header.tsx
+++ trello-with-ai/components/header.tsx
@@ -1,5 +1,8 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+"use client";
+
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import Avatar from "react-avatar";
 
 const Header = () => {
   return (
@@ -13,7 +16,7 @@ const Header = () => {
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
         />
 
-        <div className=" flex ">
+        <div className=" flex items-center space-x-5 flex-1 justify-end w-full">
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial ">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
             <input
@@ -25,7 +28,15 @@ const Header = () => {
               Search
             </button>
           </form>
+
+          <Avatar name="avatar" round size="50" />
         </div>
+      </div>
+      <div className="flex items-center justify-center px-5 md:py-5">
+        <p className="flex items-center text-sm font-light p-5 pr-5 shadow-xl rounded-xlw-fit bg-white italic max-w-3xl ">
+          <UserCircleIcon className="inline-block h-10 w-10 mr-1" />
+          GPT is summarising...
+        </p>
       </div>
     </header>
   );
