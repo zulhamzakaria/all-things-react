e24396e66ededa791476cf6c76453583897150ec
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 13:39:51 +0800

    - resized logo

--- trello-with-ai/components/header.tsx
+++ trello-with-ai/components/header.tsx
@@ -7,8 +7,8 @@ const Header = () => {
       <Image
         src="https://links.papareact.com/c2cdd5"
         alt="logo"
-        width={300}
-        height={100}
+        width={150}
+        height={50}
         className="w-44 md:w-56 pb-10 md:pd-0 object-contain"
       />
     </div>
