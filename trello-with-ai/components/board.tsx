322706b706819339cc0d4dd3d67f67015bc05968
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 22:24:39 +0800

    - setBoardState implementation - dragging task between columns/ inside same column

--- trello-with-ai/components/board.tsx
+++ trello-with-ai/components/board.tsx
@@ -6,9 +6,10 @@ import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
 import Column from "./column";
 
 const Board = () => {
-  const [board, getBoard] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState] = useBoardStore((state) => [
     state.board,
     state.getBoard,
+    state.setBoardState,
   ]);
 
   useEffect(() => {
@@ -24,6 +25,64 @@ const Board = () => {
       const [removed] = entries.splice(source.index, 1);
       entries.splice(destination.index, 0, removed);
       const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    }
+
+    // for getting the items movement between columns
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const endColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+    const endCol: Column = {
+      id: endColIndex[0],
+      todos: endColIndex[1].todos,
+    };
+
+    // if the item is dragged and dropped inside the same column, do nothing
+    if (!startCol || !endCol) return;
+
+    // if the item is dragged and drop at the same position, do nothing
+    if (source.index === destination.index && startCol === endCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === endCol.id) {
+      // same col task drag
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      // dragging task to another col
+      const endTodos = Array.from(endCol.todos);
+      endTodos.splice(destination.index, 0, todoMoved);
+
+      const newCols = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newCols.set(startCol.id, newCol);
+      newCols.set(endCol.id, {
+        id: endCol.id,
+        todos: endTodos,
+      });
+
+      setBoardState({ ...board, columns: newCols });
     }
   };
 
