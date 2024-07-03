850fea38de1a32853c825fd0c044363937a97e8d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 22:29:46 +0800

    - function for updating Todo stae to db

--- trello-with-ai/store/board-store.ts
+++ trello-with-ai/store/board-store.ts
@@ -1,3 +1,4 @@
+import { databases } from "@/appwrite";
 import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn";
 import { create } from "zustand";
 
@@ -5,6 +6,7 @@ interface BoardState {
   board: Board;
   getBoard: () => void;
   setBoardState: (board: Board) => void;
+  updateTodoInDb: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -16,4 +18,15 @@ export const useBoardStore = create<BoardState>((set) => ({
     set({ board });
   },
   setBoardState: (board) => set({ board }),
+  async updateTodoInDb(todo, columnId) {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
+  },
 }));
