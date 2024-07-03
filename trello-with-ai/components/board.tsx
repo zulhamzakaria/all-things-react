cae139846b6865512726aa320ee6d9452d0ef960
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 15:55:54 +0800

    - board.tsx

--- trello-with-ai/components/board.tsx
+++ trello-with-ai/components/board.tsx
@@ -1,19 +1,16 @@
+import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
+  useEffect(() => {
+    // get boards
+  }, []);
+
   return (
     <div>
       <DragDropContext>
-        <Droppable
-          droppableId="board"
-          direction="horizontal"
-          type="column"
-        >
-            {(provided)=>(
-                <div>
-                    render column here
-                </div>
-            )}
+        <Droppable droppableId="board" direction="horizontal" type="column">
+          {(provided) => <div>render column here</div>}
         </Droppable>
       </DragDropContext>
     </div>
