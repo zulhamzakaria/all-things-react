1e3f5f80af467e6b0d384cdb4224c27b651d9766
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 13:59:40 +0800

    - header (cont...) - stylings for page.tsx - uncommented globals.css import - dont need flex-row for lining up controls? - space-x-5 - flex-1 : child should take the whole width - is button type submit only for form? - bg-gray-500: bbg grey of 10% opacity

--- trello-with-ai/app/page.tsx
+++ trello-with-ai/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <Header />
       <h1>Trello with AI</h1>
     </main>
--- trello-with-ai/components/header.tsx
+++ trello-with-ai/components/header.tsx
@@ -1,21 +1,31 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
-import React from "react";
 
 const Header = () => {
   return (
     <header>
-      <Image
-        src="https://links.papareact.com/c2cdd5"
-        alt="logo"
-        width={150}
-        height={50}
-        className="w-44 md:w-56 pb-10 md:pd-0 object-contain"
-      />
-      <div>
-        <form action="">
-            <input type="text"/>
-            <button hidden>Search</button>
-        </form>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10">
+        <Image
+          src="https://links.papareact.com/c2cdd5"
+          alt="logo"
+          width={300}
+          height={100}
+          className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+        />
+
+        <div className=" flex ">
+          <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial ">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="p-1 flex-1 outline-none"
+            />
+            <button type="submit" hidden>
+              Search
+            </button>
+          </form>
+        </div>
       </div>
     </header>
   );
