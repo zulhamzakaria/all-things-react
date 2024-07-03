5372b9b8397cca8966d2d80342975d825ed8fb8e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 16:21:30 +0800

    - expord useBoardStore - calls useBoardStore from board component

--- trello-with-ai/components/board.tsx
+++ trello-with-ai/components/board.tsx
@@ -1,18 +1,22 @@
+import { useBoardStore } from "@/store/board-store";
 import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
+  const getBoard = useBoardStore((state) => state.getBoard);
+
   useEffect(() => {
     // get boards
-  }, []);
+    getBoard();
+  }, [getBoard]);
 
   return (
     <div>
-      <DragDropContext>
+      {/* <DragDropContext>
         <Droppable droppableId="board" direction="horizontal" type="column">
           {(provided) => <div>render column here</div>}
         </Droppable>
-      </DragDropContext>
+      </DragDropContext> */}
     </div>
   );
 };
--- trello-with-ai/store/board-store.ts
+++ trello-with-ai/store/board-store.ts
@@ -5,7 +5,7 @@ interface BoardState {
   getBoard: () => void;
 }
 
-const useBoardStore = create<BoardState>((set) => ({
+export const useBoardStore = create<BoardState>((set) => ({
   board: {
     columns: new Map<TypedColumn, Column>(),
   },
