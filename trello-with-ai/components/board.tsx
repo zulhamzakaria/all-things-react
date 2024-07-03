2e2a459c55fd33ede4a315e0d5dcf5ab2fdec41b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 18:04:41 +0800

    - function for dragging boards

--- trello-with-ai/components/board.tsx
+++ trello-with-ai/components/board.tsx
@@ -2,7 +2,7 @@
 
 import { useBoardStore } from "@/store/board-store";
 import { useEffect } from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
 
 const Board = () => {
   const [board, getBoard] = useBoardStore((state) => [
@@ -15,13 +15,36 @@ const Board = () => {
     getBoard();
   }, [getBoard]);
 
+  const handleOnDragEnd = (result:DropResult)=>{
+
+  }
+
   return (
     <div>
-      {/* <DragDropContext>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId="board" direction="horizontal" type="column">
-          {(provided) => <div>render column here</div>}
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+            >
+              {Array.from(board.columns.entries()).map(
+                ([id, column], index) => (
+                  <div>
+                    <Column
+                      key={id}
+                      id={id}
+                      todos={column.todos}
+                      index={index}
+                    />
+                  </div>
+                )
+              )}
+            </div>
+          )}
         </Droppable>
-      </DragDropContext> */}
+      </DragDropContext>
     </div>
   );
 };
