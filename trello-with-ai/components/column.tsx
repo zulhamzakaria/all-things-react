77aaca8805b3875a27070f296d0b7f5d5a913857
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 21:27:45 +0800

    - added button - provided.placeholder for creating space between columns during dragging

--- trello-with-ai/components/column.tsx
+++ trello-with-ai/components/column.tsx
@@ -1,3 +1,4 @@
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import React from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 
@@ -62,6 +63,13 @@ const Column = ({ id, todos, index }: ColumnProps) => {
                       </Draggable>
                     </div>
                   ))}
+                  {/* create space between the columns during dragging */}
+                  {provided.placeholder}
+                  <div className="flex items-end justify-end p-2">
+                    <button className="text-green-500 hover:text-green-600">
+                      <PlusCircleIcon className="h-10 w-10" />
+                    </button>
+                  </div>
                 </div>
               </div>
             )}
