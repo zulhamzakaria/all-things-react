2dc1f1df6a3f8666e592e45154592f195f101af3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 15:33:53 +0800

    - dragdropcontext for board

--- trello-with-ai/components/board.tsx
+++ trello-with-ai/components/board.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
-  return <div>Board</div>;
+  return (
+    <div>
+      <DragDropContext>
+        <Droppable
+          droppableId="board"
+          direction="horizontal"
+          type="column"
+        >
+            {(provided)=>(
+                <div>
+                    render column here
+                </div>
+            )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 };
 
 export default Board;
