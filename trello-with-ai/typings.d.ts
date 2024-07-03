fdf0e319869f07395c44c3a7f261cb215d90dc20
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 16:09:10 +0800

    - file for types and interfaces

---
+++ trello-with-ai/typings.d.ts
@@ -0,0 +1,24 @@
+interface Board {
+  columns: Map<TypedColumn, Column>;
+}
+
+type TypedColumn = "todo" | "in-progress" | "done";
+
+interface Column {
+  id: TypedColumn;
+  todos: Todo[];
+}
+
+// values gotten from AppWrite
+interface Todo {
+  $id: string;
+  $createdAt: string;
+  title: string;
+  status: TypedColumn;
+  image?: string;
+}
+
+interface Image {
+  bucketId: string;
+  fileId: string;
+}
