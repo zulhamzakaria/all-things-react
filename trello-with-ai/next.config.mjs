cf322c925bfce0d3467384f65444bb99fcd74f87
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-03 13:38:50 +0800

    - replaced domains with remotePtterns that accept an array of objects

--- trello-with-ai/next.config.mjs
+++ trello-with-ai/next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["links.papareact.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "links.papareact.com",
+      },
+    ],
   },
 };
 
