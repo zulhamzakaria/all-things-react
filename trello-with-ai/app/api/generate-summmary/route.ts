b3669f7b64e005f4b6dcf6b7e8161261e679d565
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-04 14:06:35 +0800

    - WIP

--- trello-with-ai/app/api/generate-summmary/route.ts
+++ trello-with-ai/app/api/generate-summmary/route.ts
@@ -1,3 +1,8 @@
+import openai from "@/openai";
+
 export async function POST(req: Request) {
   const {todos} = await req.json();
+
+
+
 }
--- trello-with-ai/openai.ts
+++ trello-with-ai/openai.ts
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
 
-const config = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-export default config;
+export default openai;
