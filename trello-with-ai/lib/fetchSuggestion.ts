94f6a85d548b7c46e4079f839340617af585cd32
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-04 14:39:38 +0800

    - fetchSuggestion helper method

---
+++ trello-with-ai/lib/fetchSuggestion.ts
@@ -0,0 +1,16 @@
+import { POST } from "@/app/api/generate-summmary/route";
+
+const fetchSuggestion = async (board: Board) => {
+  const todos = formatTodosForAI(board);
+  const res = await fetch("api/generate-summary", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ todos }),
+  });
+
+  const GPTData = await res.json();
+  const { content } = GPTData;
+  return content;
+};
+
+export default fetchSuggestion;
