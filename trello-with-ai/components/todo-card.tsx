cd5dd038e131d58db95b67192b2696f8b3e58d3b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-04 16:00:18 +0800

    - call deleteTask from todo-card

--- trello-with-ai/components/todo-card.tsx
+++ trello-with-ai/components/todo-card.tsx
@@ -1,3 +1,4 @@
+import { useBoardStore } from "@/store/board-store";
 import { XCircleIcon } from "@heroicons/react/24/solid";
 import {
   DraggableProvidedDraggableProps,
@@ -21,6 +22,10 @@ const TodoCard = ({
   draggableProps,
   dragHandleProps,
 }: TodoCardProps) => {
+
+  // no array since it's only one element
+  const deleteTask = useBoardStore((state) => state.deleteTask);
+
   return (
     <div
       className="bg-white rounded-md space-y-2 drop-shadow-md"
@@ -30,11 +35,13 @@ const TodoCard = ({
     >
       <div className="flex justify-between items-center p-3">
         <p>{todo.title}</p>
-        <button className=" text-red-500 hover:text-red-600">
+        <button
+          onClick={() => deleteTask(index, todo, id)}
+          className=" text-red-500 hover:text-red-600"
+        >
           <XCircleIcon className="ml-5 h-8 w-8" />
         </button>
       </div>
-      
     </div>
   );
 };
