b60bf1560840bac83d6fe2c7ac2ebd0894230408
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-04 20:29:53 +0800

    - wip

--- trello-with-ai/components/task-type-radio-group.tsx
+++ trello-with-ai/components/task-type-radio-group.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useBoardStore } from "@/store/board-store";
+import { Radio, RadioGroup } from "@headlessui/react";
 
 const types = [
   {
@@ -24,10 +25,35 @@ const types = [
 ];
 
 const TaskTypeRadioGroup = () => {
+  const { newTaskType, setNewTaskType } = useBoardStore();
 
-    const {newTaskType, setNewTaskType} = useBoardStore()
-
-  return <div>TaskTypeRadioGroup</div>;
+  return (
+    <div className="w-full py-5">
+      <div className="mx-auto w-full max-w-d">
+        <RadioGroup value={newTaskType} onChange={(e) => setNewTaskType(e)}>
+          <div className="space-y-2">
+            {types.map((type) => (
+              <Radio
+                key={type.id}
+                value={type.id}
+                className={({ active, checked }) =>
+                  `${
+                    active
+                      ? "ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300"
+                      : ""
+                  }${
+                    checked
+                      ? `${type.color} bg-opacity-75 text-white`
+                      : "bg-white"
+                  } relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
+                }
+              ></Radio>
+            ))}
+          </div>
+        </RadioGroup>
+      </div>
+    </div>
+  );
 };
 
 export default TaskTypeRadioGroup;
