a55d593bddc03718cfe9f1be3b8302855aef9737
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-10 21:56:53 +0800

    - app settings and foundations

--- healthcare-system/app/layout.tsx
+++ healthcare-system/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const jakarta_sans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Pulse",
@@ -16,7 +22,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          " min-h-screen bg-dark-300 font-sans antialiased ",
+          jakarta_sans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- healthcare-system/app/page.tsx
+++ healthcare-system/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline">Home</h1>
+      <h1 className="text-3xl underline text-white">Home</h1>
+      <Button>button</Button>
     </div>
   );
 }
