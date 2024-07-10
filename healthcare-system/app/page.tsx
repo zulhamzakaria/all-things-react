489da226c3a31ef7d3ed4c333f1f92e26052bc85
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-10 22:20:19 +0800

    - app page stylings

--- healthcare-system/app/page.tsx
+++ healthcare-system/app/page.tsx
@@ -1,5 +1,6 @@
 import PatientForm from "@/components/forms/patient-form";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,8 +15,23 @@ export default function Home() {
             className=" mb-12 h-10 w-fit"
           />
           <PatientForm />
+          <div className=" text-14-regular mt-20 flex justify-between">
+            <p className=" justify-items-end text-dark-600 xl:text-left">
+              © 2024 healthcare-app
+            </p>
+            <Link href={"/?admin=true"} className=" text-green-500 ">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className=" side-img max-w-[50%]"
+      />
     </div>
   );
 }
