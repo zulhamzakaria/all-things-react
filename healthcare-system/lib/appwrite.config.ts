0a94a588874007c653793e1e582ec74873932724
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-13 01:13:22 +0800

    - wip

--- healthcare-system/lib/appwrite.config.ts
+++ healthcare-system/lib/appwrite.config.ts
@@ -9,6 +9,11 @@ client
   .setProject(process.env.PROJECT_ID!);
 // .setKey(process.env.API_KEY!);
 
+clientSdk
+  .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+  .setProject(process.env.PROJECT_ID!)
+  .setKey(process.env.API_KEY!);
+
 export const databases = new Databases(client);
 export const storage = new Storage(client);
 export const messaging = new Messaging(client);
