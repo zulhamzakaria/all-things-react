b006be84814d4f7c78679a6e7b10379d4d4cd0c7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-13 01:05:44 +0800

    - wip

--- healthcare-system/lib/actions/patient.actions.ts
+++ healthcare-system/lib/actions/patient.actions.ts
@@ -1,23 +1,22 @@
 import { ID, Query } from "node-appwrite";
-import { users } from "../appwrite.config";
+import { account } from "../appwrite.config";
 import { parseStringify } from "../utils";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
-    const newUser = await users.create(
+    const newUser = await account.create(
       ID.unique(),
       user.email,
-      user.phone,
       "undefined",
       user.name
     );
     return parseStringify(newUser);
   } catch (error: any) {
-    if (error && error?.code === 409) {
-      const existingUser = await users.list([
-        Query.equal("email", [user.email]),
-      ]);
-      return existingUser.users[0];
-    }
+    // if (error && error?.code === 409) {
+    //   const existingUser = await accounts.list([
+    //     Query.equal("email", [user.email]),
+    //   ]);
+    //   return existingUser.users[0];
+    // }
   }
 };
