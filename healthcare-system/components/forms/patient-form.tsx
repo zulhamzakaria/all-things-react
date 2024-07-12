3eb2e5279fe041dbb518bc517bfc95a958e1c4b2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-12 23:18:26 +0800

    - debugging

--- healthcare-system/components/forms/patient-form.tsx
+++ healthcare-system/components/forms/patient-form.tsx
@@ -44,7 +44,7 @@ const PatientForm = () => {
     try {
       const userData = { name, email, phone };
       const user = await createUser(userData);
-      // if (user) router.push(`/patients/${user.$id}/register`);
+      if (user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.error(error);
     }
