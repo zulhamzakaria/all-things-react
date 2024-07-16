aafd4ef8baf640941582035b1ffd3b1e263846a1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-16 23:06:44 +0800

    - copied everything from patient-form to register-form

--- healthcare-system/components/forms/register-form.tsx
+++ healthcare-system/components/forms/register-form.tsx
@@ -1,7 +1,92 @@
-import React from "react";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "@/components/ui/form";
+import CustomFormField from "../custom-form-field";
+import SubmitButton from "../submit-button";
+import { useState } from "react";
+import { PatientFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+import { createUntrackedSearchParams } from "next/dist/client/components/search-params";
+import { createUser } from "@/lib/actions/patient.actions";
+
+export enum FormFieldTypes {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneinput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datepicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
 
 const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof PatientFormValidation>>({
+    resolver: zodResolver(PatientFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof PatientFormValidation>) {
+    setIsLoading(true);
+    try {
+      const userData = { name, email, phone };
+      const user = await createUser(userData);
+      if (user) router.push(`/patients/${user.$id}/register`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className=" mb-12 space-y-4">
+          <h1 className=" header">Hi, there 👋</h1>
+          <p className=" text-dark-700">Schedule your first appointment.</p>
+        </section>
+
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldTypes.INPUT}
+          name="name"
+          label="Full Name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldTypes.INPUT}
+          name="email"
+          label="Email"
+          placeholder="john.doe@mail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldTypes.PHONE_INPUT}
+          name="phone"
+          label="Phone"
+          placeholder="012-3456789"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
 };
 
 export default RegisterForm;
