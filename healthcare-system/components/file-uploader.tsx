5b5f114b0cf181a3bc641d305c6b37414266e8b1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-18 20:44:07 +0800

    - file-uploader component

--- healthcare-system/components/file-uploader.tsx
+++ healthcare-system/components/file-uploader.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+"use client";
 
-const FileUploader = () => {
-  return <div>File Uploader</div>;
+import { convertFileToUrl } from "@/lib/utils";
+import Image from "next/image";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+interface FileUploaderProps {
+  files: File[] | undefined;
+  onChange: (files: File[]) => void;
+}
+
+const FileUploader = ({ files, onChange }: FileUploaderProps) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    onChange(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()} className="file-upload">
+      <input {...getInputProps()} />
+      {files && files?.length > 0 ? (
+        <Image
+          src={convertFileToUrl(files[0])}
+          width={1000}
+          height={1000}
+          alt="uploaded-image"
+          className="max-h-[400px] overflow-hidden object-cover"
+        />
+      ) : (
+        <>
+          <Image
+            src={"/assets/icons/upload.svg"}
+            width={40}
+            height={40}
+            alt="upload"
+          />
+          <div className="file-upload_label">
+            <p className="text-14-regular">
+              <span className="text-green-500">Click to upload</span>or drag and
+              drop
+            </p>
+            <p>.svg/.png/.jpg/.gif (max 800x400)</p>
+          </div>
+        </>
+      )}
+      {/* {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )} */}
+    </div>
+  );
 };
 
 export default FileUploader;
