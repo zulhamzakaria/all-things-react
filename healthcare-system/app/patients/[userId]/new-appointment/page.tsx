d8d101124379ad34cd981e4a1fdf4fb6d8e6caf5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-20 20:04:23 +0800

    - new appointment page (WIP)

--- healthcare-system/app/patients/[userId]/new-appointment/page.tsx
+++ healthcare-system/app/patients/[userId]/new-appointment/page.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import Image from "next/image";
 
 const NewAppointment = () => {
-  return <div>Book an appointment here</div>;
+  return (
+    <div className=" flex h-screen max-h-screen">
+      <section className=" remove-scrollbar container my-auto">
+        <div className=" sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            height={1000}
+            width={1000}
+            alt="patient"
+            className=" mb-12 h-10 w-fit"
+          />
+          {/* <PatientForm /> */}
+          <p className=" justify-items-end text-dark-600 xl:text-left">
+            © 2024 healthcare-app
+          </p>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className=" side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
 };
 
 export default NewAppointment;
