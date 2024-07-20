1db5f0cb12e99d3bfb528025a0720c6fff2bd8f0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-20 19:52:47 +0800

    - libs

--- netcore-with-react/restaurant-api/restaurant.api/obj/Debug/net6.0/restaurant.api.AssemblyInfo.cs
+++ netcore-with-react/restaurant-api/restaurant.api/obj/Debug/net6.0/restaurant.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("restaurant.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b85b6b525dd129ffe80ea73e2311d3852966d22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3edb90fb0acc65cae6db096660a2343910bf2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("restaurant.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("restaurant.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
