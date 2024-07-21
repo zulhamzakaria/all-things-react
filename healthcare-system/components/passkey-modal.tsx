ce99c591268439d39e89ff3ff7c6ea337ec57ff6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-21 23:13:33 +0800

    - passkey-modal (WIP)

--- healthcare-system/components/passkey-modal.tsx
+++ healthcare-system/components/passkey-modal.tsx
@@ -1,6 +1,58 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const PasskeyModal = () => {
-  return <div>PasskeyModal</div>;
+  const router = useRouter();
+  const [open, setOpen] = useState(true);
+
+  const closeModal = () => {
+    setOpen(false);
+    router.push("/");
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent className="shad-alert-dialog ">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex items-start justify-between">
+            Admin access verification
+            <Image
+              src={"/assets/icons/close.svg"}
+              width={20}
+              height={20}
+              alt="close"
+              onClick={() => closeModal()}
+              className="cursor-pointer"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Please enter the provided passkey to access the admin page.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+<div></div>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 };
 
 export default PasskeyModal;
