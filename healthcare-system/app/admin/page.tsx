849e141e3e224cf44fad35e4b6519cd073842f65
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-22 20:28:49 +0800

    - admin page stylings (WIP)

--- healthcare-system/app/admin/page.tsx
+++ healthcare-system/app/admin/page.tsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            alt="logo"
+            width={162}
+            height={32}
+            className="h-8 w-fit"
+          />
+        </Link>
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header">Welcome</h1>
+          <p className="text-dark-700">Manage appointments</p>
+        </section>
+        <section className="admin-stat">
+            
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Admin;
