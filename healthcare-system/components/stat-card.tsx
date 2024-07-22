237060b605ec8daeb3583bde336c4b91f2af2344
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-22 20:47:39 +0800

    - stat-card stylings (WIP)

--- healthcare-system/components/stat-card.tsx
+++ healthcare-system/components/stat-card.tsx
@@ -1,5 +1,36 @@
-const StatCard = () => {
-  return <div>StatCard</div>;
+import clsx from "clsx";
+import Image from "next/image";
+
+interface StatCardProps {
+  count: number;
+  label: string;
+  icon: string;
+  type: StatCardType;
+}
+
+type StatCardType = "appointments" | "pending" | "cancelled";
+
+const StatCard = ({ count = 0, label, icon, type }: StatCardProps) => {
+  return (
+    <div
+      className={clsx("stat-card", {
+        "bg-appointments": type === "appointments",
+        "bg-pending": type === "pending",
+        "bg-cancelled": type === "cancelled",
+      })}
+    >
+      <div className="flex items-center gap-4">
+        <Image
+          src={icon}
+          height={32}
+          width={32}
+          alt={label}
+          className="size-8 w-fit"
+        />
+        <h2 className="text-32-bold text-white">{count}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default StatCard;
