b2f6bccbd3529e30f8c57065027ecab9ebae2b20
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-23 16:27:15 +0800

    - adding dialog

--- healthcare-system/components/table/columns.tsx
+++ healthcare-system/components/table/columns.tsx
@@ -1,16 +1,8 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { MoreHorizontal } from "lucide-react";
+
+
 import StatusBadge from "../status-badge";
 import { formatDateTime } from "@/lib/utils";
 import { Doctors } from "@/constants";
