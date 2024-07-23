ebe59df81a23d5661f7b524560755513514f4c34
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-23 21:31:34 +0800

    - installed sentry

---
+++ healthcare-system/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7ef37c7cb06839e0c0405623eb72475b@o4507651346268160.ingest.de.sentry.io/4507651349741648",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
