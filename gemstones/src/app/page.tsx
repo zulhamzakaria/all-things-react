beec0589207e0f42edf108774d96567f573216b9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-24 17:57:50 +0800

    - basic maxwidthwrapper component - import maxwidthwrapper from app page

--- gemstones/src/app/page.tsx
+++ gemstones/src/app/page.tsx
@@ -1,9 +1,13 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="bg-slate-50">
-    <section>
-      {/* reusable component that works as a 'container' for uniform design */}
-    </section>
-  </div>;
+  return (
+    <div className="bg-slate-50">
+      <section>
+        {/* reusable component that works as a 'container' for uniform design */}
+        <MaxWidthWrapper></MaxWidthWrapper>
+      </section>
+    </div>
+  );
 }
