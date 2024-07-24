8029d6a6205dd29752061992cb8124e9acdcc042
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-24 18:26:01 +0800

    - app page stylings

--- gemstones/src/app/page.tsx
+++ gemstones/src/app/page.tsx
@@ -6,8 +6,43 @@ export default function Home() {
     <div className="bg-slate-50">
       <section>
         {/* reusable component that works as a 'container' for uniform design */}
-        <MaxWidthWrapper>
-          <div>hello</div>
+        <MaxWidthWrapper
+          className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 
+        lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52"
+        >
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <Image
+                  src="/snake-1.png"
+                  alt="snake-1"
+                  className="w-full"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <h1
+                className="relative w-fit tracking-tight text-balance mt-16 
+                font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl"
+              >
+                Your image on a
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favorite memories with your own,{" "}
+                <span className="font-semibold">one-of-one</span> phone case.
+                Immortalise your memories
+              </p>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    High quality, durable material
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </div>
