4d1f2dd66560375949a13d6d4d49f9d32c1f6b9e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-24 21:59:54 +0800

    - navbar component

---
+++ gemstones/src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className=" sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gry-200 bg-white/75 backdrop-blur-lg transition-all">
+      {/* use maxwidthwrapper so that the with stays the same */}
+      <MaxWidthWrapper>
+        <div className=" flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href={"/"} className=" flex z-40 font-semibold">
+            case <span className=" text-green-600">cobra</span>
+          </Link>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
