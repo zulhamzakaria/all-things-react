c4aa8d5f2cebe92bb20d5475bb3f2bc0f3fc4b95
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-24 22:42:59 +0800

    - stylings

--- gemstones/src/components/Navbar.tsx
+++ gemstones/src/components/Navbar.tsx
@@ -68,6 +68,16 @@ const Navbar = () => {
                   })}
                 >
                   Sign in
+                </Link>
+                <div className="h-8 w-px bg-zinc-200 sm:block " />
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
                   <ArrowRight className=" ml-1.5 h-5 w-5" />
                 </Link>
               </>
