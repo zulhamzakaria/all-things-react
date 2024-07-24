14b93c025dd82fc6841c09c2ccd0bb943e88ffa1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-24 20:36:12 +0800

    - Phone

--- gemstones/src/components/Phone.tsx
+++ gemstones/src/components/Phone.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
 
-const Phone = () => {
-  return <div>Phone</div>;
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  darkMode?: boolean;
+}
+
+const Phone = ({
+  className,
+  imgSrc,
+  darkMode = false,
+  ...props
+}: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+    >
+      Phone
+    </div>
+  );
 };
 
 export default Phone;
