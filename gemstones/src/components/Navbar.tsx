b3eaf3a0cce081a67cc5881156f786db227dcc7a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-24 22:40:11 +0800

    - sign up, sign in, sign out

--- gemstones/src/components/Navbar.tsx
+++ gemstones/src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
 
 const Navbar = () => {
   const user = undefined;
+  const isAdmin = false;
   return (
     <nav className=" sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gry-200 bg-white/75 backdrop-blur-lg transition-all">
       {/* use maxwidthwrapper so that the with stays the same */}
@@ -14,10 +17,61 @@ const Navbar = () => {
           </Link>
           <div className=" h-full flex items-center space-x-4">
             {user ? (
-                <>
-                <Link href='/api/auth/logout' className=""></Link>
-                </>
-            ):()}
+              <>
+                <Link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign out
+                </Link>
+                {isAdmin && (
+                  <Link
+                    href="/api/auth/logout"
+                    className={buttonVariants({
+                      size: "sm",
+                      variant: "ghost",
+                    })}
+                  >
+                    Dashboard
+                  </Link>
+                )}
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className=" ml-1.5 h-5 w-5" />
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/api/auth/register"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign up
+                </Link>
+                <Link
+                  href="/api/auth/login"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign in
+                  <ArrowRight className=" ml-1.5 h-5 w-5" />
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
