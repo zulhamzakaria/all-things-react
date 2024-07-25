e1bd7eed08cb042fabd724850041d8922429949e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-25 15:47:25 +0800

    - basic reviews component - import reviews component from home page

--- gemstones/src/app/page.tsx
+++ gemstones/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import Reviews from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
@@ -200,6 +201,9 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
     </div>
   );
