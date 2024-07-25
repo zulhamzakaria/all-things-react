32c8c49947a22c61f02beb0b31e9ea048b54a795
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-25 16:36:02 +0800

    - reviews component (WIP)

--- gemstones/src/components/Reviews.tsx
+++ gemstones/src/components/Reviews.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 
 const Reviews = () => {
-  return <div>Reviews</div>;
+  return (
+    <MaxWidthWrapper className="relative max-w-5xl">
+      <img
+        aria-hidden
+        src="/what-people-are-buying.png"
+        alt="sold-items"
+        className="absolute select-none hidden xl:block -left-32 top-1/3"
+      />
+    </MaxWidthWrapper>
+  );
 };
 
 export default Reviews;
