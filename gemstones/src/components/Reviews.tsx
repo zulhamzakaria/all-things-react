7daa148b5bda8401f66723c71bc39d985cd559f4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-25 16:43:41 +0800

    - grid animation (WIP)

--- gemstones/src/components/Reviews.tsx
+++ gemstones/src/components/Reviews.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+
+function ReviewGrid() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    ></div>
+  );
+}
 
 const Reviews = () => {
   return (
@@ -10,6 +23,8 @@ const Reviews = () => {
         alt="sold-items"
         className="absolute select-none hidden xl:block -left-32 top-1/3"
       />
+
+      <ReviewGrid />
     </MaxWidthWrapper>
   );
 };
