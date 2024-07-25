452d06e43204a4475cd45baac43307cbd3feba52
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-25 14:02:48 +0800

    - testimonial section (WIP)

--- gemstones/src/app/page.tsx
+++ gemstones/src/app/page.tsx
@@ -111,6 +111,17 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
       </section>
+
+      {/* value proposition? */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              Tesimonies:
+            </h2>
+          </div>
+        </MaxWidthWrapper>
+      </section>
     </div>
   );
 }
