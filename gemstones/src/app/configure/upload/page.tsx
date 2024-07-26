e825dd9656d40bfdfb46e13ca368839ece9b8ee3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-26 18:55:01 +0800

    - Upload page

---
+++ gemstones/src/app/configure/upload/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+const Upload = () => {
+  const [isDraggedOver, setIsDraggedOver] = useState(false);
+  return (
+    <div
+      className={cn(
+        "relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center",
+        { "ring-blue-900/25 bg-blue-900/10": isDraggedOver }
+      )}
+    >
+      Upload
+    </div>
+  );
+};
+
+export default Upload;
