1d1d1c61f2704712d4ff94d1c5fb4b19828941e6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-26 20:44:48 +0800

    - changes for getting configId to pass to upload page shift + alt + o to remove unused imports

--- gemstones/src/app/api/uploadthing/core.ts
+++ gemstones/src/app/api/uploadthing/core.ts
@@ -1,28 +1,21 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { z } from "zod";
 
 const f = createUploadthing();
-
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .input(z.object({ configId: z.string().optional() }))
     // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
-
-      // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
-
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+    .middleware(async ({ input }) => {
+      return { input };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
+      //   const { configId } = metadata.input;
+      //   return { configId };
+      return { configId: metadata.input.configId };
     }),
 } satisfies FileRouter;
 
