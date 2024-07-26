036b5b8c588650b41040a92909a2e1a5f94ca4c2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-26 23:48:57 +0800

    - steps separator

--- gemstones/src/components/Steps.tsx
+++ gemstones/src/components/Steps.tsx
@@ -78,6 +78,22 @@ const Steps = () => {
                   </span>
                 </span>
               </span>
+              {index !== 0 ? (
+                <div className="absolute inset-0 hidden w-3 lg:block">
+                  <svg
+                    className="h-full w-full text-gray-300"
+                    viewBox="0 0 12 82"
+                    fill="none"
+                    preserveAspectRatio="none"
+                  >
+                    <path
+                      d="M0.5 0V31L10.5 41L0.5 51V82"
+                      stroke="currentcolor"
+                      vectorEffect="non-scaling-stroke"
+                    />
+                  </svg>
+                </div>
+              ) : null}
             </div>
           </li>
         );
