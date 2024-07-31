aa489c18d5e4231316107c8143b6c7c4704117c7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-31 20:57:27 +0800

    - getting user from clerk middleware

--- resume-v2/middleware.ts
+++ resume-v2/middleware.ts
@@ -9,17 +9,24 @@ const protectedRoutes = createRouteMatcher(["/some-link"]);
 
 export default clerkMiddleware((auth, req) => {
   if (protectedRoutes(req)) auth().protect();
-});
 
-// default user
-export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname === "/" && !req.nextUrl.searchParams.has("user")) {
-    return NextResponse.redirect(
-      new URL("/?user=user_2gzSBiNggGcNbE28mwhWxtaZyLC", req.url)
-    );
+  const userId = auth().userId;
+
+  if (userId) {
+    console.log(userId);
   }
   return NextResponse.next();
-}
+});
+
+// // default user
+// export function middleware(req: NextRequest) {
+//   if (req.nextUrl.pathname === "/" && !req.nextUrl.searchParams.has("user")) {
+//     return NextResponse.redirect(
+//       new URL("/?user=user_2gzSBiNggGcNbE28mwhWxtaZyLC", req.url)
+//     );
+//   }
+//   return NextResponse.next();
+// }
 
 // Stop Middleware running on static files
 export const config = {
