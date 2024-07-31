719f28506d52ab465923691be931749c5eaffe7c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-31 19:49:35 +0800

    - focusing on auth

--- resume-v2/components/resume.tsx
+++ resume-v2/components/resume.tsx
@@ -31,10 +31,10 @@ const ResumePage = () => {
         className=" h-[auto] lg:w-[900px] sm:w-auto bg-gray-50 rounded-t-sm shadow-sm pt-10 pb-10 pl-5 pr-5 flex flex-col"
       >
         <DetailsPage />
-        <SummaryPage />
+        {/* <SummaryPage />
         <SkillsPage />
         <ExperiencesPage />
-        <EducationPage />
+        <EducationPage /> */}
         <div className="flex flex-row text-sm font-thin mt-36 justify-end mr-2">
           <span>powered by</span>
           <p className=" ml-1 font-semibold">nextJS</p>
