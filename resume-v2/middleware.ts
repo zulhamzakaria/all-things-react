f60775d20593f353665986b821cb42bb5a7f2ac8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-31 19:57:55 +0800

    - middleware for re-direction

--- resume-v2/middleware.ts
+++ resume-v2/middleware.ts
@@ -1,10 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = createRouteMatcher(["/some-link"]);
 
 export default clerkMiddleware((auth, req) => {
   if (protectedRoutes(req)) auth().protect();
 });
+
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname === "/") {
+    return NextResponse.redirect(
+      new URL("/?user=user_2gzSBiNggGcNbE28mwhWxtaZyLC")
+    );
+  }
+  return NextResponse.next();
+}
 
 // Stop Middleware running on static files
 export const config = {
