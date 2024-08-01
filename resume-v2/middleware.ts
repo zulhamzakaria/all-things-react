5af44c4a36832601595136fc5a02ca51df5c7bca
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-01 10:42:18 +0800

    - added condition

--- resume-v2/middleware.ts
+++ resume-v2/middleware.ts
@@ -13,8 +13,8 @@ export default clerkMiddleware((auth, req) => {
   const userId = auth().userId;
 
   if (userId) {
-    // if (req.nextUrl.pathname === "/" && !req.nextUrl.searchParams.has("user"))
-    return NextResponse.redirect(new URL(`/?user=${userId}`, req.url));
+    if (req.nextUrl.pathname === "/" && !req.nextUrl.searchParams.has("user"))
+      return NextResponse.redirect(new URL(`/?user=${userId}`, req.url));
   } else {
     if (req.nextUrl.pathname === "/" && !req.nextUrl.searchParams.has("user")) {
       return NextResponse.redirect(
