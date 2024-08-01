ffbb4b2a4cae1041e149cb83d6fa45fd297948c2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-01 20:09:29 +0800

    - huh

--- resume-v2/components/wrappers/notification-wrapper.tsx
+++ resume-v2/components/wrappers/notification-wrapper.tsx
@@ -1,7 +1,13 @@
 import { AlertCircleIcon, XIcon } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 const NotificationWrapper = ({ children }: { children: React.ReactNode }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
   return (
     <div className="bg-dark-3/75 space-x-2  rounded-md my-2 lg:w-[900px] py-6 w-auto px-2 flex flex-row">
       <p className="text-yellow-100">
@@ -9,7 +15,10 @@ const NotificationWrapper = ({ children }: { children: React.ReactNode }) => {
       </p>
       <span className="text-slate-100 font-sans w-full">{children}</span>
       <div className="border-l border-gray-400 h-full mx-2"></div>
-      <span className="text-white font-semibold cursor-pointer">
+      <span
+        className="text-white font-semibold cursor-pointer"
+        onClick={handleClose}
+      >
         <XIcon />
       </span>
     </div>
