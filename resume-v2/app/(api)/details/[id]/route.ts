45ef12c6b2c14ca42b0f8f8f95c273ba1268c7ed
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-03 20:27:38 +0800

    - const id follows the folder param [id]?

--- resume-v2/app/(api)/details/[id]/route.ts
+++ resume-v2/app/(api)/details/[id]/route.ts
@@ -2,5 +2,9 @@ import { details } from "@/data";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-    const user = details
+  const { id } = req.query;
+  const result = details.find((user) => user.userId === id);
+  if (result) {
+    res.status(200).json(result);
+  }
 }
