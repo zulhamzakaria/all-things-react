a529982f353c3734e6abe61997ec4101c24e0673
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-06 20:09:34 +0800

    - WIP

--- resume-v2/components/details/create-details.tsx
+++ resume-v2/components/details/create-details.tsx
@@ -1,7 +1,9 @@
 import { useDialog } from "@/lib/use-dialog";
 import { CreateDetailsSchema } from "@/schemas/details";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import useSWR from "swr";
 import { z } from "zod";
 
@@ -9,12 +11,24 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const CreateDetails = ({ userId }: { userId: string }) => {
   // format: http://localhost:3000/details/user_2gb3uVBwoAB9WzzUB5Ix3FIBc8e
-  const { data, isLoading, error } = useSWR(`/details/${userId}`, fetcher);
+  // data is for mutating it
+  const { data } = useSWR(`/details/${userId}`, fetcher);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const { onClose } = useDialog();
 
   const {} = useForm<z.infer<typeof CreateDetailsSchema>>({
     resolver: zodResolver(CreateDetailsSchema),
   });
+
+  const onSubmit = async () => {
+    setIsLoading(true);
+    try {
+    } catch (err) {
+      toast.error((err as Error).message);
+    }finally{
+      setIsLoading(false)
+    }
+  };
 
   return <div>CreateDetails</div>;
 };
