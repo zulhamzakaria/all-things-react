5898963b6a211be584948cbb18fea8af9ba17d12
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-06 19:54:32 +0800

    - zod schema

--- resume-v2/components/details/create-details.tsx
+++ resume-v2/components/details/create-details.tsx
@@ -1,6 +1,20 @@
+import { useDialog } from "@/lib/use-dialog";
+import { AddDetailsSchema } from "@/schemas/details";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import useSWR from "swr";
+import { z } from "zod";
+
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-const CreateDetails = () => {
+const CreateDetails = ({ userId }: { userId: string }) => {
+  const { data, isLoading, error } = useSWR(`/details/${userId}`, fetcher);
+  const { onClose } = useDialog();
+
+  const {} = useForm<z.infer<typeof AddDetailsSchema>>({
+    resolver: zodResolver(AddDetailsSchema),
+  });
+
   return <div>CreateDetails</div>;
 };
 
