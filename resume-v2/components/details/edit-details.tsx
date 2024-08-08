cf2dd06cac887b04192dabbf2672c35d49dc34f6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-08 20:16:35 +0800

    - loading state for data

--- resume-v2/components/details/edit-details.tsx
+++ resume-v2/components/details/edit-details.tsx
@@ -6,6 +6,7 @@ import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import useSWR from "swr";
 import { z } from "zod";
+import LoadingCard from "../loading-card";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -13,8 +14,8 @@ const EditDetails = () => {
   const { userId, itemId } = EditDialogItemIdStore();
   // format: http://localhost:3000/details/user_2gb3uVBwoAB9WzzUB5Ix3FIBc8e
   // data is for mutating it
-  const { data } = useSWR(`/details/${userId}`, fetcher);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { data, isLoading } = useSWR(`/details/${userId}`, fetcher);
+  const [isPending, setIsPending] = useState<boolean>(false);
   const { onClose } = useDialog();
 
   const {} = useForm<z.infer<typeof CreateDetailsSchema>>({
@@ -22,16 +23,22 @@ const EditDetails = () => {
   });
 
   const onSubmit = async () => {
-    setIsLoading(true);
+    setIsPending(true);
     try {
     } catch (err) {
       toast.error((err as Error).message);
     } finally {
-      setIsLoading(false);
+      setIsPending(false);
     }
   };
 
-  return <div>{userId}</div>;
+  return data && !isLoading ? (
+    <div>{userId}</div>
+  ) : (
+    <div className=" w-full flex justify-center">
+      <LoadingCard />
+    </div>
+  );
 };
 
 export default EditDetails;
