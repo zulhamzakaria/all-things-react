b587f3554f0f272ad2914a3650f38fdd1fedd19d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-08 23:33:17 +0800

    - typo - min length for phone

--- resume-v2/schemas/details.ts
+++ resume-v2/schemas/details.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
 export const CreateDetailsSchema = z.object({
-  name: z.string().min(2, { message: "Name must be longer that 2 chars" }),
-  phone: z.string(),
+  name: z.string().min(2, { message: "Name must be longer than 2 chars" }),
+  phone: z.string().min(7, { message: "Phone must be longer than 7 chars" }),
   email: z.string().email(),
-  role: z.string().min(2, { message: "Role must be longer that 2 chars" }),
+  role: z.string().min(2, { message: "Role must be longer than 2 chars" }),
   fullLocation: z
     .string()
-    .min(2, { message: "Full Location must be longer that 2 chars" }),
+    .min(2, { message: "Full Location must be longer than 2 chars" }),
   shortLocation: z
     .string()
-    .min(2, { message: "Full Location must be longer that 2 chars" }),
+    .min(2, { message: "Full Location must be longer than 2 chars" }),
 });
