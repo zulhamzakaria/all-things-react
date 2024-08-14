a1f1e67f6d36a17966d29642dcc62e084ee07f8f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-14 21:27:01 +0800

    - summary dynamic GET

--- resume-v2/app/(api)/summary/[id]/route.ts
+++ resume-v2/app/(api)/summary/[id]/route.ts
@@ -5,3 +5,23 @@ const apiUrl = `${process.env.API_URL}/details`;
 const defaultSummary: Summary = {
   description: "Not available",
 };
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const response = await fetch(`${apiUrl}/${params.id}`);
+
+    if (!response.ok) {
+      throw new Error(`Error fetching user data. Error:${response.status}`);
+    }
+
+    const summary = await response.json();
+
+    return Response.json(summary);
+  } catch (err) {
+    console.error((err as Error).message);
+    return Response.json(defaultSummary);
+  }
+}
