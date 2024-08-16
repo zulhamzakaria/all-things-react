b772e4845bb73adf912fb19c85a733e1f3874d35
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-16 20:43:32 +0800

    - WIP

--- modern-ai-chatbot/src/app/[...url]/page.tsx
+++ modern-ai-chatbot/src/app/[...url]/page.tsx
@@ -1,10 +1,27 @@
+import { ragChat } from "@/lib/rag-chat";
+
 interface PageProps {
   params: {
     url: string | string[] | undefined;
   };
 }
 
-const Page = ({ params }: PageProps) => {
+function reconstructUrl({ url }: { url: string[] }) {
+  const decodedComponents = url.map((component) =>
+    decodeURIComponent(component)
+  );
+  // joins the elements with a /
+  return decodedComponents.join("/");
+}
+
+const Page = async ({ params }: PageProps) => {
+  const url = reconstructUrl({ url: params.url as string[] });
+  await ragChat.context.add({
+    type: "html",
+    source: url,
+    config: { chunkOverlap: 50, chunkSize: 200 },
+  });
+
   return <p>{params.url}</p>;
 };
 
