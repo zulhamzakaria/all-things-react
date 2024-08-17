52deb13d4cca923ffb5b9d6a66828cf32c094389
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-17 15:40:41 +0800

    - (WIP)

--- modern-ai-chatbot/src/components/Message.tsx
+++ modern-ai-chatbot/src/components/Message.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 interface MessageProps {
@@ -6,7 +7,28 @@ interface MessageProps {
 }
 
 const Message = ({ content, isUserMessage }: MessageProps) => {
-  return <div>Message</div>;
+  return (
+    <div
+      className={cn({
+        "bg-zinc-800": isUserMessage,
+        "bg-zinc-900/25": !isUserMessage,
+      })}
+    >
+      <div className="p-6">
+        <div className="max-w-3xl mx-auto flex items-start gap-2.5">
+          <div
+            // display ai and user's message differently
+            className={cn(
+              "size-10 shrink-0 aspect-square rounded-full border border-zinc-700 bg-zinc-900 flex justify-center items-center ",
+              {
+                "bg-blue-950 border-blue-700 text-zinc-200": isUserMessage,
+              }
+            )}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
