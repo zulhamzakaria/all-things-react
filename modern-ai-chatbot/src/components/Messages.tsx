1f69dbee8ed240f9b45ea85a14a7db385ebd422a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-17 15:22:12 +0800

    - WIP

--- modern-ai-chatbot/src/components/Messages.tsx
+++ modern-ai-chatbot/src/components/Messages.tsx
@@ -5,7 +5,21 @@ interface MessagesProps {
 }
 
 const Messages = ({ messages }: MessagesProps) => {
-  return <div>Messages</div>;
+  return (
+    <div className=" flex-1 flex-col overflow-y-auto flex max-h-[calc(100vh-3.5rem-7rem)]">
+      {messages ? (
+        messages.map((msg, idx) => (
+          <Message
+            key={idx}
+            content={msg.content}
+            isUserMessage={msg.role === "user"}
+          />
+        ))
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 };
 
 export default Messages;
