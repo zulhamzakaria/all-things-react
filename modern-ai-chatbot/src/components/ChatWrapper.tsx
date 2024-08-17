bc39c290792ae2fa58a7b6d96d40b37bc89981ad
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-17 15:51:53 +0800

    - missing closing / for component Messages

--- modern-ai-chatbot/src/components/ChatWrapper.tsx
+++ modern-ai-chatbot/src/components/ChatWrapper.tsx
@@ -17,7 +17,7 @@ const ChatWrapper = ({ sessionId }: ChatWrapperProps) => {
   return (
     <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
       <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col">
-        <Messages messages={messages}>
+        <Messages messages={messages} />
       </div>
 
       <form onSubmit={handleSubmit}>
