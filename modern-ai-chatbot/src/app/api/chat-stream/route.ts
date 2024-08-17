b8fd2acdffc50c8df116f4c91758d4f31fd963bb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-17 15:13:38 +0800

    - POST endpoint

--- modern-ai-chatbot/src/app/api/chat-stream/route.ts
+++ modern-ai-chatbot/src/app/api/chat-stream/route.ts
@@ -1,4 +1,5 @@
 import { ragChat } from "@/lib/rag-chat";
+import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
@@ -6,5 +7,9 @@ export const POST = async (req: NextRequest) => {
 
   //get the last message/chat content
   const lastMessage = messages[messages.length - 1].content;
-  const response = await ragChat.chat(lastMessage);
+  const response = await ragChat.chat(lastMessage, {
+    streaming: true,
+    sessionId,
+  });
+  return aiUseChatAdapter(response);
 };
