fad41c1e3de3c06f4c3f31efd91355cf2cd37e04
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-17 15:09:59 +0800

    -wrap the input and submit button inside a form

--- modern-ai-chatbot/src/components/ChatWrapper.tsx
+++ modern-ai-chatbot/src/components/ChatWrapper.tsx
@@ -7,7 +7,7 @@ interface ChatWrapperProps {
 }
 
 const ChatWrapper = ({ sessionId }: ChatWrapperProps) => {
-  const { messages, handleInputChange, input } = useChat({
+  const { messages, handleInputChange, handleSubmit, input } = useChat({
     // data source for ai
     api: "/api/chat-stream",
     // added session for isolation
@@ -18,7 +18,11 @@ const ChatWrapper = ({ sessionId }: ChatWrapperProps) => {
       <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col">
         {JSON.stringify(messages)}
       </div>
-      <input value={input} onChange={handleInputChange} type="text" />
+
+      <form onSubmit={handleSubmit}>
+        <input value={input} onChange={handleInputChange} type="text" />
+        <button type="submit">send</button>
+      </form>
     </div>
   );
 };
