3147a39bff5a3f4238ca28d0ceede39aa3e64d3a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-30 23:40:25 +0800

    - attendance chart (WIP)

--- my-school-management/src/app/components/AttendanceChart.tsx
+++ my-school-management/src/app/components/AttendanceChart.tsx
@@ -1,9 +1,74 @@
 "use client";
 
-import React from "react";
+import Image from "next/image";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Mon",
+    present: 4000,
+    absent: 2400,
+  },
+  {
+    name: "Tue",
+    present: 3000,
+    absent: 1398,
+  },
+  {
+    name: "Wed",
+    present: 2000,
+    absent: 9800,
+  },
+  {
+    name: "Thu",
+    present: 2780,
+    absent: 3908,
+  },
+  {
+    name: "Fri",
+    present: 1890,
+    absent: 4800,
+  },
+];
 
 const AttendanceChart = () => {
-  return <div>AttendanceChart</div>;
+  return (
+    <div className="h-full bg-white rounded-lg p-4">
+      <div>
+        <h1>Attendance</h1>
+        <Image src={"/moreDark.png"} alt="" width={20} height={20} />
+      </div>
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart width={500} height={300} data={data} barSize={20}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey="present"
+            fill="#8884d8"
+            activeBar={<Rectangle fill="pink" stroke="blue" />}
+          />
+          <Bar
+            dataKey="absent"
+            fill="#82ca9d"
+            activeBar={<Rectangle fill="gold" stroke="purple" />}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default AttendanceChart;
