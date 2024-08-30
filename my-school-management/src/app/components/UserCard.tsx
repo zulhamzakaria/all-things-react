2c4255cd48961a0cefa4e2d8f2cf504dc526ed87
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-30 22:22:36 +0800

    - usr card component

---
+++ my-school-management/src/app/components/UserCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface UserCardProps {
+  type: string;
+}
+
+const UserCard = ({ type }: UserCardProps) => {
+  return <div>UserCard</div>;
+};
+
+export default UserCard;
