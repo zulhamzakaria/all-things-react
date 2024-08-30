683f134fecf6999ddc90aa825446cf9d72c8ef40
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-30 20:05:30 +0800

    - dashboarrd basic layout page

---
+++ my-school-management/src/app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className="">{children}</body>
+    </html>
+  );
+}
