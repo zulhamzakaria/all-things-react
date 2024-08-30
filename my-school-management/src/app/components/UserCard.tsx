7b9246e7083d51e033f8882879763deb4d9d8f82
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-30 22:35:50 +0800

    - user card stylings

--- my-school-management/src/app/components/UserCard.tsx
+++ my-school-management/src/app/components/UserCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 interface UserCardProps {
@@ -6,8 +7,13 @@ interface UserCardProps {
 
 const UserCard = ({ type }: UserCardProps) => {
   return (
-    <div className="rounded-2xl odd:bg-lamaPurple p-4 even:bg-lamaYellow">
-      UserCard
+    <div className="rounded-2xl flex-1 min-w-130px odd:bg-lamaPurple p-4 even:bg-lamaYellow">
+      <div>
+        <span>2024/5</span>
+        <Image src={"/more.png"} alt="" width={20} height={20} />
+      </div>
+      <h1>1,234</h1>
+      <h2>{type}</h2>
     </div>
   );
 };
