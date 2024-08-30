45076b6c172e109eccd27010a4a9ff65ef400426
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-30 22:36:42 +0800

    - stylings

--- my-school-management/src/app/(dashboard)/admin/page.tsx
+++ my-school-management/src/app/(dashboard)/admin/page.tsx
@@ -4,7 +4,7 @@ const AdminPage = () => {
   return (
     <div className="p-4 flex -gap-4 flex-col md:flex-row">
       <div className="w-full lg:w-2/3">
-        <div className="flex gap-4 justify-between">
+        <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
