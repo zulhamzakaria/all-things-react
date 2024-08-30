b9ce6676cbadedd40ebcede2d1406c5f23e19578
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-30 21:32:09 +0800

    - basic navbar component - dashboard layout

--- my-school-management/src/app/(dashboard)/layout.tsx
+++ my-school-management/src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function DashboardLayout({
@@ -6,7 +8,13 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex flex-row">
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] text-black bg-green-50 ">
-        LHS
+        <Link
+          href="/"
+          className=" p-4 lg:justify-start flex items-center justify-center gap-2 "
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">School! School!</span>
+        </Link>
       </div>
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] text-black bg-slate-200">
         RHS
