dd79913bffbf391694cf93b57f1161d8bfc62207
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-30 22:20:39 +0800

    - admin page stylings (WIP)

--- my-school-management/src/app/(dashboard)/admin/page.tsx
+++ my-school-management/src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,10 @@
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="p-4 flex -gap-4 flex-col md:flex-row">
+      <div className="w-full lg:w-2/3">left</div>
+      <div className="w-full lg:w-1/3">right</div>
+    </div>
+  );
 };
 
 export default AdminPage;
