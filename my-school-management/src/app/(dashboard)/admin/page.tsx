77311a766f08f4d739de44b709670f82fde1c853
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-30 22:24:49 +0800

    - import UserCard from admin page

--- my-school-management/src/app/(dashboard)/admin/page.tsx
+++ my-school-management/src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,16 @@
+import UserCard from "@/app/components/UserCard";
+
 const AdminPage = () => {
   return (
     <div className="p-4 flex -gap-4 flex-col md:flex-row">
-      <div className="w-full lg:w-2/3">left</div>
+      <div className="w-full lg:w-2/3">
+        <div className="flex gap-4 justify-between">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
       <div className="w-full lg:w-1/3">right</div>
     </div>
   );
