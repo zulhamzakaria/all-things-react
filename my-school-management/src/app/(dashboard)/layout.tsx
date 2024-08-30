632cb204261af832de319a4baf4a1fe78319eafb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-30 21:24:21 +0800

    - layout

--- my-school-management/src/app/(dashboard)/layout.tsx
+++ my-school-management/src/app/(dashboard)/layout.tsx
@@ -5,8 +5,12 @@ export default function DashboardLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="h-screen flex flex-row">
-      <div className="w-1/6 text-black bg-green-50 ">LHS</div>
-      <div className="w-5/6 text-black bg-slate-200">RHS</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] text-black bg-green-50 ">
+        LHS
+      </div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] text-black bg-slate-200">
+        RHS
+      </div>
       {/* {children} */}
     </div>
   );
