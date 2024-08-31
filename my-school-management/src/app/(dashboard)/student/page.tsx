3f3ef6bacd5091a9d62f471974fad5333cd19be2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-31 15:38:25 +0800

    - stylings (WIP)

--- my-school-management/src/app/(dashboard)/student/page.tsx
+++ my-school-management/src/app/(dashboard)/student/page.tsx
@@ -6,7 +6,11 @@ const StudentPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
       {/* left */}
-      <div className="w-full xl:w-2/3"></div>
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+        </div>
+      </div>
       {/* right */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
         <EventCalendar />
