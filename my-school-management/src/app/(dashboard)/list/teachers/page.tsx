d95d2096bec9cc688f8c18d43d90180565427148
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-31 19:55:12 +0800

    - WIP

--- my-school-management/src/app/(dashboard)/list/teachers/page.tsx
+++ my-school-management/src/app/(dashboard)/list/teachers/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TeacherPage = () => {
-  return <div>TeacherPage</div>;
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0 h-screen">
+      {/* top */}
+      <div>
+        <h1>Teachers</h1>
+        <div></div>
+      </div>
+      {/* list */}
+      <div></div>
+      {/* pagination */}
+      <div></div>
+    </div>
+  );
 };
 
 export default TeacherPage;
