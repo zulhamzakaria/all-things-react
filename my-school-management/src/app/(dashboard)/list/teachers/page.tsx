7944ef21a60b3f5b6a4a1814035114477fe118b2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-31 20:12:04 +0800

    - stylings

--- my-school-management/src/app/(dashboard)/list/teachers/page.tsx
+++ my-school-management/src/app/(dashboard)/list/teachers/page.tsx
@@ -10,9 +10,15 @@ const TeacherPage = () => {
         <h1 className="hidden md:block text-lg font-semibold">Teachers</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
-          <div>
-            <button>
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 bg-lamaYellow flex items-center justify-center rounded-full">
               <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 bg-lamaYellow flex items-center justify-center rounded-full">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 bg-lamaYellow flex items-center justify-center rounded-full">
+              <Image src="/plus.png" alt="" width={14} height={14} />
             </button>
           </div>
         </div>
