2a151c9b41f9f7c23621a327923a3b1e70e5d93f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-31 15:26:10 +0800

    - WIP

--- my-school-management/src/app/(dashboard)/student/page.tsx
+++ my-school-management/src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,19 @@
+import Announcement from "@/app/components/Announcement";
+import EventCalendar from "@/app/components/EventCalendar";
 import React from "react";
 
 const StudentPage = () => {
-  return <div>StudentPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* left */}
+      <div className="w-full xl:w-2/3"></div>
+      {/* right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcement />
+      </div>
+    </div>
+  );
 };
 
 export default StudentPage;
