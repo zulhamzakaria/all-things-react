427173b95cb9a437c00d2584be6011e6e8aeb113
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-31 15:59:27 +0800

    - add BigCalendar tp Student page

--- my-school-management/src/app/(dashboard)/student/page.tsx
+++ my-school-management/src/app/(dashboard)/student/page.tsx
@@ -1,4 +1,5 @@
 import Announcement from "@/app/components/Announcement";
+import BigCalendar from "@/app/components/BigCalendar";
 import EventCalendar from "@/app/components/EventCalendar";
 import React from "react";
 
@@ -9,6 +10,7 @@ const StudentPage = () => {
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
         </div>
       </div>
       {/* right */}
