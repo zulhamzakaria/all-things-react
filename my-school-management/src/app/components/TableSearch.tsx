e5c2b9e7c9a5f4fdb13dd608a214fc442f9c1a8f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-31 20:09:22 +0800

    - stylings

--- my-school-management/src/app/components/TableSearch.tsx
+++ my-school-management/src/app/components/TableSearch.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Image from "next/image";
 
 const TableSearch = () => {
-  return <div>TableSearch</div>;
+  return (
+    <div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] px-2 py-1 ring-gray-300">
+      <Image
+        className="object-contain"
+        src={"/search.png"}
+        alt=""
+        width={14}
+        height={14}
+      />
+      <input
+        type="text"
+        placeholder="Search..."
+        className="w-[200px] p-2 bg-transparent outline-none"
+      />
+    </div>
+  );
 };
 
 export default TableSearch;
