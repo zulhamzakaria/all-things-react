cbea90f1c0d944053fce20ca18c5b2d09246f1ef
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-31 20:02:58 +0800

    - stylings

--- my-school-management/src/app/(dashboard)/list/teachers/page.tsx
+++ my-school-management/src/app/(dashboard)/list/teachers/page.tsx
@@ -1,12 +1,21 @@
+import TableSearch from "@/app/components/TableSearch";
+import Image from "next/image";
 import React from "react";
 
 const TeacherPage = () => {
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0 h-screen">
       {/* top */}
-      <div>
-        <h1>Teachers</h1>
-        <div></div>
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">Teachers</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div>
+            <button>
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+          </div>
+        </div>
       </div>
       {/* list */}
       <div></div>
---
+++ my-school-management/src/app/components/TableSearch.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TableSearch = () => {
+  return <div>TableSearch</div>;
+};
+
+export default TableSearch;
