68c0f76e5c0dae6b1aeb3573cf98c70708179f21
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-31 12:19:15 +0800

    - finance chart styling

--- my-school-management/src/app/components/FinanceChart.tsx
+++ my-school-management/src/app/components/FinanceChart.tsx
@@ -1,7 +1,141 @@
-import React from "react";
+"use client";
 
+import Image from "next/image";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Jan",
+    income: 4000,
+    expense: 2400,
+  },
+  {
+    name: "Feb",
+    income: 3000,
+    expense: 1398,
+  },
+  {
+    name: "Mar",
+    income: 2000,
+    expense: 9800,
+  },
+  {
+    name: "Apr",
+    income: 2780,
+    expense: 3908,
+  },
+  {
+    name: "May",
+    income: 1890,
+    expense: 4800,
+  },
+  {
+    name: "Jun",
+    income: 2390,
+    expense: 3800,
+  },
+  {
+    name: "Jul",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Aug",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Sep",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Oct",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Nov",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Dec",
+    income: 3490,
+    expense: 4300,
+  },
+];
 const FinanceChart = () => {
-  return <div>FinanceChart</div>;
+  return (
+    <div className="bg-white rounded-xl w-full h-full">
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Finance</h1>
+        <Image
+          className=" pr-2"
+          src={"/moreDark.png"}
+          alt=""
+          width={20}
+          height={20}
+        />
+      </div>
+
+      <ResponsiveContainer width="100%" height="90%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" stroke="#ddd" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tick={{ fill: "#d1d5db" }}
+            tickLine={false}
+            tickMargin={20}
+          />
+          <YAxis
+            axisLine={false}
+            tick={{ fill: "#d1d5db" }}
+            tickMargin={10}
+            tickLine={false}
+          />
+          <Tooltip />
+          <Legend
+            align="center"
+            verticalAlign="top"
+            wrapperStyle={{ paddingTop: "10px", paddingBottom: "30px" }}
+          />
+          <Line
+            type="monotone"
+            dataKey="income"
+            stroke="#c3ebfa"
+            strokeWidth={5}
+          />
+          <Line
+            type="monotone"
+            strokeWidth={5}
+            dataKey="expense"
+            stroke="#cfceff"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default FinanceChart;
