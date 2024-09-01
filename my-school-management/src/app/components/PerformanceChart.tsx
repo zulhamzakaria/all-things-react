f331cda894a98e1d339cf91e6f26fe11435c4941
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-02 00:13:44 +0800

    - stylings

--- my-school-management/src/app/components/PerformanceChart.tsx
+++ my-school-management/src/app/components/PerformanceChart.tsx
@@ -9,13 +9,13 @@ const data = [
 
 const PerformanceChart = () => {
   return (
-    <div className="bg-white p-4 rounded-md h-80">
+    <div className="bg-white p-4 rounded-md h-80 relative">
       <div className="flex items-center justify-between">
         <h1 className=" text-xl font-semibold  ">Performance</h1>
         <Image src="/moreDark.png" alt="" width={16} height={16} />
       </div>
       <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
+        <PieChart>
           <Pie
             dataKey="value"
             startAngle={180}
@@ -23,12 +23,16 @@ const PerformanceChart = () => {
             data={data}
             cx="50%"
             cy="50%"
-            outerRadius={80}
+            innerRadius={70}
             fill="#8884d8"
-            label
           />
         </PieChart>
       </ResponsiveContainer>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 className=" text-3xl font-bold">9.2</h1>
+        <p className="text-sm text-gray-300">of 10 max LTS</p>
+      </div>
+      <h2 className=" font-medium absolute bottom-16 left-0 right-0 m-auto text-center">1st Semester - 2nd Semester</h2>
     </div>
   );
 };
