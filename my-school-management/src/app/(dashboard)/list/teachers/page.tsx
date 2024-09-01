c2bd8c380e6383f0e98e0275505e6dd1fbf59193
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-01 19:58:18 +0800

    - rbac display condition

--- my-school-management/src/app/(dashboard)/list/teachers/page.tsx
+++ my-school-management/src/app/(dashboard)/list/teachers/page.tsx
@@ -50,7 +50,10 @@ const columns = [
 
 const TeacherPage = () => {
   const renderRow = (item: Teacher) => (
-    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
       <td className="flex items-center gap-4 p-4">
         <Image
           src={item.photo}
@@ -104,9 +107,11 @@ const TeacherPage = () => {
             <button className="w-8 h-8 bg-lamaYellow flex items-center justify-center rounded-full">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 bg-lamaYellow flex items-center justify-center rounded-full">
-              <Image src="/plus.png" alt="" width={14} height={14} />
-            </button>
+            {role === "admin" && (
+              <button className="w-8 h-8 bg-lamaYellow flex items-center justify-center rounded-full">
+                <Image src="/plus.png" alt="" width={14} height={14} />
+              </button>
+            )}
           </div>
         </div>
       </div>
