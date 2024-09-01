19277b5d68b72c0180c4c05bd96765c78601f8fc
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-01 23:56:47 +0800

    - Teacher;s details page (WIP)

--- my-school-management/src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ my-school-management/src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,4 +1,7 @@
+import Announcement from "@/app/components/Announcement";
+import BigCalendar from "@/app/components/BigCalendar";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const SingleTeacherPage = () => {
@@ -16,19 +19,135 @@ const SingleTeacherPage = () => {
                 alt=""
                 width={144}
                 height={144}
-                className="w-36 h-36 rounded-full object-cover"
+                className="w-40 h-32 rounded-full object-cover"
               />
             </div>
-            <div className="w-2/3"></div>
+            <div className="w-2/3 flex flex-col justify-between gap-4">
+              <h1 className="text-xl font-semibold">Janna Nick</h1>
+              <p className="text-sm text-gray-400">
+                Janna Nick is a Malaysian actress, singer, host, director and
+                producer.
+              </p>
+              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                <div className="w-full md:w-1/3 2xl:w-1/3 lg:w-full flex items-center gap-2 ">
+                  <Image
+                    src={"/blood.png"}
+                    alt="blood"
+                    width={14}
+                    height={14}
+                  />
+                  <span>A+</span>
+                </div>
+                <div className="w-full lg:w-full 2xl:w-1/3 md:w-1/3 flex items-center gap-2 ">
+                  <Image src={"/date.png"} alt="date" width={14} height={14} />
+                  <span>Jan 2025</span>
+                </div>
+                <div className="w-full lg:w-full md:w-1/3 2xl:w-1/3 flex items-center gap-2 ">
+                  <Image src={"/mail.png"} alt="mail" width={14} height={14} />
+                  <span>jn@mycelebs.com</span>
+                </div>
+                <div className="w-full lg:w-full md:w-1/3 2xl:w-1/3 flex items-center gap-2 ">
+                  <Image
+                    src={"/phone.png"}
+                    alt="phone"
+                    width={14}
+                    height={14}
+                  />
+                  <span>0199999999</span>
+                </div>
+              </div>
+            </div>
           </div>
           {/* small cards */}
-          <div className="flex-1 "></div>
+          <div className="flex-1 flex gap-4 justify-between flex-wrap ">
+            {/* card */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src={"/singleAttendance.png"}
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div>
+                <h1 className="text-xl font-semibold">90%</h1>
+                <span className="text-sm text-gray-500">Attendance</span>
+              </div>
+            </div>
+            {/* card */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full  md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src={"/singleBranch.png"}
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div>
+                <h1 className="text-xl font-semibold">2</h1>
+                <span className="text-sm text-gray-500">Branches</span>
+              </div>
+            </div>
+            {/* card */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full  md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src={"/singleLesson.png"}
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div>
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-500">Lessons</span>
+              </div>
+            </div>
+            {/* card */}
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full  md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src={"/singleClass.png"}
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div>
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm text-gray-500">Classes</span>
+              </div>
+            </div>
+          </div>
         </div>
         {/* bottom */}
-        <div>Schedule</div>
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+          <h1>Teacher's Schedule</h1>
+          <BigCalendar />
+        </div>
       </div>
       {/* right */}
-      <div className="w-full xl:w-1/3"></div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md ">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+              Teacher's Classes
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaPurpleLight" href="/">
+              Teacher's Students
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaYellowLight" href="/">
+              Teacher's Lessons
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="/">
+              Teacher's Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">
+              Teacher's Assignments
+            </Link>
+          </div>
+        </div>
+        <Announcement />
+      </div>
     </div>
   );
 };
