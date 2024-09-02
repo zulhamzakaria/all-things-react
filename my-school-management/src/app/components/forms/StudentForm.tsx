385ed812942dd99eb1c28e2b27e34884a06f8c3d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-02 22:08:24 +0800

    - student form

---
+++ my-school-management/src/app/components/forms/StudentForm.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FieldError, useForm } from "react-hook-form";
+import { z, ZodSchema } from "zod";
+import InputField from "../InputField";
+import Image from "next/image";
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 chars long" })
+    .max(20, { message: "Username must be at most 20 chars long" }),
+  email: z.string().email({ message: "Invalid email address format" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 chars long" }),
+  firstName: z.string().min(1, { message: "First name is required" }),
+  lastName: z.string().min(1, { message: "Last name is required" }),
+  phone: z.string().min(1, { message: "Phone is required" }),
+  address: z.string().min(1, { message: "Address is required" }),
+  bloodType: z.string().min(1, { message: "Bloodtype is required" }),
+  birthDay: z.date({ message: "Birthday is required" }),
+  sex: z.enum(["male", "female", "other"], { message: "Sex is required" }),
+  image: z.instanceof(File, { message: "Image is required" }),
+});
+
+type Inputs = z.infer<typeof schema>;
+
+const StudentForm = ({
+  requestType,
+  data,
+}: {
+  requestType: "create" | "update";
+  data?: any;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({ resolver: zodResolver(schema) });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <form className=" flex flex-col gap-8" onSubmit={onSubmit}>
+      <h1 className=" text-xl font-semibold">Create a new student</h1>
+      <span className="text-xs text-gray-400 font-medium">
+        Authentication Information
+      </span>
+      <div className="flex justify-between flex-wrap gap-4">
+        <InputField
+          label="Username"
+          name="username"
+          defaultValue={data?.username}
+          register={register}
+          error={errors?.username}
+        />
+        <InputField
+          label="Email"
+          name="email"
+          type="email"
+          defaultValue={data?.email}
+          register={register}
+          error={errors?.email}
+        />
+        <InputField
+          label="Password"
+          name="password"
+          type="password"
+          defaultValue={data?.password}
+          register={register}
+          error={errors?.password}
+        />
+      </div>
+      <span className=" text-xs text-gray-400 font-medium">
+        Personal Informartion
+      </span>
+
+      <div className=" flex justify-between flex-wrap gap-4">
+        <InputField
+          label="First Name"
+          name="firstname"
+          defaultValue={data?.firstName}
+          register={register}
+          error={errors?.firstName}
+        />
+        <InputField
+          label="Last Name"
+          name="lastname"
+          defaultValue={data?.lastName}
+          register={register}
+          error={errors?.lastName}
+        />
+        <InputField
+          label="Phone"
+          name="phone"
+          defaultValue={data?.phone}
+          register={register}
+          error={errors?.phone}
+        />
+        <InputField
+          label="Address"
+          name="address"
+          defaultValue={data?.address}
+          register={register}
+          error={errors?.address}
+        />
+        <InputField
+          label="Blood Type"
+          name="bloodtype"
+          defaultValue={data?.bloodtype}
+          register={register}
+          error={errors?.bloodType}
+        />
+        <InputField
+          label="Birth Day"
+          name="birthDay"
+          defaultValue={data?.birthDay}
+          register={register}
+          error={errors?.birthDay}
+          type="date"
+        />
+      </div>
+
+      <div className=" flex justify-between flex-wrap gap-4">
+        <div className=" flex flex-col gap-2 w-full md:w-1/4">
+          <label className=" text-xs text-gray-500">Sex</label>
+          <select
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            {...register("sex")}
+            defaultValue={data?.sex}
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          {errors.sex?.message && (
+            <p className=" text-xs font-thin text-red-400">
+              {errors.sex?.message.toString()}
+            </p>
+          )}
+        </div>
+        <div className=" flex flex-col gap-2 w-full md:w-1/4 justify-center">
+          <label
+            className=" text-xs text-gray-500 flex items-center gap-2 cursor-pointer"
+            htmlFor="image"
+          >
+            <Image src={"/upload.png"} alt="" width={28} height={28} />
+            <span>Upload a photo</span>
+          </label>
+          <input
+            id="image"
+            type="file"
+            {...register("image")}
+            className="hidden"
+          />
+          {errors.image?.message && (
+            <p className=" text-xs font-thin text-red-400">
+              {errors.image?.message.toString()}
+            </p>
+          )}
+        </div>
+      </div>
+      <button className=" bg-black text-white p-2 rounded-md">
+        {requestType === "create" ? "Create" : "Update"}
+      </button>
+    </form>
+  );
+};
+
+export default StudentForm;
