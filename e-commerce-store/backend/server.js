20f7048fa0c37f3eb7f9277d914d9ddd0c5e9ffe
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-04 15:16:07 +0800

    - 'dev' setting allows for the app to be refresh automatically after each change - dotenv is required to see the port gotten from .env

--- e-commerce-store/backend/server.js
+++ e-commerce-store/backend/server.js
@@ -1,1 +1,12 @@
 // entry file
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT ?? 5000;
+
+app.listen(PORT, () => {
+  console.log("server is running on port 5000");
+});
