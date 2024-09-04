b484fbedb892112642367a87daacbae3e231808e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-04 15:24:33 +0800

    - replicated routes

--- e-commerce-store/backend/routes/auth.route.js
+++ e-commerce-store/backend/routes/auth.route.js
@@ -5,5 +5,11 @@ const router = express.Router();
 router.get("/signup", (req, res) => {
   res.send("signup route called");
 });
+router.get("/signin", (req, res) => {
+  res.send("signin route called");
+});
+router.get("/signout", (req, res) => {
+  res.send("signout route called");
+});
 
 export default router;
