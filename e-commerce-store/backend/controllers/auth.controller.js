ef90b80a850fc93afe987d386a0368975b01f229
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-04 15:27:29 +0800

    - controller file for housing code for auth.route.js

---
+++ e-commerce-store/backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = async (_req, res) => {
+  res.send("sign-up endpoint called");
+};
+export const signin = async (_req, res) => {
+  res.send("sign-in endpoint called");
+};
+export const signout = async (_req, res) => {
+  res.send("signout endpoint called");
+};
