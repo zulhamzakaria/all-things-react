0b37edde4d9ae78dd1ed2516c335e44b06e4d515
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-04 20:18:34 +0800

    - change verb to post

--- e-commerce-store/backend/routes/auth.route.js
+++ e-commerce-store/backend/routes/auth.route.js
@@ -3,8 +3,8 @@ import { signin, signout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", await signup);
-router.get("/signin", await signin);
-router.get("/signout", await signout);
+router.post("/signup", await signup);
+router.post("/signin", await signin);
+router.post("/signout", await signout);
 
 export default router;
