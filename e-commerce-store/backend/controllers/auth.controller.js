05b30d6f1ff8afca85d406934bdae941b702b56e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-04 20:30:02 +0800

    -try-catch from error handlling

--- e-commerce-store/backend/controllers/auth.controller.js
+++ e-commerce-store/backend/controllers/auth.controller.js
@@ -2,12 +2,17 @@ import User from "../models/user.model.js";
 
 export const signup = async (_req, res) => {
   const { email, password, name } = req.body;
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    return res.status(400).json({ message: "User already exists" });
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({ name, email, password });
+    res.status(201).json({ user, message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  const user = await User.create({ name, email, password });
-  res.status(201).json({ user, message: "User created successfully" });
 };
 export const signin = async (_req, res) => {
   res.send("sign-in endpoint called");
