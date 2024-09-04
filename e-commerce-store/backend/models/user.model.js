46d7777e9786b50506dbb3cdf2d8912f199aa766
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-04 17:29:18 +0800

    - password hasing and comparer

--- e-commerce-store/backend/models/user.model.js
+++ e-commerce-store/backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -40,3 +41,25 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+const User = mongoose.model("User", userSchema);
+
+// Pre-save hook to hash pwd before saving to database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next;
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    // calls next function
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+export default User;
