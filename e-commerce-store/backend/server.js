0fda3ca615fff7adf5f2c2ae4556f6c35c08663b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-04 15:22:58 +0800

    - basic api route - calling api route from servr.js

--- e-commerce-store/backend/server.js
+++ e-commerce-store/backend/server.js
@@ -1,11 +1,14 @@
 // entry file
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on port 5000");
