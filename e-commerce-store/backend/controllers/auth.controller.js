41d78de273d7b32fe20c36896fcb98043d824698
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-04 20:24:40 +0800

    - enpoint for adding user to the db

--- e-commerce-store/backend/controllers/auth.controller.js
+++ e-commerce-store/backend/controllers/auth.controller.js
@@ -1,6 +1,13 @@
+import User from "../models/user.model.js";
+
 export const signup = async (_req, res) => {
   const { email, password, name } = req.body;
-  res.send("sign-up endpoint called");
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  const user = await User.create({ name, email, password });
+  res.status(201).json({ user, message: "User created successfully" });
 };
 export const signin = async (_req, res) => {
   res.send("sign-in endpoint called");
