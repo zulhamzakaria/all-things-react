6755e751259910805d82d9b310e3f82f31133ab8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-05 20:45:14 +0800

    - get userSession - added signout button

--- blog-saas/app/page.tsx
+++ blog-saas/app/page.tsx
@@ -2,17 +2,29 @@ import { Button } from "@/components/ui/button";
 import {
   RegisterLink,
   LoginLink,
+  LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const userSession = await getUser();
   return (
-    <div className="flex gap-2">
-      <RegisterLink>
-        <Button>Register</Button>
-      </RegisterLink>
-      <LoginLink>
-        <Button>Login</Button>
-      </LoginLink>
+    <div className="flex gap-2 p-2">
+      {userSession ? (
+        <LogoutLink>
+          <Button>Signout</Button>
+        </LogoutLink>
+      ) : (
+        <div>
+          <RegisterLink>
+            <Button>Signup</Button>
+          </RegisterLink>
+          <LoginLink>
+            <Button>Signin</Button>
+          </LoginLink>
+        </div>
+      )}
     </div>
   );
 }
