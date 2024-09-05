b74938ae5cedbf05e363f06662899d0886692f1a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-05 23:33:55 +0800

    - basic Sites component

---
+++ blog-saas/app/dashboard/sites/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const SitesPage = () => {
+  return (
+    <>
+      <div className=" flex w-full justify-end">
+        <Button>Create site</Button>
+      </div>
+    </>
+  );
+};
+
+export default SitesPage;
