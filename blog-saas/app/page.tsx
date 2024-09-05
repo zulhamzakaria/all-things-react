9448f3a4179d39100962d7482b468d0d3290c2d1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-05 20:38:24 +0800

    - implementing register and login with kinde

--- blog-saas/app/page.tsx
+++ blog-saas/app/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
     <div>
       <h1>hello, world</h1>
-      <Button>hello from button</Button>
+      <RegisterLink>
+        <Button>Register</Button>
+      </RegisterLink>
+      <LoginLink>
+        <Button>Login</Button>
+      </LoginLink>
     </div>
   );
 }
