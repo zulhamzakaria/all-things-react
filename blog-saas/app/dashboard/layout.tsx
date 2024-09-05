01b163bf2c2843caa8c4f7a49d326658c81219ac
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-05 23:30:54 +0800

    - calling children

--- blog-saas/app/dashboard/layout.tsx
+++ blog-saas/app/dashboard/layout.tsx
@@ -72,9 +72,10 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
             </DropdownMenu>
           </div>
         </header>
+        <main className=" flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          {children}
+        </main>
       </div>
-
-      {/* {children} */}
     </section>
   );
 };
