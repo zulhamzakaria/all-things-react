9d4caa8a76dc5f723a244c99cbfed3dbf2360868
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-06 00:13:39 +0800

    - stylings

--- blog-saas/app/dashboard/sites/new/page.tsx
+++ blog-saas/app/dashboard/sites/new/page.tsx
@@ -1,15 +1,19 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const NewSiteRoute = () => {
   return (
     <div className=" flex flex-col flex-1 items-center justify-center">
-      <Card>
+      <Card className=" max-w-[450px]">
         <CardHeader>
           <CardTitle>Create Site</CardTitle>
           <CardDescription>
@@ -18,9 +22,23 @@ const NewSiteRoute = () => {
         </CardHeader>
         <CardContent>
           <div className=" flex flex-col gap-y-6">
-            <div></div>
+            <div className=" grid gap-2">
+              <label>Site Name</label>
+              <Input placeholder="Site name" />
+            </div>
+            <div className=" grid gap-2">
+              <label>Subdirectory</label>
+              <Input placeholder="Subdirectory" />
+            </div>
+            <div className=" grid gap-2">
+              <label>Description</label>
+              <Textarea placeholder="Small description of your site" />
+            </div>
           </div>
         </CardContent>
+        <CardFooter>
+          <Button className=" ml-auto">Submit</Button>
+        </CardFooter>
       </Card>
     </div>
   );
