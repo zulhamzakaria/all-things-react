741a1a0f1c1df6094107fa114c0c561776dd4450
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-06 00:02:45 +0800

    - WIP

--- blog-saas/app/dashboard/sites/new/page.tsx
+++ blog-saas/app/dashboard/sites/new/page.tsx
@@ -1,4 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const NewSiteRoute = () => {
-  return <div>new site!</div>;
+  return (
+    <div className=" flex flex-col flex-1 items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Create Site</CardTitle>
+          <CardDescription>
+            Create your site here. Click 'save' once you are done
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className=" flex flex-col gap-y-6">
+            <div></div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 export default NewSiteRoute;
