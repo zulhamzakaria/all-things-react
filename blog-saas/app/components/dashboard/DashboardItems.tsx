61d20e27a6d1fec719adc78d1cc2c95c5af92586
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-05 22:46:40 +0800

    - sidebar links/items

--- blog-saas/app/components/dashboard/DashboardItems.tsx
+++ blog-saas/app/components/dashboard/DashboardItems.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { navLinks } from "@/app/dashboard/layout";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export function DashboardItems() {
-  return <div>sidebar</div>;
+  const pathName = usePathname();
+  return (
+    <>
+      {navLinks.map((link) => (
+        <Link
+          href={link.href}
+          key={link.name}
+          className={cn(
+            pathName == link.href
+              ? "bg-muted text-blue-500"
+              : " text-muted-foreground bg-none",
+            "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary/90"
+          )}
+        >
+          <link.icon className=" size-4" />
+          {link.name}
+        </Link>
+      ))}
+    </>
+  );
 }
