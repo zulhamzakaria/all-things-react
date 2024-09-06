1c63bb03bc731f086f777fc4864318cfe8af895b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-06 23:07:32 +0800

    - implementing create site server action (WIP)

--- blog-saas/app/dashboard/sites/new/page.tsx
+++ blog-saas/app/dashboard/sites/new/page.tsx
@@ -1,3 +1,4 @@
+import { CreateSiteAction } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,8 +10,11 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useActionState } from "react";
 
 const NewSiteRoute = () => {
+  // for getting state from server action?
+  const [lastResult, action] = useActionState(CreateSiteAction, undefined);
   return (
     <div className=" flex flex-col flex-1 items-center justify-center">
       <Card className=" max-w-[450px]">
@@ -20,25 +24,27 @@ const NewSiteRoute = () => {
             Create your site here. Click 'save' once you are done
           </CardDescription>
         </CardHeader>
-        <CardContent>
-          <div className=" flex flex-col gap-y-6">
-            <div className=" grid gap-2">
-              <label>Site Name</label>
-              <Input placeholder="Site name" />
+        <form>
+          <CardContent>
+            <div className=" flex flex-col gap-y-6">
+              <div className=" grid gap-2">
+                <label>Site Name</label>
+                <Input placeholder="Site name" />
+              </div>
+              <div className=" grid gap-2">
+                <label>Subdirectory</label>
+                <Input placeholder="Subdirectory" />
+              </div>
+              <div className=" grid gap-2">
+                <label>Description</label>
+                <Textarea placeholder="Small description of your site" />
+              </div>
             </div>
-            <div className=" grid gap-2">
-              <label>Subdirectory</label>
-              <Input placeholder="Subdirectory" />
-            </div>
-            <div className=" grid gap-2">
-              <label>Description</label>
-              <Textarea placeholder="Small description of your site" />
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className=" ml-auto">Submit</Button>
-        </CardFooter>
+          </CardContent>
+          <CardFooter>
+            <Button className=" ml-auto">Submit</Button>
+          </CardFooter>
+        </form>
       </Card>
     </div>
   );
