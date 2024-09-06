2ac891edd880903ca89372d0988e17cb9246d0c1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-06 22:10:31 +0800

    - route handler for saving user data to db

--- blog-saas/app/api/auth/creation/route.ts
+++ blog-saas/app/api/auth/creation/route.ts
@@ -1,4 +1,6 @@
+import prisma from "@/app/utils/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const { getUser } = getKindeServerSession();
@@ -6,4 +8,24 @@ export async function GET() {
   if (!user || user === null || !user.id) {
     throw new Error("Something went wrong");
   }
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+      },
+    });
+  }
+
+  return NextResponse.redirect("http://localhost:3000/dashboard");
 }
