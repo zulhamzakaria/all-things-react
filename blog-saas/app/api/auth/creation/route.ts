a6ee5961e507c84397e148f9a09c41058dfd3a16
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-06 22:00:07 +0800

    - kinde hook for saving data to DB (WIP)

---
+++ blog-saas/app/api/auth/creation/route.ts
@@ -0,0 +1,9 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export async function GET() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user || user === null || !user.id) {
+    throw new Error("Something went wrong");
+  }
+}
