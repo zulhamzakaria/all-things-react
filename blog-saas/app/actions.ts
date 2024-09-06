b931f1f388f0138d17a8faca6f6453be23ab92b2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-06 22:34:29 +0800

    - for site creation (WIP)

--- blog-saas/app/actions.ts
+++ blog-saas/app/actions.ts
@@ -1,12 +1,20 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import { parseWithZod } from "@conform-to/zod";
+import { SiteSchema } from "./utils/zodSchemas";
 
-export async function CreateSiteAction() {
+export async function CreateSiteAction(formData: FormData) {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
   if (!user) {
     return redirect("/api/auth/login");
   }
 
-  
+  const submission = parseWithZod(formData, {
+    schema: SiteSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
 }
