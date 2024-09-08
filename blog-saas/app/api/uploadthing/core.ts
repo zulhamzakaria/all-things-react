ba0c3d4ab9e3fcb361157ce34e3f52b76e3a0b21
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-08 15:44:58 +0800

    - get authenticated user - maxfileCount

--- blog-saas/app/api/uploadthing/core.ts
+++ blog-saas/app/api/uploadthing/core.ts
@@ -1,18 +1,17 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
 
       // If you throw, the user will not be able to upload
       if (!user) throw new UploadThingError("Unauthorized");
