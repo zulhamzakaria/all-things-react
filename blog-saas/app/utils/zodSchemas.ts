170fd0135a1048a13698ca88eb3d9908e410f424
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-08 22:00:15 +0800

    - zod article schema

--- blog-saas/app/utils/zodSchemas.ts
+++ blog-saas/app/utils/zodSchemas.ts
@@ -5,3 +5,11 @@ export const SiteSchema = z.object({
   description: z.string().min(1).max(150),
   subdirectory: z.string().min(1).max(45),
 });
+
+export const ArticleSchema = z.object({
+  title: z.string().min(1).max(100),
+  slug: z.string().min(1).max(190),
+  imageUrl: z.string().min(1),
+  smallDescription: z.string().min(1).max(200),
+  articleContent: z.string().min(1),
+});
