c1082c7063b7492030427b15eebdcf036f2888cb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-08 17:58:44 +0800

    - applying extensions

--- blog-saas/app/components/dashboard/EditorWrapper.tsx
+++ blog-saas/app/components/dashboard/EditorWrapper.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { EditorContent, EditorRoot, JSONContent } from "novel";
+import { defaultExtensions } from "./extensions";
 
 interface EditorProps {
   initialValue?: JSONContent;
   onChange: (value: JSONContent) => void;
 }
 
+const extensions = [...defaultExtensions];
+
 const TailwindEditor = ({ initialValue, onChange }: EditorProps) => {
   return (
     <EditorRoot>
       <EditorContent
+        extensions={extensions}
         initialContent={initialValue}
         onUpdate={({ editor }) => {
           const json = editor.getJSON();
