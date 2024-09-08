ab881a9eb318fb36c1539ff2e68accd091883b7c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-08 14:55:32 +0800

    - copied from Sites page (WIP)

--- blog-saas/app/dashboard/sites/[siteId]/page.tsx
+++ blog-saas/app/dashboard/sites/[siteId]/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/app/utils/db";
 import { Button } from "@/components/ui/button";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { Book, PlusCircle, Settings } from "lucide-react";
+import { Book, FileIcon, PlusCircle, Settings } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -55,7 +55,22 @@ const Site = async ({ params }: { params: { siteId: string } }) => {
         </Button>
       </div>
       {posts === undefined || posts.length === 0 ? (
-        <h1>no post</h1>
+        <div className=" flex flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
+          <div className=" flex size-20 items-center justify-center rounded-full bg-primary/10">
+            <FileIcon className=" size-10 text-primary " />
+          </div>
+          <h2 className=" mt-6 text-xl font-semibold">
+            You have 0 site created
+          </h2>
+          <p className=" max-w-sm mx-auto mb-8 text-center text-sm leading-tight text-muted-foreground">
+            Please create some so that you can see them right here
+          </p>
+          <Button asChild>
+            <Link href={"/dashboard/sites/new"}>
+              <PlusCircle className=" size-4 mr-2" /> Site
+            </Link>
+          </Button>
+        </div>
       ) : (
         <h1>data</h1>
       )}
