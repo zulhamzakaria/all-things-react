a0b2a376e5ed08eb96180636a73d5fc56eb6a81a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-08 15:32:17 +0800

    - form and stylings (WIP)

--- blog-saas/app/dashboard/sites/[siteId]/create/page.tsx
+++ blog-saas/app/dashboard/sites/[siteId]/create/page.tsx
@@ -1,3 +1,68 @@
-export default function CreateArticle() {
-  return <h1>Create an article</h1>;
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import { ArrowLeft, Atom } from "lucide-react";
+import Link from "next/link";
+
+export default function CreateArticle({
+  params,
+}: {
+  params: { siteId: string };
+}) {
+  return (
+    <>
+      <div className=" flex items-center">
+        {/* asChild cause it has a Link */}
+        <Button
+          size={"icon"}
+          variant={"outline"}
+          asChild
+          className=" mr-3 hover:bg-primary"
+        >
+          <Link href={`/dashboard/sites/${params.siteId}`}>
+            <ArrowLeft className=" size-4 hover:text-black" />
+          </Link>
+        </Button>
+        <h1 className=" text-xl font-semibold">Create Article</h1>
+      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Article Details</CardTitle>
+          <CardDescription>Article Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className=" flex flex-col gap-6">
+            <div className=" grid gap-2">
+              <Label>Title</Label>
+              <Input placeholder="Blogging app powered by nextJs" />
+            </div>
+            <div className=" grid gap-2">
+              <Label>Slug</Label>
+              <Input placeholder="Article slug" />
+              {/* type button so it's not mistaken for a submit button */}
+              <Button className=" w-fit" variant={"secondary"} type="button">
+                <Atom className=" size-4 mr-2" />
+                Generate slug
+              </Button>
+            </div>
+            <div className=" grid gap-2">
+              <Label>Small Description</Label>
+              <Textarea
+                placeholder="Small Description for the blog"
+                className="h-32"
+              />
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
