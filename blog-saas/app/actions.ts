69a59e96537a49d9ce3a34c0f82c133c8f5d1477
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-08 22:13:58 +0800

    - server action for creating article/post

--- blog-saas/app/actions.ts
+++ blog-saas/app/actions.ts
@@ -3,15 +3,12 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import { parseWithZod } from "@conform-to/zod";
-import { SiteSchema } from "./utils/zodSchemas";
+import { ArticleSchema, SiteSchema } from "./utils/zodSchemas";
 import prisma from "./utils/db";
+import { requireUser } from "./utils/requireUser";
 
 export async function CreateSiteAction(previousState: any, formData: FormData) {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-  if (!user) {
-    return redirect("/api/auth/login");
-  }
+  const user = await requireUser();
 
   const submission = parseWithZod(formData, {
     schema: SiteSchema,
@@ -33,3 +30,27 @@ export async function CreateSiteAction(previousState: any, formData: FormData) {
   return redirect("/dashboard/sites");
 }
 
+export async function CreatePostAction(formData: FormData) {
+  const user = await requireUser();
+  const submission = parseWithZod(formData, {
+    schema: ArticleSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data = await prisma.post.create({
+    data: {
+      title: submission.value.title,
+      smallDescription: submission.value.smallDescription,
+      slug: submission.value.slug,
+      articleContent: JSON.parse(submission.value.articleContent),
+      imageUrl: submission.value.imageUrl,
+      userId: user.id,
+      siteId: formData.get("siteId") as string,
+    },
+  });
+
+  return redirect("/dashboard/sites");
+}
