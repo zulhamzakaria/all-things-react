f3c1a9f0280a0f903120b848ffe48e2a362d8793
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-08 18:05:27 +0800

    -added slashcommand items

--- blog-saas/app/components/dashboard/EditorWrapper.tsx
+++ blog-saas/app/components/dashboard/EditorWrapper.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-import { EditorContent, EditorRoot, JSONContent } from "novel";
+import {
+  EditorCommand,
+  EditorCommandEmpty,
+  EditorCommandItem,
+  EditorCommandList,
+  EditorContent,
+  EditorRoot,
+  JSONContent,
+} from "novel";
 import { defaultExtensions } from "./extensions";
-import { slashCommand } from "./SlashCommand";
+import { slashCommand, suggestionItems } from "./SlashCommand";
 
 interface EditorProps {
   initialValue?: JSONContent;
@@ -21,7 +29,33 @@ const TailwindEditor = ({ initialValue, onChange }: EditorProps) => {
           const json = editor.getJSON();
           onChange(json);
         }}
-      />
+      >
+        <EditorCommand className="z-50 h-auto max-h-[330px]  w-72 overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all">
+          <EditorCommandEmpty className="px-2 text-muted-foreground">
+            No results
+          </EditorCommandEmpty>
+          <EditorCommandList>
+            {suggestionItems.map((item) => (
+              <EditorCommandItem
+                value={item.title}
+                onCommand={(val) => item.command?.(val)}
+                className={`flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent `}
+                key={item.title}
+              >
+                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background">
+                  {item.icon}
+                </div>
+                <div>
+                  <p className="font-medium">{item.title}</p>
+                  <p className="text-xs text-muted-foreground">
+                    {item.description}
+                  </p>
+                </div>
+              </EditorCommandItem>
+            ))}
+          </EditorCommandList>
+        </EditorCommand>
+      </EditorContent>
     </EditorRoot>
   );
 };
