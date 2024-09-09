9cc979d865baf7373923d82fe19ef629a59c4de9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-09 17:54:30 +0800

    - WIP

--- blog-saas/app/dashboard/sites/[siteId]/[articleId]/delete/page.tsx
+++ blog-saas/app/dashboard/sites/[siteId]/[articleId]/delete/page.tsx
@@ -1,7 +1,40 @@
-export default function DeleteArticle() {
+import { SubmitButton } from "@/app/components/dashboard/SubmitButton";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+export default function DeleteArticle({
+  params,
+}: {
+  params: {
+    siteId: string;
+    articleId: string;
+  };
+}) {
   return (
-    <div>
-      <h1>Delete article</h1>
+    <div className=" flex flex-1 items-center justify-center">
+      <Card className=" max-w-xl">
+        <CardHeader>
+          <CardTitle>Are you sure?</CardTitle>
+          <CardDescription>
+            This action will delete the data from the database and irreversible
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className=" w-full flex justify-between">
+          <Button variant={"secondary"} asChild>
+            <Link href={`/dashboard/sites/${params.siteId}`}>Cancel</Link>
+          </Button>
+          <form>
+            <SubmitButton text="Delete Article" variant={"destructive"} />
+          </form>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
