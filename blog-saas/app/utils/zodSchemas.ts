73f13b3916633774530235189dc97377ca82d3f4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-09 23:14:43 +0800

    - zodSchema with condition for subirectory uniqueness

--- blog-saas/app/utils/zodSchemas.ts
+++ blog-saas/app/utils/zodSchemas.ts
@@ -1,3 +1,4 @@
+import { conformZodMessage } from "@conform-to/zod";
 import { z } from "zod";
 
 export const SiteSchema = z.object({
@@ -13,3 +14,38 @@ export const ArticleSchema = z.object({
   smallDescription: z.string().min(1).max(200),
   articleContent: z.string().min(1),
 });
+
+export function SiteCreationSchema(options?: {
+  isSubdirectoryUnique: () => Promise<boolean>;
+}) {
+  return z.object({
+    subdirectory: z
+      .string()
+      .min(1)
+      .max(45)
+      .regex(/^[a-z]+$/, "Subdirectory must only be in lowercase")
+      .transform((value) => value.toLowerCase())
+      .pipe(
+        z.string().superRefine((email, ctx) => {
+          if (typeof options?.isSubdirectoryUnique !== "function") {
+            ctx.addIssue({
+              code: "custom",
+              message: conformZodMessage.VALIDATION_UNDEFINED,
+              fatal: true,
+            });
+            return;
+          }
+          return options.isSubdirectoryUnique().then((isUnique) => {
+            if (!isUnique) {
+              ctx.addIssue({
+                code: "custom",
+                message: "Subdirectory is already taken",
+              });
+            }
+          });
+        })
+      ),
+    name: z.string().min(3).max(35),
+    description: z.string().min(1).max(150),
+  });
+}
