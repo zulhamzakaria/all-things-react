c9d141a997e2a609be1fbfc07acf846df5352d52
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-10 15:04:19 +0800

    - get Post data based on slug

--- blog-saas/app/blog/[name]/[slug]/page.tsx
+++ blog-saas/app/blog/[name]/[slug]/page.tsx
@@ -1,3 +1,27 @@
-export default function SlugPage() {
-  return <h1>slug page</h1>;
+import prisma from "@/app/utils/db";
+import { notFound } from "next/navigation";
+
+async function getPost(slug: string) {
+  const data = await prisma.post.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      title: true,
+      imageUrl: true,
+      smallDescription: true,
+      articleContent: true,
+    },
+  });
+  if (!data) return notFound();
+  return data;
+}
+
+export default async function SlugPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const post = await getPost(params.slug);
+  return <h1>{post.imageUrl}</h1>;
 }
