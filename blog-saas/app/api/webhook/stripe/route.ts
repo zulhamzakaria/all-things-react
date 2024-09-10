f9d86446d53022013cb3fc2ad68f2eb2f68d8ff8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-10 23:55:19 +0800

    - event listener for ivoice

--- blog-saas/app/api/webhook/stripe/route.ts
+++ blog-saas/app/api/webhook/stripe/route.ts
@@ -48,4 +48,25 @@ export async function POST(req: Request) {
       },
     });
   }
+
+  //invoice event after a successful subscription
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription as string
+    );
+
+    await prisma.subscription.update({
+      where: {
+        stripeSubscriptionId: subscription.id,
+      },
+      data: {
+        planId: subscription.items.data[0].price.id,
+        currentPeriodStart: subscription.current_period_start,
+        currentPeriodEnd: subscription.current_period_end,
+        status: subscription.status,
+      },
+    });
+  }
+
+  return new Response(null, { status: 200 });
 }
