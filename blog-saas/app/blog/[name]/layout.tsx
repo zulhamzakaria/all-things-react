1cd5dc511a693f02ea5bd825ee74c220d4e132aa
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-10 13:54:26 +0800

    - blog layout

---
+++ blog-saas/app/blog/[name]/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+const BlogLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
+        {children}
+    </main>
+  );
+};
+
+export default BlogLayout;
