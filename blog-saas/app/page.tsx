61ea20827ecf05e33601b77fe47c7efd4b26b213
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-11 20:35:23 +0800

    - redirect signed in user to dashboard

--- blog-saas/app/page.tsx
+++ blog-saas/app/page.tsx
@@ -4,10 +4,16 @@ import { Hero } from "./components/frontend/Hero";
 import Logos from "./components/frontend/Logos";
 import { PricingPlans } from "./components/shared/PricingTable";
 import PricingPage from "./dashboard/pricing/page";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const { getUser } = getKindeServerSession();
   const userSession = await getUser();
+
+  if (userSession?.id) {
+    return redirect("/dashboard");
+  }
+
   return (
     <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
       <Hero />
