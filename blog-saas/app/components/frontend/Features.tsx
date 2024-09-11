8fb3b6cf981c03db22741d6868f8a6362d4f104a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-11 20:15:40 +0800

    - Features component - import Features from app page

---
+++ blog-saas/app/components/frontend/Features.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div className=" py-24 sm:py-32">
+      <div className=" max-w-2xl mx-auto lg:text-center">
+        <p>Blog Faster</p>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
--- blog-saas/app/page.tsx
+++ blog-saas/app/page.tsx
@@ -7,6 +7,7 @@ import {
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Hero } from "./components/frontend/Hero";
 import Logos from "./components/frontend/Logos";
+import Features from "./components/frontend/Features";
 
 export default async function Home() {
   const { getUser } = getKindeServerSession();
@@ -14,7 +15,8 @@ export default async function Home() {
   return (
     <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
       <Hero />
-      <Logos/>
+      <Logos />
+      <Features />
     </div>
   );
 }
