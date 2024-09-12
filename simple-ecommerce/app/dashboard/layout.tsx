5821474c8b73994b1235109636fd091ee17a16b5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-12 22:15:13 +0800

    - stylings

--- simple-ecommerce/app/dashboard/layout.tsx
+++ simple-ecommerce/app/dashboard/layout.tsx
@@ -5,9 +5,11 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className=" flex-col flex w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
       <div className=" sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
-        <DashboardNavigation />
+        <nav className=" hidden font-medium md:flex md:flex-row lg:gap-6 md:text-sm md:items-center md:gap-5 ">
+          <DashboardNavigation />
+        </nav>
       </div>
-      {children}
+      {/* {children} */}
     </div>
   );
 }
