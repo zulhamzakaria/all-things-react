a5747c8fa447d4e7568a85f81f62eb7351027312
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-12 22:11:18 +0800

    - dashboard nav links

--- simple-ecommerce/app/components/dashboard/DashboardNavigation.tsx
+++ simple-ecommerce/app/components/dashboard/DashboardNavigation.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Link from "next/link";
+
+const links = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    name: "Orders",
+    href: "/dashboard/orders",
+  },
+  {
+    name: "Products",
+    href: "/dashboard/products",
+  },
+  {
+    name: "Categories",
+    href: "/dashboard/categories",
+  },
+];
 
 const DashboardNavigation = () => {
   return (
-    <div>DashboardNavigation</div>
-  )
-}
+    <>
+      {links.map((link) => (
+        <Link href={link.href} key={link.name}>
+          {link.name}
+        </Link>
+      ))}
+    </>
+  );
+};
 
-export default DashboardNavigation
+export default DashboardNavigation;
