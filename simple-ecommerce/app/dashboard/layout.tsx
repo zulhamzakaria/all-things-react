136e28022c2b68ac94121196e89f89abca446b55
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-12 22:04:14 +0800

    - component placement

--- simple-ecommerce/app/dashboard/layout.tsx
+++ simple-ecommerce/app/dashboard/layout.tsx
@@ -4,8 +4,9 @@ import DashboardNavigation from "../components/dashboard/DashboardNavigation";
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className=" flex-col flex w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-      <div className=" sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white"></div>
-      <DashboardNavigation />
+      <div className=" sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
+        <DashboardNavigation />
+      </div>
       {children}
     </div>
   );
