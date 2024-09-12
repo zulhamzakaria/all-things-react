d87d15b7bf7b1e7bbeac7d8d671f08b87d35b909
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-12 22:02:58 +0800

    - basic dashboardnavigation ocmponent - import dashboardNavigation from dashboard layout

--- simple-ecommerce/app/dashboard/layout.tsx
+++ simple-ecommerce/app/dashboard/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
+import DashboardNavigation from "../components/dashboard/DashboardNavigation";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className=" flex-col flex w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
       <div className=" sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white"></div>
-
+      <DashboardNavigation />
       {children}
     </div>
   );
