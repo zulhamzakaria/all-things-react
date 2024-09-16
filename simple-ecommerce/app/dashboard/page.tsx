047b111785a77fbce97f505b7da54b4fa55794bc
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-16 21:53:03 +0800

    - stylings (WIP)

--- simple-ecommerce/app/dashboard/page.tsx
+++ simple-ecommerce/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
-
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <>
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+        
+      </div>
+    </>
+  );
 };
 
 export default DashboardPage;
