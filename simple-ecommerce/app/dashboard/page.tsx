66be6093995ecf1ce42190e77bb53544ce808cd4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-16 21:55:01 +0800

    - Totla revenue card

--- simple-ecommerce/app/dashboard/page.tsx
+++ simple-ecommerce/app/dashboard/page.tsx
@@ -1,8 +1,14 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 const DashboardPage = () => {
   return (
     <>
       <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-        
+        <Card>
+          <CardHeader>
+            <CardTitle>Total Revenue</CardTitle>
+          </CardHeader>
+        </Card>
       </div>
     </>
   );
