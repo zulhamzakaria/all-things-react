376a978fc872f4ace46e780b3cf722bb0e216b17
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-18 20:16:54 +0800

    - finished stylings

--- simple-ecommerce/app/dashboard/orders/page.tsx
+++ simple-ecommerce/app/dashboard/orders/page.tsx
@@ -5,6 +5,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function OrdersPage() {
   return (
@@ -13,7 +21,45 @@ export default function OrdersPage() {
         <CardTitle>Orders</CardTitle>
         <CardDescription>Recent orders from your store!</CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className=" text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <p className=" font-medium">Cust 1</p>
+                <p className=" hidden md:flex text-sm text-muted-foreground">
+                  cust1@customer.com
+                </p>
+              </TableCell>
+              <TableCell>Sale</TableCell>
+              <TableCell>Successful</TableCell>
+              <TableCell>{new Date().toLocaleDateString()}</TableCell>
+              <TableCell className=" text-right">RM 250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <p className=" font-medium">Cust 2</p>
+                <p className=" hidden md:flex text-sm text-muted-foreground">
+                  cust2@customer.com
+                </p>
+              </TableCell>
+              <TableCell>Sale</TableCell>
+              <TableCell>Pending</TableCell>
+              <TableCell>{new Date().toLocaleDateString()}</TableCell>
+              <TableCell className=" text-right">RM 2500.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
     </Card>
   );
 }
