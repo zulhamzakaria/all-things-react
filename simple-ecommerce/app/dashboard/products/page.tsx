2de9a09f089c8fc930ad228fb4463294b536b819
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-19 14:31:39 +0800

    - somehow, asChild renders the Link inside the button properly

--- simple-ecommerce/app/dashboard/products/page.tsx
+++ simple-ecommerce/app/dashboard/products/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
+
 const ProductsPage = () => {
-  return <h1>Products</h1>;
+  return (
+    <>
+      <div className=" flex items-center justify-end">
+        <Button className=" gap-x-2" asChild>
+          <Link href="dashboard/products/create">
+            <PlusCircle />
+            <p>Add product</p>
+          </Link>
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default ProductsPage;
