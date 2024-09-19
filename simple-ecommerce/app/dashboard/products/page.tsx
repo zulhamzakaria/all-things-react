75c95bde625d45dd7296dbe1f7a7feac639d5549
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-19 14:58:41 +0800

    - stylings

--- simple-ecommerce/app/dashboard/products/page.tsx
+++ simple-ecommerce/app/dashboard/products/page.tsx
@@ -1,17 +1,90 @@
 import { Button } from "@/components/ui/button";
-import { PlusCircle } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { MoreHorizontal, PlusCircle, UserIcon } from "lucide-react";
 import Link from "next/link";
 
 const ProductsPage = () => {
   return (
     <>
-      <div className=" flex items-center justify-end">
-        <Button className=" gap-x-2" asChild>
-          <Link href="dashboard/products/create">
-            <PlusCircle />
-            <p>Add product</p>
-          </Link>
-        </Button>
+      <div className=" flex flex-col gap-y-2">
+        <div className=" flex items-center justify-end">
+          <Button className=" gap-x-2 bg-blue-950" asChild>
+            <Link href="dashboard/products/create">
+              <PlusCircle />
+              <p>Add product</p>
+            </Link>
+          </Button>
+        </div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Products</CardTitle>
+            <CardDescription>
+              Manage your products and the sales performance
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Image</TableHead>
+                  <TableHead>Name</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Price</TableHead>
+                  <TableHead>Date</TableHead>
+                  <TableHead className=" text-end">Actions</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <UserIcon className=" h-16 w-16" />
+                  </TableCell>
+                  <TableCell>Nike Air</TableCell>
+                  <TableCell>Active</TableCell>
+                  <TableCell>RM 120.00</TableCell>
+                  <TableCell>{new Date().toLocaleDateString()}</TableCell>
+                  <TableCell className=" text-end">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button size={"icon"} variant={"ghost"}>
+                          <MoreHorizontal className=" h-4 w-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
       </div>
     </>
   );
