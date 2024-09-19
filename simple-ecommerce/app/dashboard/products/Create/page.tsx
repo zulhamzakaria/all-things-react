b0a9e7979e3dc70b88c7e926e970ad58cd60db3e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-19 15:59:11 +0800

    - some changes (WIP)

--- simple-ecommerce/app/dashboard/products/Create/page.tsx
+++ simple-ecommerce/app/dashboard/products/Create/page.tsx
@@ -28,9 +28,7 @@ const CreateProductPage = () => {
         </CardHeader>
         <CardContent>
           <div className=" flex flex-col gap-6">
-            <div className=" flex flex-col gap-3">
-              
-            </div>
+            <div className=" flex flex-col gap-3"></div>
           </div>
         </CardContent>
       </Card>
