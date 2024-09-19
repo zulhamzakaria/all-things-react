8041cfdb12063bc1a683d8cb9898c924a42ff77f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-19 22:24:58 +0800

    - GET() User endpoint

--- simple-ecommerce/app/api/auth/creation/route.ts
+++ simple-ecommerce/app/api/auth/creation/route.ts
@@ -1,8 +1,32 @@
+import prisma from "@/app/lib/db";
 import { getUser } from "@/lib/utils";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const user = await getUser();
   if (!user || user === null) {
     throw new Error("User not found");
   }
+
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        email: user.email as string,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name!,
+        profileImage:
+          user.picture ??
+          "https://vignette.wikia.nocookie.net/meme/images/5/52/Polite_Cat-0.jpg/revision/latest?cb=20200611120017",
+      },
+    });
+  }
+
+  NextResponse.redirect(process.env.INDEX_PAGE ?? "https://google.com");
 }
