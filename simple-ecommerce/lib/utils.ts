8de60d73336bd1b91a3c5b3a7771dfee7b94d104
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-19 20:31:36 +0800

    - re-usable function for gettingUser

--- simple-ecommerce/lib/utils.ts
+++ simple-ecommerce/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function getUser() {
+  const { getUser } = getKindeServerSession();
+  return await getUser();
 }
