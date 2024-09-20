9d6579efe907cff54fb3cc8bdd8aa1db1fbbf9d1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-20 12:01:19 +0800

    - re-usable Button with form state for loading/pending state

---
+++ simple-ecommerce/app/components/SubmitButton.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
+
+export default function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      {pending ? (
+        <Button disabled>
+          <Loader2 className=" mr-2 h-4 w-4 animate-spin" />
+          Please wait...
+        </Button>
+      ) : (
+        <Button type="submit">Create Product</Button>
+      )}
+    </>
+  );
+}
