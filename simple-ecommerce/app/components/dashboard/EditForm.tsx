90a9ee9bf15475b5d0cc6dedac181abd06ee84ac
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-20 15:20:54 +0800

    - separate component for 'use client' - import EditForm from EditProductPage

---
+++ simple-ecommerce/app/components/dashboard/EditForm.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export function EditProductForm() {
+  return <h1>Edit Product Form</h1>;
+}
--- simple-ecommerce/app/dashboard/products/[id]/page.tsx
+++ simple-ecommerce/app/dashboard/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { EditProductForm } from "@/app/components/dashboard/EditForm";
 import prisma from "@/app/lib/db";
 import { notFound } from "next/navigation";
 
@@ -14,7 +15,7 @@ async function getProduct(productId: string) {
 
 const EditProductPage = async ({ params }: { params: { id: string } }) => {
   const product = await getProduct(params.id);
-  return <div>ProductEditPage</div>;
+  return <EditProductForm />;
 };
 
 export default EditProductPage;
