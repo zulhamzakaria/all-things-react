a185a580f955c5c63b8197ec6086b0553caa010b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-20 23:33:10 +0800

    - getData - data population

--- simple-ecommerce/app/dashboard/banner/page.tsx
+++ simple-ecommerce/app/dashboard/banner/page.tsx
@@ -1,3 +1,4 @@
+import prisma from "@/app/lib/db";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -23,9 +24,24 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { MoreHorizontal, PlusCircle, User2 } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
-export default function BannerPage() {
+async function GetBanners() {
+  const result = await prisma.banner.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (!result) return notFound();
+
+  return result;
+}
+
+export default async function BannerPage() {
+  const banners = await GetBanners();
   return (
     <>
       <div className=" flex items-center justify-end">
@@ -51,32 +67,40 @@ export default function BannerPage() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell>
-                  <User2 className=" size-16" />
-                </TableCell>
-                <TableCell className=" font-medium">Great products</TableCell>
-                <TableCell className=" text-end">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button size={"icon"} variant={"ghost"}>
-                        <MoreHorizontal className=" h-4 w-4" />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuItem asChild>
-                        <Link
-                          href={`/dashboard/products/${"product.id"}/delete`}
-                        >
-                          Delete
-                        </Link>
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </TableCell>
-              </TableRow>
+              {banners.map((banner) => (
+                <TableRow key={banner.id}>
+                  <TableCell>
+                    <Image
+                      src={banner.imageString}
+                      alt="🖼️"
+                      width={100}
+                      height={100}
+                      className=" rounded-lg object-cover size-16"
+                    />
+                  </TableCell>
+                  <TableCell className=" font-medium">{banner.title}</TableCell>
+                  <TableCell className=" text-end">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button size={"icon"} variant={"ghost"}>
+                          <MoreHorizontal className=" h-4 w-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem asChild>
+                          <Link
+                            href={`/dashboard/banner/${banner.id}/delete`}
+                          >
+                            Delete
+                          </Link>
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </CardContent>
