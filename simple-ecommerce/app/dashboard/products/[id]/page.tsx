859403c0d1df0e8867abb168e800a71e97120a37
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-20 15:18:15 +0800

    - getttin data (WIP)

--- simple-ecommerce/app/dashboard/products/[id]/page.tsx
+++ simple-ecommerce/app/dashboard/products/[id]/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import prisma from "@/app/lib/db";
+import { notFound } from "next/navigation";
 
-const ProductEditPage = () => {
+async function getProduct(productId: string) {
+  const data = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+
+  if (!data) return notFound();
+  return data;
+}
+
+const EditProductPage = async ({ params }: { params: { id: string } }) => {
+  const product = await getProduct(params.id);
   return <div>ProductEditPage</div>;
 };
 
-export default ProductEditPage;
+export default EditProductPage;
