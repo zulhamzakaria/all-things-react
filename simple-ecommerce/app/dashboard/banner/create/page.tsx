4adb0578daa6bb7e1928ac896d064445d6c6e615
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-20 22:28:27 +0800

    - form (WIP)

--- simple-ecommerce/app/dashboard/banner/create/page.tsx
+++ simple-ecommerce/app/dashboard/banner/create/page.tsx
@@ -1,3 +1,53 @@
+import SubmitButton from "@/app/components/SubmitButton";
+import { UploadDropzone } from "@/app/lib/uploadthing";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function CreateBannerPage() {
-  return <>Create banner here</>;
+  return (
+    <>
+      <form action="">
+        <div className=" flex items-center gap-x-4">
+          <Button variant={"outline"} size={"icon"} asChild>
+            <Link href="/dashboard/products">
+              <ChevronLeft className="w-4 h-4" />
+            </Link>
+          </Button>
+          <h2 className=" text-xl font-semibold tracking-tight">New Banner</h2>
+        </div>
+        <Card className="mt-5">
+          <CardHeader>
+            <CardTitle>Banner Details</CardTitle>
+            <CardDescription>Create your banner here</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className=" flex flex-col gap-y-6">
+              <div className=" flex flex-col gap-3">
+                <Label>Name</Label>
+                <Input type="text" placeholder="Create title for Banner" />
+              </div>
+              <div className=" flex flex-col gap-3">
+                <Label>Images</Label>
+                <UploadDropzone endpoint="imageUploader" />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <SubmitButton text="Create Banner" />
+          </CardFooter>
+        </Card>
+      </form>
+    </>
+  );
 }
