98a548020e8e6a1668b7a03ef6bd0efda5ac3bab
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-20 20:27:27 +0800

    - 80vh == 80% of the screen - WIP

--- simple-ecommerce/app/dashboard/products/[id]/delete/page.tsx
+++ simple-ecommerce/app/dashboard/products/[id]/delete/page.tsx
@@ -1,3 +1,32 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
 export default function DeleteProduct({ params }: { params: { id: string } }) {
-  return <div>{params.id}</div>;
+  return (
+    <div className=" h-[80vh] w-full flex items-center justify-center">
+      <Card className=" max-w-xl">
+        <CardHeader>
+          <CardTitle>Are you sure?</CardTitle>
+          <CardDescription>
+            This action will delete the product and irrecoverable
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className=" w-full flex justify-between">
+          <Button variant={"secondary"} asChild>
+            <Link href={"/dashboard/products"}>Cancel</Link>
+          </Button>
+          <form action="">
+            <Button variant={"destructive"}>Delete</Button>
+          </form>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
