8e1463098963c1964e7d08b759b5bd28c67d8e5c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-20 11:22:41 +0800

    - somns wrogn with the create product

--- simple-ecommerce/app/actions.ts
+++ simple-ecommerce/app/actions.ts
@@ -4,6 +4,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import { parseWithZod } from "@conform-to/zod";
 import { ProductSchema } from "./lib/zodSchemas";
+import prisma from "./lib/db";
 
 export async function createProduct(currentState: any, formData: FormData) {
   const { getUser } = getKindeServerSession();
@@ -20,4 +21,18 @@ export async function createProduct(currentState: any, formData: FormData) {
   if (submission.status !== "success") {
     return submission.reply();
   }
+
+  await prisma.product.create({
+    data: {
+      name: submission.value.name,
+      description: submission.value.description,
+      status: submission.value.status,
+      price: submission.value.price,
+      images: submission.value.images,
+      category: submission.value.category,
+      isFeatured: submission.value.isFeatured,
+    },
+  });
+
+  redirect("/dashboard/products");
 }
