4763d6c6ada1c0c6f351b742eb86676fdee85e55
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-20 15:49:22 +0800

    - edit product server action

--- simple-ecommerce/app/actions.ts
+++ simple-ecommerce/app/actions.ts
@@ -40,3 +40,41 @@ export async function createProduct(currentState: any, formData: FormData) {
 
   redirect("/dashboard/products");
 }
+
+export async function editProduct(currentState: any, formData: FormData) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user.email !== "leonn.sameonn@gmail.com") {
+    return redirect("/");
+  }
+
+  const submission = parseWithZod(formData, {
+    schema: ProductSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const flattenUrls = submission.value.images.flatMap((urlString) =>
+    urlString.split(",").map((url) => url.trim())
+  );
+
+  const productId = formData.get("productId") as string;
+  await prisma.product.update({
+    where: {
+      id: productId,
+    },
+    data: {
+      name: submission.value.name,
+      description: submission.value.description,
+      status: submission.value.status,
+      price: submission.value.price,
+      images: flattenUrls,
+      category: submission.value.category,
+      isFeatured: submission.value.isFeatured,
+    },
+  });
+  redirect("/dashboard/products");
+}
