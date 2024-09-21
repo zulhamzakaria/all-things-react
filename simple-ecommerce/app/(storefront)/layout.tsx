37373cd3cb2ae0670ca1bb901c48b328e773d8d2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-21 10:50:11 +0800

    - stylings

--- simple-ecommerce/app/(storefront)/layout.tsx
+++ simple-ecommerce/app/(storefront)/layout.tsx
@@ -9,7 +9,9 @@ export default function StorefrontLayout({
   return (
     <>
       <Navbar />
-      {children}
+      <main className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {children}
+      </main>
     </>
   );
 }
