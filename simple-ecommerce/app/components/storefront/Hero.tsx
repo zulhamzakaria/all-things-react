c334a350ed3ee88368e5014fb4192d7d0176b92c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-21 20:22:35 +0800

    - w-[XXvh] means XX% of width - stylings

--- simple-ecommerce/app/components/storefront/Hero.tsx
+++ simple-ecommerce/app/components/storefront/Hero.tsx
@@ -1,3 +1,49 @@
-export function Hero() {
-  return <div>hero!</div>;
+import prisma from "@/app/lib/db";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+async function GetBanners() {
+  const results = await prisma.banner.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (!results) {
+    return notFound();
+  }
+
+  return results;
+}
+
+export async function Hero() {
+  const banners = await GetBanners();
+  return (
+    <Carousel>
+      <CarouselContent>
+        {banners.map((banner) => (
+          <CarouselItem key={banner.id}>
+            <div className=" relative h-[60vh] lg:h-[80vh]">
+              <Image
+                src={banner.imageString}
+                alt="banner image"
+                className=" rounded-lg object-cover w-full h-full "
+                fill
+              />
+              <div className=" absolute top-6 bg-opacity-75 transition-transform hover:scale-105 p-6 rounded-xl text-white bg-black left-6 ">
+                <h1 className=" text-xl lg:text-4xl font-bold">
+                  {banner.title}
+                </h1>
+              </div>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
 }
