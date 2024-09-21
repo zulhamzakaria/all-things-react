aa8f9085ddc9a792217a14590c8d0e4a10c76159
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-21 14:26:14 +0800

    - user dropdown menu

--- simple-ecommerce/app/components/storefront/UserDropdown.tsx
+++ simple-ecommerce/app/components/storefront/UserDropdown.tsx
@@ -2,19 +2,38 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export function UserDropdown() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button>
-          <Avatar>
-            <AvatarFallback>e-commerce</AvatarFallback>
+        <Button value={"ghost"} className=" relative size-10 rounded-full">
+          <Avatar className=" size-10">
+            <AvatarFallback className=" text-muted-foreground">
+              LS
+            </AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent className=" w-56" align="end" forceMount>
+        <DropdownMenuLabel className=" flex flex-col space-y-1">
+          <p className=" text-sm font-medium leading-none">Leonn Sameonn</p>
+          <p className=" text-xs leading-none text-muted-foreground ">
+            ls@google.com
+          </p>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <LogoutLink>Sign out</LogoutLink>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
