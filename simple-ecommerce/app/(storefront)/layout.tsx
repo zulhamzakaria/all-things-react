79dc99b70fe8c50e4bf2f1c384638ebeca46e5bd
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-21 10:47:03 +0800

    - import Navbar

---
+++ simple-ecommerce/app/(storefront)/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import { Navbar } from "../components/storefront/Navbar";
+
+export default function StorefrontLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
