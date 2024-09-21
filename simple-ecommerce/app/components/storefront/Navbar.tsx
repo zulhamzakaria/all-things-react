170436d7578f9509797a6da452e2890335ace11d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-21 10:55:47 +0800

    - stylings

--- simple-ecommerce/app/components/storefront/Navbar.tsx
+++ simple-ecommerce/app/components/storefront/Navbar.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className=" w-full max-w-7xl mx-auto px-4 sm:px-6 justify-between flex items-center lg:px-8 py-5">
+      <div className=" flex items-center">
+        <Link href={"/"}>
+          <h1 className=" text-black font-bold text-xl lg:text-3xl">
+            Home<span className=" text-primary">Page</span>
+          </h1>
+        </Link>
+      </div>
+    </nav>
+  );
 }
