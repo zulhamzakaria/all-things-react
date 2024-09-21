da8f77017553e51abc6e42f3666e0bb56bc612f5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-21 12:13:56 +0800

    - stylings

--- simple-ecommerce/app/components/storefront/Navbar.tsx
+++ simple-ecommerce/app/components/storefront/Navbar.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { NavbarLinks } from "./NavbarLinks";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ShoppingBagIcon } from "lucide-react";
 
-export function Navbar() {
+export async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <nav className=" w-full max-w-7xl mx-auto px-4 sm:px-6 justify-between flex items-center lg:px-8 py-5">
       <div className=" flex items-center">
@@ -11,6 +15,20 @@ export function Navbar() {
           </h1>
         </Link>
         <NavbarLinks />
+      </div>
+      <div>
+        {user ? (
+          <>
+            <Link href={"/bag"} className=" group p-2 flex items-center mr-2">
+              <ShoppingBagIcon className=" size-6 text-gray-400  group-hover:text-gray-500" />
+              <span className=" ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800 ">
+                5
+              </span>
+            </Link>
+          </>
+        ) : (
+          <div>please login</div>
+        )}
       </div>
     </nav>
   );
