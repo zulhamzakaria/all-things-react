8d1c7df79add358422795ad8496cc709d7ddcbe1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-21 23:34:58 +0800

    - get published products - strylings

--- simple-ecommerce/app/components/storefront/FeaturedProducts.tsx
+++ simple-ecommerce/app/components/storefront/FeaturedProducts.tsx
@@ -1,3 +1,35 @@
-export function FeaturedProducts() {
-  return <div></div>;
+import prisma from "@/app/lib/db";
+
+async function GetProducts() {
+  const results = await prisma.product.findMany({
+    where: {
+      status: "published",
+    },
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      images: true,
+      price: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return results;
+}
+
+export async function FeaturedProducts() {
+  const products = await GetProducts();
+  return (
+    <>
+      <h2 className=" text-2xl font-extrabold tracking-tight">
+        <div className=" mt-5 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+          {products.map((product) => (
+            <p>{product.name}</p>
+          ))}
+        </div>
+      </h2>
+    </>
+  );
 }
