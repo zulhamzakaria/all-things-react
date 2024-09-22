0e0eeb5bed5e69f5c1739682cfccf78d137a4adc
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-22 23:04:26 +0800

    - import footer

--- simple-ecommerce/app/(storefront)/layout.tsx
+++ simple-ecommerce/app/(storefront)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import { Navbar } from "../components/storefront/Navbar";
+import { Footer } from "../components/storefront/Footer";
 
 export default function StorefrontLayout({
   children,
@@ -12,6 +13,7 @@ export default function StorefrontLayout({
       <main className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {children}
       </main>
+      <Footer />
     </>
   );
 }
