df396f06966768e5f1872b458fdfe479ccdc0485
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-22 23:03:29 +0800

    - footer and stylings

---
+++ simple-ecommerce/app/components/storefront/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="mt-16 mb-10 max-w-7xl sm:px-6 lg:px-8 px-4  mx-auto">
+      <div className=" border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
+        <p>&copy;</p>
+      </div>
+    </footer>
+  );
+}
