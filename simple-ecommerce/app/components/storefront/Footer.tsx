254319d44cb772e0fbf5bf6e7ae268ddbff93c7e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-22 23:06:04 +0800

    - stylings

--- simple-ecommerce/app/components/storefront/Footer.tsx
+++ simple-ecommerce/app/components/storefront/Footer.tsx
@@ -2,7 +2,9 @@ export function Footer() {
   return (
     <footer className="mt-16 mb-10 max-w-7xl sm:px-6 lg:px-8 px-4  mx-auto">
       <div className=" border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
-        <p>&copy;</p>
+        <p className=" text-xs leading-5 text-gray-700">
+          &copy;2024 eKedaiKasut. All Rights Reserved
+        </p>
       </div>
     </footer>
   );
