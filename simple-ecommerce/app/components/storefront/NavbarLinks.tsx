c089e29dcd6096d4f190073a8d3a8a765420bb44
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-22 22:00:30 +0800

    - stylings for active link

--- simple-ecommerce/app/components/storefront/NavbarLinks.tsx
+++ simple-ecommerce/app/components/storefront/NavbarLinks.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function NavbarLinks() {
+  const location = usePathname();
   return (
     <div className="hidden md:flex justify-center ml-5 items-center gap-x-4">
       {navbarLinks.map((link) => (
-        <Link key={link.id} href={link.href} className=" font-medium">
+        <Link
+          key={link.id}
+          href={link.href}
+          className={cn(
+            location === link.href
+              ? "bg-muted"
+              : " hover:bg-muted hover:bg-opacity-75",
+            "group p-2 rounded-md font-medium"
+          )}
+        >
           {link.name}
         </Link>
       ))}
