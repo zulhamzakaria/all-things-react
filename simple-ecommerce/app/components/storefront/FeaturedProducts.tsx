bb20cb70409f931a6ea8085019f5be37b43bff30
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-22 20:12:11 +0800

    - props for ProductCard

--- simple-ecommerce/app/components/storefront/FeaturedProducts.tsx
+++ simple-ecommerce/app/components/storefront/FeaturedProducts.tsx
@@ -27,7 +27,7 @@ export async function FeaturedProducts() {
       <h2 className=" text-2xl font-extrabold tracking-tight">
         <div className=" mt-5 sm:grid-cols-2 lg:grid-cols-3 gap-5">
           {products.map((product) => (
-            <ProductCard key={product.id} />
+            <ProductCard key={product.id} product={product} />
           ))}
         </div>
       </h2>
--- simple-ecommerce/app/components/storefront/ProductCard.tsx
+++ simple-ecommerce/app/components/storefront/ProductCard.tsx
@@ -1,4 +1,14 @@
-const ProductCard = () => {
+interface ProductCardProps {
+  product: {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    images: string[];
+  };
+}
+
+const ProductCard = ({ product }: ProductCardProps) => {
   return <div>product card</div>;
 };
 export default ProductCard;
