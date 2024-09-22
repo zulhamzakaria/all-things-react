1c76552ca90a8b915b03713102f6dcafb8898a5d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-22 20:43:59 +0800

    - stylings (WIP)

---
+++ simple-ecommerce/app/(storefront)/product/[id]/page.tsx
@@ -0,0 +1,35 @@
+import prisma from "@/app/lib/db";
+import { notFound } from "next/navigation";
+
+async function GetProduct(productId: string) {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+    select: {
+      price: true,
+      images: true,
+      description: true,
+      name: true,
+      id: true,
+    },
+  });
+  if (!product) {
+    return notFound();
+  }
+  return product;
+}
+
+const ProductDetailsPage = async ({ params }: { params: { id: string } }) => {
+  const product = await GetProduct(params.id);
+
+  return (
+    <>
+      <div className=" grid grid-cols-1 md:grid-cols-2 py-6 lg:gap-x-24 gap-6 items-start">
+        
+      </div>
+    </>
+  );
+};
+
+export default ProductDetailsPage;
