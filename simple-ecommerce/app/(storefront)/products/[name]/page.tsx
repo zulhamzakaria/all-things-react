36404923aad04ec39b80a059d0659346e25b35a0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-22 22:56:22 +0800

    - stylings

--- simple-ecommerce/app/(storefront)/products/[name]/page.tsx
+++ simple-ecommerce/app/(storefront)/products/[name]/page.tsx
@@ -1,6 +1,8 @@
+import ProductCard from "@/app/components/storefront/ProductCard";
 import prisma from "@/app/lib/db";
+import { notFound } from "next/navigation";
 
-async function GetData(category: string) {
+async function GetProducts(category: string) {
   switch (category) {
     case "all":
       const data = await prisma.product.findMany({
@@ -35,7 +37,7 @@ async function GetData(category: string) {
         },
       });
       return {
-        title: "Men Products",
+        title: "Men",
         data: data,
       };
     }
@@ -55,17 +57,51 @@ async function GetData(category: string) {
         },
       });
       return {
-        title: "Women Products",
+        title: "Women",
         data: data,
       };
+    }
+
+    case "kids": {
+      const data = await prisma.product.findMany({
+        where: {
+          status: "published",
+          category: "kid",
+        },
+        select: {
+          name: true,
+          images: true,
+          price: true,
+          description: true,
+          id: true,
+        },
+      });
+      return {
+        title: "Kids",
+        data: data,
+      };
+    }
+
+    default: {
+      return notFound();
     }
   }
 }
 
-export default function CategoriesPage() {
+export default async function CategoriesPage({
+  params,
+}: {
+  params: { name: string };
+}) {
+  const { data, title } = await GetProducts(params.name);
   return (
-    <div>
-      <h1>CategoriesPage</h1>
-    </div>
+    <section>
+      <h1 className=" font-semibold text-3xl my-5">{title}</h1>
+      <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {data.map((data) => (
+          <ProductCard product={data} />
+        ))}
+      </div>
+    </section>
   );
 }
