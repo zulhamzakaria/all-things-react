2360eba80cbfc7a67085ec3f86e0f4b10bff746f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-22 20:18:55 +0800

    - styligns - fill attribute fills the image to the container size

--- simple-ecommerce/app/components/storefront/ProductCard.tsx
+++ simple-ecommerce/app/components/storefront/ProductCard.tsx
@@ -1,3 +1,10 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+
 interface ProductCardProps {
   product: {
     id: string;
@@ -9,6 +16,20 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({ product }: ProductCardProps) => {
-  return <div>product card</div>;
+  return (
+    <div className=" rounded-lg">
+      <Carousel className=" w-full mx-auto">
+        <CarouselContent>
+          {product.images.map((image, index) => (
+            <CarouselItem key={index}>
+              <div className=" relative h-[330px]">
+                <Image src={image} alt='product-image' fill  />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
 };
 export default ProductCard;
