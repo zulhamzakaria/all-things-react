3f5c8479f7b259f7c3c2130d72eba07e5687111b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-23 17:43:58 +0800

    - stylings

--- simple-ecommerce/app/(storefront)/products/[name]/loading.tsx
+++ simple-ecommerce/app/(storefront)/products/[name]/loading.tsx
@@ -1,3 +1,9 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function LoadingPage() {
-  return <h1>Loading data...</h1>;
+  return (
+    <div>
+      <Skeleton className=" h-10 w-56 my-5" />
+    </div>
+  );
 }
