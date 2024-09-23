363d107bc9be59a00edbac68e85543a36f661307
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-23 15:18:59 +0800

    const totalItems = cart?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
    - is an accumulator
    - item starts at 0

--- simple-ecommerce/app/components/storefront/Navbar.tsx
+++ simple-ecommerce/app/components/storefront/Navbar.tsx
@@ -1,18 +1,22 @@
-import Link from "next/link";
-import { NavbarLinks } from "./NavbarLinks";
+import { Button } from "@/components/ui/button";
 import {
   getKindeServerSession,
   LoginLink,
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ShoppingBagIcon } from "lucide-react";
+import Link from "next/link";
+import { NavbarLinks } from "./NavbarLinks";
 import { UserDropdown } from "./UserDropdown";
-import { useReducer } from "react";
-import { Button } from "@/components/ui/button";
+import { redis } from "@/app/lib/redis";
+import { Cart } from "@/app/lib/interfaces";
 
 export async function Navbar() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  const cart: Cart | null = await redis.get(`cart-${user.id}`);
+  const totalItems =
+    cart?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
   return (
     <nav className=" w-full max-w-7xl mx-auto px-4 sm:px-6 justify-between flex items-center lg:px-8 py-5">
       <div className=" flex items-center">
@@ -29,7 +33,7 @@ export async function Navbar() {
             <Link href={"/bag"} className=" group p-2 flex items-center mr-2">
               <ShoppingBagIcon className=" size-6 text-gray-400  group-hover:text-gray-500" />
               <span className=" ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800 ">
-                5
+                {totalItems}
               </span>
             </Link>
             <UserDropdown
