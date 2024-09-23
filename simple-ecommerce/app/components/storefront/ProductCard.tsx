e3eec573135dcd584e8940e9b0107aa276e5eff0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-23 17:47:36 +0800

    - skeleton to mimic the Product Card

--- simple-ecommerce/app/components/storefront/ProductCard.tsx
+++ simple-ecommerce/app/components/storefront/ProductCard.tsx
@@ -6,6 +6,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Skeleton } from "@/components/ui/skeleton";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -56,3 +57,16 @@ const ProductCard = ({ product }: ProductCardProps) => {
   );
 };
 export default ProductCard;
+
+export function LoadingProductCard() {
+  return (
+    <div className=" flex flex-col">
+      <Skeleton className=" w-full h-[330px]" />
+      <div className=" flex flex-col mt-2 gap-y-2">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className=" w-full h-6" />
+      </div>
+      <Skeleton className=" w-full h-10 mt-5" />
+    </div>
+  );
+}
