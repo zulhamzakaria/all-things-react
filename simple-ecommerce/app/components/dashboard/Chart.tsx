386b74dae6da00539c3737f798e9d97cc9d4e497
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-24 22:42:44 +0800

    - adding rechart and its components (WIP)

--- simple-ecommerce/app/components/dashboard/Chart.tsx
+++ simple-ecommerce/app/components/dashboard/Chart.tsx
@@ -1,5 +1,28 @@
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
 const Chart = () => {
-  return <div>Chart</div>;
+  return (
+    <ResponsiveContainer height={400} width="100%">
+      <LineChart>
+        <CartesianGrid strokeDasharray={"3 3"}>
+          <XAxis />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type={"monotone"} stroke="#3b83f6" activeDot={{ r: 8 }} />
+        </CartesianGrid>
+      </LineChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default Chart;
