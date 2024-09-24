2d83f490e0db0688f8251db2d6436bf7ae596f93
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-24 22:04:31 +0800

    - data population - reduce() function for calculating total amount - totalAmount/100 because stripe accepts 00 as cents i.e 4000 -> 40 - getting data  in parallel with prisma

--- simple-ecommerce/app/components/dashboard/DashboardStats.tsx
+++ simple-ecommerce/app/components/dashboard/DashboardStats.tsx
@@ -1,8 +1,37 @@
+import prisma from "@/app/lib/db";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DollarSign, PartyPopper, ShoppingBag, User2 } from "lucide-react";
-import React from "react";
 
-const DashboardStats = () => {
+async function GetUsers() {
+  //parallel
+  const [users, products, orders] = await Promise.all([
+    await prisma.user.findMany({
+      select: {
+        id: true,
+      },
+    }),
+    await prisma.product.findMany({
+      select: {
+        id: true,
+      },
+    }),
+    await prisma.order.findMany({
+      select: {
+        amount: true,
+      },
+    }),
+  ]);
+
+  return { users, products, orders };
+}
+
+const DashboardStats = async () => {
+  const { users, products, orders } = await GetUsers();
+
+  const totalAmount = orders.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.amount;
+  }, 0);
+
   return (
     <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
       <Card>
@@ -11,7 +40,9 @@ const DashboardStats = () => {
           <DollarSign className=" h-4 w-4 text-green-500" />
         </CardHeader>
         <CardContent>
-          <p className=" text-2xl font-bold">100.00</p>
+          <p className=" text-2xl font-bold">
+            RM {new Intl.NumberFormat("en-MY").format(totalAmount / 100)}
+          </p>
           <p className=" text-xs text-muted-foreground ">
             Based on 100 charges
           </p>
@@ -23,7 +54,7 @@ const DashboardStats = () => {
           <ShoppingBag className=" h-4 w-4 text-blue-500" />
         </CardHeader>
         <CardContent>
-          <p className=" text-2xl font-bold">+50</p>
+          <p className=" text-2xl font-bold">+{orders.length}</p>
           <p className=" text-xs text-muted-foreground ">Total Sales</p>
         </CardContent>
       </Card>
@@ -33,7 +64,7 @@ const DashboardStats = () => {
           <PartyPopper className=" h-4 w-4 text-indigo-500" />
         </CardHeader>
         <CardContent>
-          <p className=" text-2xl font-bold">37</p>
+          <p className=" text-2xl font-bold">{products.length}</p>
           <p className=" text-xs text-muted-foreground ">
             Total Products created
           </p>
@@ -45,7 +76,7 @@ const DashboardStats = () => {
           <User2 className=" h-4 w-4 text-orange-500" />
         </CardHeader>
         <CardContent>
-          <p className=" text-2xl font-bold">120</p>
+          <p className=" text-2xl font-bold">{users.length}</p>
           <p className=" text-xs text-muted-foreground ">Total Active Users</p>
         </CardContent>
       </Card>
