68cb58d357e9d3189759be7762503d3236e8a73c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-09-29 23:20:57 +0800

    - importing toggles

--- dynamic-theme-selector/app/page.tsx
+++ dynamic-theme-selector/app/page.tsx
@@ -1,9 +1,17 @@
+import { ThemeColourToggle } from "@/components/theme-color-toggle";
+import { ThemeModeToggle } from "@/components/theme-mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div
+      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen
+        p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+    >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <ThemeModeToggle />
+        <ThemeColourToggle />
+
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -12,7 +20,10 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol
+          className="list-inside list-decimal text-sm text-center sm:text-left
+            font-[family-name:var(--font-geist-mono)]"
+        >
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
@@ -25,7 +36,10 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="rounded-full border border-solid border-transparent transition-colors flex
+              items-center justify-center bg-foreground text-background gap-2
+              hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4
+              sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -40,7 +54,10 @@ export default function Home() {
             Deploy now
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145]
+              transition-colors flex items-center justify-center hover:bg-[#f2f2f2]
+              dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10
+              sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
