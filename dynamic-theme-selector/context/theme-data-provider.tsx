621763b1eaa058dc96612fef32839fe14ae586b8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-10-01 23:50:03 +0800

    - debugging

--- dynamic-theme-selector/context/theme-data-provider.tsx
+++ dynamic-theme-selector/context/theme-data-provider.tsx
@@ -27,8 +27,9 @@ export default function ThemeDataProvider({ children }: ThemeProviderProps) {
   const [isMounted, setIsMounted] = useState(false);
   const { theme } = useTheme();
   useEffect(() => {
-    console.log(themeColor);
     localStorage.setItem("themeColor", themeColor);
+    console.log(localStorage.getItem("themeColor"));
+
     setGlobalColourTheme(theme as "light" | "dark", themeColor);
     if (!isMounted) {
       setIsMounted(true);
