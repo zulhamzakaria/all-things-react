2c792c1aa709f68dbc4fe5e29b751a57a5937f83
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-10-03 12:55:41 +0800

    debugging

--- dynamic-theme-selector/context/theme-data-provider.tsx
+++ dynamic-theme-selector/context/theme-data-provider.tsx
@@ -25,7 +25,7 @@ export default function ThemeDataProvider({ children }: ThemeProviderProps) {
     getSavedThemeColor() as ThemeColors,
   );
   const [isMounted, setIsMounted] = useState(false);
-  const { theme } = useTheme();
+  const  theme  = 'light'//useTheme();
   useEffect(() => {
     localStorage.setItem("themeColor", themeColor);
     setGlobalColourTheme(theme as "light" | "dark", themeColor);
