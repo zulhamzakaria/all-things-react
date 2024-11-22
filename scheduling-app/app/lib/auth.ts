2f8a20220f2d13eb27bbb213e2e807bcb0487449
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-22 15:29:04 +0800

    - added adapter for prisma. adapter takes db file (./db) prisma client as arg

--- scheduling-app/app/lib/auth.ts
+++ scheduling-app/app/lib/auth.ts
@@ -1,6 +1,9 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import prisma from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter:PrismaAdapter(prisma),
   providers: [GitHub],
 });
