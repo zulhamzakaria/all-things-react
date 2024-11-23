002f0b8fa6d599ada6ba647132a20752589420cb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-23 22:51:51 +0800

    - logos

--- scheduling-app/app/components/NavBar.tsx
+++ scheduling-app/app/components/NavBar.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className=" flex p-5 items-center justify-between">
-      <Link href={"/"}>📅</Link>
+      <Link href={"/"}>
+        <Image
+          src={"/jn6.png"}
+          alt="logo"
+          width={100}
+          height={100}
+          className=" object-contain rounded-md"
+        />
+      </Link>
     </div>
   );
 };
