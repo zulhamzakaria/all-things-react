a60bbaecd53cf0ad770d25e893c3e34af4bb86eb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-23 22:08:23 +0800

    - cehckif user is logged in - redirect to landing page if not

--- scheduling-app/app/dashboard/page.tsx
+++ scheduling-app/app/dashboard/page.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { auth } from "../lib/auth";
+import { redirect } from "next/navigation";
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const session = await auth();
+
+  if (!session?.user) return redirect("/");
+
   return <div>Dashboard</div>;
 };
 
