0ff114958e4b44f9513a2d77663ea1ad50566cbe
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-24 21:51:19 +0800

    - stylings

--- scheduling-app/app/components/AuthModal.tsx
+++ scheduling-app/app/components/AuthModal.tsx
@@ -6,6 +6,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import Image from "next/image";
 
 export function AuthModal() {
   return (
@@ -15,9 +16,20 @@ export function AuthModal() {
           <Button>Click me!</Button>
         </DialogTrigger>
 
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Test!</DialogTitle>
+        <DialogContent className=" max-w-[360px]">
+          <DialogHeader className=" flex flex-row items-center gap-x-2 justify-center">
+            <Image
+              src={"/jn4.png"}
+              alt="logo"
+              height={50}
+              width={50}
+              className=" rounded-md object-cover"
+            />
+            <DialogTitle>
+              <div className=" text-3xl font-semibold font-mono">
+                authentication
+              </div>
+            </DialogTitle>
           </DialogHeader>
           <div>cock!</div>
         </DialogContent>
