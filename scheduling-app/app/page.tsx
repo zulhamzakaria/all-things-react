ae88dbd8b2d404b10364f00c844cdc541b1b5027
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-24 22:36:43 +0800

    - redirect signed-in user to /dashboard

--- scheduling-app/app/page.tsx
+++ scheduling-app/app/page.tsx
@@ -1,7 +1,17 @@
+import { redirect } from "next/navigation";
 import Navbar from "./components/NavBar";
+import { auth } from "./lib/auth";
 
-export default function Home() {
-  return <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-    <Navbar/>
-  </div>;
+export default async function Home() {
+  const session = await auth();
+
+  if (session?.user) {
+    return redirect("/dashboard");
+  }
+
+  return (
+    <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
+      <Navbar />
+    </div>
+  );
 }
