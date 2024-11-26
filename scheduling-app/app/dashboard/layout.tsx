fcaf0178fda092e3056641575b82073f00ea0f1f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-26 21:38:32 +0800

    - basic layout for dashboard items

--- scheduling-app/app/dashboard/layout.tsx
+++ scheduling-app/app/dashboard/layout.tsx
@@ -117,6 +117,9 @@ export default async function DashboardLayout({
               </DropdownMenu>
             </div>
           </header>
+          <main className=" flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+            {children}
+          </main>
         </div>
       </div>
     </>
