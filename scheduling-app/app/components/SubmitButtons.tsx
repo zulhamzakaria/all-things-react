d328995f1da0a8f5b82479dee21d2d319aa10909
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-27 23:26:27 +0800

    - re-usable SubmitButton component

--- scheduling-app/app/components/SubmitButtons.tsx
+++ scheduling-app/app/components/SubmitButtons.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
@@ -33,6 +34,47 @@ export function GithubAuthButton() {
       ) : (
         <Button variant={"outline"} className=" w-full">
           Sign in with GitHub
+        </Button>
+      )}
+    </>
+  );
+}
+
+interface SubmitButtonProps {
+  text: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | null
+    | undefined;
+  className?: string;
+}
+
+export function SubmitButton({ text, variant, className }: SubmitButtonProps) {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button
+          disabled
+          variant={"outline"}
+          className={cn(" w-fit", className)}
+        >
+          <Loader2 className="size-4 mr-2 animate-spin" />
+          submitting...
+        </Button>
+      ) : (
+        <Button
+          type="submit"
+          variant={variant}
+          className={cn(" w-fit", className)}
+        >
+          {text}
         </Button>
       )}
     </>
