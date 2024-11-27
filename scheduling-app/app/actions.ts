9dc632c18922457083f6a77538d79ca6ccdcaae3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-27 22:46:14 +0800

    - applying zod schema

--- scheduling-app/app/actions.ts
+++ scheduling-app/app/actions.ts
@@ -2,16 +2,25 @@
 
 import prisma from "./lib/db";
 import { requireUser } from "./lib/hooks";
+import { parseWithZod } from "@conform-to/zod";
+import { OnboardingSchema } from "./lib/zod-schemas";
 
 export async function OnboardingAction(formData: FormData) {
   const session = await requireUser();
+
+  const submission = parseWithZod(formData, {
+    schema: OnboardingSchema,
+  });
+
+  if (submission.status !== "success") return submission.reply();
+
   const data = await prisma.user.update({
     where: {
       id: session.user?.id,
     },
     data: {
-      username: "john-doe",
-      name: "John Doe",
+      username: submission.value.username,
+      name: submission.value.fullName,
     },
   });
 }
