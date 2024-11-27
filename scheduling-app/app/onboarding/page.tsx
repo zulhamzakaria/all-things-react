74cfac9f06e92e4d2e1be584c04d4bfe4c006247
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-27 15:07:35 +0800

    - onboarding form

--- scheduling-app/app/onboarding/page.tsx
+++ scheduling-app/app/onboarding/page.tsx
@@ -1,7 +1,49 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import React from "react";
 
 const Onboarding = () => {
-  return <div>Onboarding</div>;
+  return (
+    <div className=" min-h-screen w-screen flex items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Welcome to <span className=" text-primary">scheduler!</span>
+          </CardTitle>
+          <CardDescription>
+            Fill in the details to set up your profile
+          </CardDescription>
+        </CardHeader>
+        <CardContent className=" grid gap-y-5">
+          <div className=" grid gap-y-2">
+            <Label>Full Name</Label>
+            <Input placeholder="John Doe" />
+          </div>
+          <div className=" grid gap-y-2">
+            <Label>Username</Label>
+            <div className=" flex rounded-md">
+              <span className=" inline-flex items-center px-3 rounded-l-md border border-r-0 bg-muted text-small text-muted-foreground border-muted">
+                scheduler.com/
+              </span>
+              <Input placeholder="john-doe" className=" rounded-l-none" />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className=" w-full">Submit</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default Onboarding;
