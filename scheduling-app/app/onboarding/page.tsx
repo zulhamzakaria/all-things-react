3f3aa83cf540bc5357d9fdde41771579299a4ec0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-27 23:15:04 +0800

    - applying conform/zod validation

--- scheduling-app/app/onboarding/page.tsx
+++ scheduling-app/app/onboarding/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,10 +13,23 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import React, { useActionState } from "react";
 import { OnboardingAction } from "../actions";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { OnboardingSchema } from "../lib/zod-schemas";
 
 const Onboarding = () => {
+  const [lastResult, action] = useActionState(OnboardingAction, undefined);
 
-  const [lastResult, action] = useActionState(OnboardingAction,undefined)
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: OnboardingSchema,
+      });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
 
   return (
     <div className=" min-h-screen w-screen flex items-center justify-center">
@@ -27,11 +42,17 @@ const Onboarding = () => {
             Fill in the details to set up your profile
           </CardDescription>
         </CardHeader>
-        <form action="">
+        <form action={action} id={form.id} onSubmit={form.onSubmit} noValidate>
           <CardContent className=" grid gap-y-5">
             <div className=" grid gap-y-2">
               <Label>Full Name</Label>
-              <Input placeholder="John Doe" />
+              <Input
+                placeholder="John Doe"
+                name={fields.fullName.name}
+                defaultValue={fields.fullName.initialValue}
+                key={fields.fullName.key}
+              />
+              <p className=" text-rose-500 text-sm">{fields.fullName.errors}</p>
             </div>
             <div className=" grid gap-y-2">
               <Label>Username</Label>
@@ -39,8 +60,15 @@ const Onboarding = () => {
                 <span className=" inline-flex items-center px-3 rounded-l-md border border-r-0 bg-muted text-small text-muted-foreground border-muted">
                   scheduler.com/
                 </span>
-                <Input placeholder="john-doe" className=" rounded-l-none" />
+                <Input
+                  placeholder="john-doe"
+                  className=" rounded-l-none"
+                  name={fields.username.name}
+                  defaultValue={fields.username.initialValue}
+                  key={fields.username.key}
+                />
               </div>
+              <p className=" text-rose-500 text-sm">{fields.username.errors}</p>
             </div>
           </CardContent>
           <CardFooter>
