1d4c1d6534dda0493df57792a3b9208060ceef15
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-28 15:10:24 +0800

    - apply unique username filter

--- scheduling-app/app/actions.ts
+++ scheduling-app/app/actions.ts
@@ -3,13 +3,23 @@
 import prisma from "./lib/db";
 import { requireUser } from "./lib/hooks";
 import { parseWithZod } from "@conform-to/zod";
-import { OnboardingSchema } from "./lib/zod-schemas";
+import { OnboardingSchemaValidation } from "./lib/zod-schemas";
 
 export async function OnboardingAction(prevState: any, formData: FormData) {
   const session = await requireUser();
 
-  const submission = parseWithZod(formData, {
-    schema: OnboardingSchema,
+  const submission = await parseWithZod(formData, {
+    schema: OnboardingSchemaValidation({
+      async isUsernameUnique() {
+        const existingUsername = await prisma.user.findUnique({
+          where: {
+            username: formData.get("username") as string,
+          },
+        });
+
+        return !existingUsername;
+      },
+    }),
   });
 
   if (submission.status !== "success") return submission.reply();
