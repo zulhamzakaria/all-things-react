f30b8c4ea014f5818171e5813a3d5cdde119158e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-28 15:05:35 +0800

    - schema for checking username uniqeness with superRefine()

--- scheduling-app/app/lib/zod-schemas.ts
+++ scheduling-app/app/lib/zod-schemas.ts
@@ -1,3 +1,4 @@
+import { conformZodMessage } from "@conform-to/zod";
 import { z } from "zod";
 
 export const OnboardingSchema = z.object({
@@ -10,3 +11,39 @@ export const OnboardingSchema = z.object({
       message: "Username may only contain letters, number and -",
     }),
 });
+
+export function OnboardingSchemaValidation(options?: {
+  isUsernameUnique: () => Promise<boolean>;
+}) {
+  return z.object({
+    username: z
+      .string()
+      .min(3)
+      .max(50)
+      .regex(/^[a-zA-Z0-9-]+$/, {
+        message: "Username may only contain letters, number and -",
+      })
+      .pipe(
+        z.string().superRefine((_, ctx) => {
+          if (typeof options?.isUsernameUnique !== "function") {
+            ctx.addIssue({
+              code: "custom",
+              message: conformZodMessage.VALIDATION_UNDEFINED,
+              fatal: true,
+            });
+            return;
+          }
+
+          return options.isUsernameUnique().then((isUnique) => {
+            if (!isUnique) {
+              ctx.addIssue({
+                code: "custom",
+                message: "Username is already taken",
+                
+              });
+            }
+          });
+        })
+      ),
+  });
+}
