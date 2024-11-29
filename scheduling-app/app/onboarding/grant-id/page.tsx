a7f6f8cb4a6ae08b618d0f079fae910af0681de4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-11-29 12:25:43 +0800

    - stylings

--- scheduling-app/app/onboarding/grant-id/page.tsx
+++ scheduling-app/app/onboarding/grant-id/page.tsx
@@ -1,7 +1,29 @@
-export default function OnboardingRouteTwo(){
-    return (
-        <div>
-            <h1>for getting nylas grant id</h1>
-        </div>
-    )
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+export default function OnboardingRouteTwo() {
+  return (
+    <div className=" min-h-screen w-screen flex items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>You're almost done!</CardTitle>
+          <CardDescription>
+            We will now connect your calendar to your account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button className=" w-full" asChild>
+            <Link href="/">Connect Calendar to your Account</Link>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
