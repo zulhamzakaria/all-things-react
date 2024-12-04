67947155ad06d39de170fa93ddf7d56dfac98606
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-04 16:43:58 +0800

    - moved Navbar to be directly under main

--- ai-resume/src/app/(main)/layout.tsx
+++ ai-resume/src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./resumes/Navbar";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
