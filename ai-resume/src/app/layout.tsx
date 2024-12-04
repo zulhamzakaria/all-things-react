63f648802213c336a6a98aeba3e6100ea65f62bf
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-04 14:06:12 +0800

    - clerk provider wrapper

--- ai-resume/src/app/layout.tsx
+++ ai-resume/src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - AI Resume Builder',
-    absolute: 'AI Resume Builder'
+    template: "%s - AI Resume Builder",
+    absolute: "AI Resume Builder",
   },
   description: "Next level AI-assisted Resume Builder",
 };
@@ -18,12 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={inter.className}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
