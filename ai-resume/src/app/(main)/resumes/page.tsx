490a39f4e7a6e837376fa16e804c35824dc0709e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-05 16:17:31 +0800

    - stlyings

--- ai-resume/src/app/(main)/resumes/page.tsx
+++ ai-resume/src/app/(main)/resumes/page.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { PlusSquare } from "lucide-react";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "your resumes",
+};
+
 export default function Page() {
-  return <h1>hello from Page</h1>;
+  return (
+    <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
+      <Button asChild className="mx-auto flex w-fit gap-2">
+        <Link href={"/editor"}>
+          <PlusSquare className="size-5" />
+          <span className="font-extrabold tracking-tighter">new resume</span>
+        </Link>
+      </Button>
+    </main>
+  );
 }
