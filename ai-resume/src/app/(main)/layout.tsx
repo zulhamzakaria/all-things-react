c67fa436b25fca864aa70992f487f5c25f5f35a5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-05 16:18:35 +0800

    - stylings

--- ai-resume/src/app/(main)/layout.tsx
+++ ai-resume/src/app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col">
       <Navbar />
       {children}
     </div>
