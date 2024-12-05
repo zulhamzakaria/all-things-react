2b2625c918f3d79e96d2846734c2ab987c9d90ce
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-05 15:07:41 +0800

    - theme for clerk User's menu

--- ai-resume/src/app/(main)/Navbar.tsx
+++ ai-resume/src/app/(main)/Navbar.tsx
@@ -6,8 +6,11 @@ import jn6 from "@/assets/jn6.png";
 import { UserButton } from "@clerk/nextjs";
 import { CreditCard } from "lucide-react";
 import { ModeToggle } from "@/components/ModeToggle";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { theme } = useTheme();
   return (
     <header className="shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
@@ -27,6 +30,7 @@ export default function Navbar() {
           <ModeToggle />
           <UserButton
             appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
               elements: {
                 avatarBox: {
                   width: 35,
