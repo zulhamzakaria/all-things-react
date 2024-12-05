3a9f26ed19f1b4d539c0a76d3b54ab36435fe526
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-05 16:27:07 +0800

    - added metadata - metadata only usable in a server page

--- ai-resume/src/app/(main)/editor/page.tsx
+++ ai-resume/src/app/(main)/editor/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "design your resume",
+};
 
 const Editor = () => {
-  return (
-    <div>Editor</div>
-  )
-}
+  return <div>Editor</div>;
+};
 
-export default Editor
+export default Editor;
