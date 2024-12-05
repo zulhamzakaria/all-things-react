e251af5015e95a1278fea6ce302a7215a9f061f6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-05 14:57:56 +0800

    - import ThemeProvider from ThemeToggle.tsx not next-theemse

--- ai-resume/src/app/layout.tsx
+++ ai-resume/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/ThemeToggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
