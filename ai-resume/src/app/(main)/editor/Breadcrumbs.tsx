827b40717153bddc9598d79fe5bdc490a1437f69
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-06 21:58:36 +0800

    - breadcrumb component

---
+++ ai-resume/src/app/(main)/editor/Breadcrumbs.tsx
@@ -0,0 +1,49 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import React from "react";
+import { steps } from "./steps";
+
+interface BreadcrumbsProps {
+  currentStep: string;
+  setCurrentStep: (step: string) => void;
+}
+
+export default function Breadcrumbs({
+  currentStep,
+  setCurrentStep,
+}: BreadcrumbsProps) {
+  return (
+    <div className="flex justify-center">
+      <Breadcrumb>
+        <BreadcrumbList>
+          {steps.map((step) => (
+            <React.Fragment key={step.key}>
+              <BreadcrumbItem>
+                {step.key === currentStep ? (
+                  <div>
+                    <BreadcrumbPage>{step.title}</BreadcrumbPage>
+                  </div>
+                ) : (
+                  <div>
+                    <BreadcrumbLink asChild>
+                      <button onClick={() => setCurrentStep(step.key)}>
+                        {step.title}
+                      </button>
+                    </BreadcrumbLink>
+                  </div>
+                )}
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
