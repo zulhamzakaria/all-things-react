0becd54ef64be66dbf2972d61b7daed88e0ee60b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-07 00:27:11 +0800

    - WIP

---
+++ ai-resume/src/app/(main)/editor/forms/WorkExperienceForm.tsx
@@ -0,0 +1,33 @@
+import { EditorFormProps } from "@/lib/types";
+import { workExperienceSchema, WorkExperienceValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+export default function WorkExperienceForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<WorkExperienceValues>({
+    resolver: zodResolver(workExperienceSchema),
+    defaultValues: {
+      workExperiences: resumeData.workExperiences || [],
+    },
+  });
+
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      //update resume data
+      // get only existing workExperience, if theres none, use empty array
+      setResumeData({
+        ...resumeData,
+        workExperiences:
+          values.workExperiences?.filter((exp) => exp !== undefined) || [],
+      });
+    });
+    // ensure that theres always one form watcher
+    return unsubscribe;
+  }, [form, resumeData, setResumeData]);
+}
