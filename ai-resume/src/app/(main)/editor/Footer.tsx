b65d524bcfef69139004050ff3d4ee256fc171ce
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-06 22:23:15 +0800

    - whats happening

--- ai-resume/src/app/(main)/editor/Footer.tsx
+++ ai-resume/src/app/(main)/editor/Footer.tsx
@@ -1,18 +1,40 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { steps } from "./steps";
 
 interface FooterProps {
   currentStep: string;
   setCurrentStep: (step: string) => void;
 }
 
-export function Footer({currentStep, setCurrentStep}:FooterProps) {
+export function Footer({ currentStep, setCurrentStep }: FooterProps) {
+  const previousStep = steps.find(
+    (_, index) => steps[index + 1].key === currentStep,
+  )?.key;
+
+  const nextStep = steps.find(
+    (_, index) => steps[index - 1]?.key === currentStep,
+  )?.key;
+
   return (
     <footer className="w-full border-t px-3 py-5">
       <div className="mx-auto flex max-w-7xl flex-wrap justify-between gap-3">
         <div className="flex items-center gap-3">
-          <Button variant={"secondary"}>previous step</Button>
-          <Button>next step</Button>
+          <Button
+            variant={"secondary"}
+            onClick={
+              previousStep ? () => setCurrentStep(previousStep) : undefined
+            }
+            disabled={!previousStep}
+          >
+            previous step
+          </Button>
+          <Button
+            onClick={nextStep ? () => setCurrentStep(nextStep) : undefined}
+            disabled={!nextStep}
+          >
+            next step
+          </Button>
         </div>
         <div className="flex items-center gap-3">
           <Button variant={"secondary"} asChild>
