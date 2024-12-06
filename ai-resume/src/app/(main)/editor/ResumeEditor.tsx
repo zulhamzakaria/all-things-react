20f0b8397bc24a183b1ffc780087b21a042c5a48
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-06 22:12:23 +0800

    - moved footer to another file

--- ai-resume/src/app/(main)/editor/ResumeEditor.tsx
+++ ai-resume/src/app/(main)/editor/ResumeEditor.tsx
@@ -40,20 +40,7 @@ const ResumeEditor = () => {
           <div className="hidden w-1/2 md:flex">right</div>
         </div>
       </main>
-      <footer className="w-full border-t px-3 py-5">
-        <div className="mx-auto flex max-w-7xl flex-wrap justify-between gap-3">
-          <div className="flex items-center gap-3">
-            <Button variant={"secondary"}>previous step</Button>
-            <Button>next step</Button>
-          </div>
-          <div className="flex items-center gap-3">
-            <Button variant={"secondary"} asChild>
-              <Link href={"/resumes"}>close</Link>
-            </Button>
-            <p className="text-muted-foreground opacity-0">saving...</p>
-          </div>
-        </div>
-      </footer>
+      
     </div>
   );
 };
