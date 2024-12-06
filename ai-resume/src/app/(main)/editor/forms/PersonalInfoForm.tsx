b3828811977083803056d77c4847388f3860ce69
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-06 21:25:39 +0800

    - form (WIP)

--- ai-resume/src/app/(main)/editor/forms/PersonalInfoForm.tsx
+++ ai-resume/src/app/(main)/editor/forms/PersonalInfoForm.tsx
@@ -1,3 +1,59 @@
-export default function PersonalInfoForm(){
-    return <div>Personal Info Form</div>
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { personalInfoSchema, PersonalInfoValue } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+export default function PersonalInfoForm() {
+  const form = useForm<PersonalInfoValue>({
+    resolver: zodResolver(personalInfoSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      jobTitle: "",
+      city: "",
+      country: "",
+      phone: "",
+      email: "",
+    },
+  });
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">personal info</h2>
+        <p className="text-sm text-muted-foreground">tell us about yourself</p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name="photo"
+            render={({ field: { value, ...fieldValues } }) => (
+              <FormItem>
+                <FormLabel>your photo</FormLabel>
+                <FormControl>
+                  <Input
+                    {...fieldValues}
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => {
+                      // only accepts one file
+                      const file = e.target.files?.[0];
+                      fieldValues.onChange(file);
+                    }}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          ></FormField>
+        </form>
+      </Form>
+    </div>
+  );
 }
