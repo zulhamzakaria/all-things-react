c601e5143b976162309bf70a0892692337088b63
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-06 19:02:08 +0800

    - stylings

--- ai-resume/src/app/(main)/editor/ResumeEditor.tsx
+++ ai-resume/src/app/(main)/editor/ResumeEditor.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const ResumeEditor = () => {
   return (
@@ -12,12 +13,24 @@ const ResumeEditor = () => {
           automatically.
         </p>
       </header>
-      <main>main</main>
+      <main className="relative grow">
+        <div className="absolute bottom-0 top-0 flex w-full">
+          <div className="w-full md:w-1/2">left</div>
+          <div className="grow md:border-r "/>
+          <div className=" hidden w-1/2 md:flex ">right</div>
+        </div>
+      </main>
       <footer className="w-full border-t px-3 py-5">
         <div className="mx-auto flex max-w-7xl flex-wrap justify-between gap-3">
           <div className="flex items-center gap-3">
             <Button variant={"secondary"}>previous step</Button>
             <Button>next step</Button>
+          </div>
+          <div className="flex items-center gap-3">
+            <Button variant={"secondary"} asChild>
+              <Link href={"/resumes"}>close</Link>
+            </Button>
+            <p className="text-muted-foreground opacity-0">saving...</p>
           </div>
         </div>
       </footer>
