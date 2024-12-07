8150e0a30f02652b5b81ea02d1556eec0304363b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-07 22:17:01 +0800

    - work experience form (WIP)

--- ai-resume/src/app/(main)/editor/forms/WorkExperienceForm.tsx
+++ ai-resume/src/app/(main)/editor/forms/WorkExperienceForm.tsx
@@ -1,8 +1,9 @@
+import { Form } from "@/components/ui/form";
 import { EditorFormProps } from "@/lib/types";
 import { workExperienceSchema, WorkExperienceValues } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 
 export default function WorkExperienceForm({
   resumeData,
@@ -30,4 +31,31 @@ export default function WorkExperienceForm({
     // ensure that theres always one form watcher
     return unsubscribe;
   }, [form, resumeData, setResumeData]);
+
+  const { fields, append } = useFieldArray({
+    control: form.control,
+    name: "workExperiences",
+  });
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">work experience</h2>
+        <p className="text-sm text-muted-foreground">
+          add your working experiences here
+        </p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          {fields.map((field) => (
+            <WorkExperienceItem key={field.id} />
+          ))}
+        </form>
+      </Form>
+    </div>
+  );
+}
+
+function WorkExperienceItem() {
+  return <div>work experience item</div>;
 }
