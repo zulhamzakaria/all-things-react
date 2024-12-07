6e2d0ced9c1ea6e59543cb197ce90eeec337d3f3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-07 22:18:47 +0800

    - added step for WorkExperienceForm for Breadcrumb

--- ai-resume/src/app/(main)/editor/steps.ts
+++ ai-resume/src/app/(main)/editor/steps.ts
@@ -1,6 +1,7 @@
 import { EditorFormProps } from "@/lib/types";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
+import WorkExperienceForm from "./forms/WorkExperienceForm";
 
 export const steps: {
   title: string;
@@ -9,4 +10,5 @@ export const steps: {
 }[] = [
   { title: "General info", component: GeneralInfoForm, key: "general-info" },
   { title: "Personal info", component: PersonalInfoForm, key: "personal-info" },
+  { title: "Work experience", component: WorkExperienceForm, key: "work-experience" },
 ];
