e22017e2e9c7f00c60198189430c218ac199d810
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-08 22:28:04 +0800

    - added Education breadcrumb

--- ai-resume/src/app/(main)/editor/steps.ts
+++ ai-resume/src/app/(main)/editor/steps.ts
@@ -2,6 +2,7 @@ import { EditorFormProps } from "@/lib/types";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
 import WorkExperienceForm from "./forms/WorkExperienceForm";
+import EducationForm from "./forms/EducationForm";
 
 export const steps: {
   title: string;
@@ -10,5 +11,10 @@ export const steps: {
 }[] = [
   { title: "General info", component: GeneralInfoForm, key: "general-info" },
   { title: "Personal info", component: PersonalInfoForm, key: "personal-info" },
-  { title: "Work experience", component: WorkExperienceForm, key: "work-experience" },
+  {
+    title: "Work experience",
+    component: WorkExperienceForm,
+    key: "work-experience",
+  },
+  { title: "Education", component: EducationForm, key: "education" },
 ];
