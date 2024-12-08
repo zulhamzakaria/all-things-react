672a48ce78b5d1fa93d2aa4b185d8b1eaa7aa1f8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-08 23:42:38 +0800

    - fomr (WIP)

---
+++ ai-resume/src/app/(main)/editor/forms/SkillsForm.tsx
@@ -0,0 +1,44 @@
+import { EditorFormProps } from "@/lib/types";
+import { skillsSchema, SkillValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+
+export default function SkillsForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<SkillValues>({
+    resolver: zodResolver(skillsSchema),
+    defaultValues: {
+      skills: resumeData.skills || [],
+    },
+  });
+
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      //update resume data
+      setResumeData({
+        ...resumeData,
+        skills:
+          values.skills
+            ?.filter((skill) => skill !== undefined)
+            .map((skill) => skill.trim())
+            .filter((skill) => skill !== "") || [],
+      });
+    });
+    // ensure that theres always one form watcher
+    return unsubscribe;
+  }, [form, resumeData, setResumeData]);
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="fonsem text-2xl">skills</h2>
+        <p className="txtsm text-muted-foreground">what are you good at?</p>
+      </div>
+    </div>
+  );
+}
