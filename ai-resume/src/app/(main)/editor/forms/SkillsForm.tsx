c66eaeef9196bebe4bdcef1e4eac958289e89749
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-08 23:56:04 +0800

    - fomr

--- ai-resume/src/app/(main)/editor/forms/SkillsForm.tsx
+++ ai-resume/src/app/(main)/editor/forms/SkillsForm.tsx
@@ -1,8 +1,18 @@
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
 import { EditorFormProps } from "@/lib/types";
 import { skillsSchema, SkillValues } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
-import { useFieldArray, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 export default function SkillsForm({
   resumeData,
@@ -39,6 +49,33 @@ export default function SkillsForm({
         <h2 className="fonsem text-2xl">skills</h2>
         <p className="txtsm text-muted-foreground">what are you good at?</p>
       </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name="skills"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="sr-only">skills</FormLabel>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder=".net, javascript, typescript"
+                    onChange={(e) => {
+                      const skills = e.target.value.split(",");
+                      field.onChange(skills);
+                    }}
+                  />
+                </FormControl>
+                <FormDescription>
+                  each skill must be separated with a comma
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </div>
   );
 }
