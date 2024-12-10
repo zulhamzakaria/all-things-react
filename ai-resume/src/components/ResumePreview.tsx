23aacc047a7467229d3c2e8ce4cd9f4fee2ed2a6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-10 14:27:09 +0800

    - div for calculating the width based on  ratio

--- ai-resume/src/components/ResumePreview.tsx
+++ ai-resume/src/components/ResumePreview.tsx
@@ -1,5 +1,7 @@
+import useDimensions from "@/hooks/useDimensions";
 import { cn } from "@/lib/utils";
 import { ResumeValues } from "@/lib/validation";
+import { useRef } from "react";
 
 interface ResumePreviewProps {
   resumeData: ResumeValues;
@@ -10,16 +12,21 @@ export default function ResumePreview({
   resumeData,
   className,
 }: ResumePreviewProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { width } = useDimensions(containerRef);
   return (
     <div
       className={cn(
         "aspect-[210/297] h-fit w-full bg-white text-black",
         className,
       )}
+      ref={containerRef}
     >
-      <h1 className="p-6 text-3xl font-bold">
-        this text should change with the size of the container div
-      </h1>
+      <div style={{ zoom: (1 / 794) * width }} className={cn('space-y-6 p-6', !width && 'invisible')}>
+        <h1 className="p-6 text-3xl font-bold">
+          this text should change with the size of the container div
+        </h1>
+      </div>
     </div>
   );
 }
