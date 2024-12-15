7f43a427c44d61c4c8a092df4996bdbe06b80fb3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-16 07:20:49 +0800

    - implementing showSmResumePreview/setShowSmResumePreview

--- ai-resume/src/app/(main)/editor/ResumeEditor.tsx
+++ ai-resume/src/app/(main)/editor/ResumeEditor.tsx
@@ -7,11 +7,14 @@ import { Footer } from "./Footer";
 import { useState } from "react";
 import { ResumeValues } from "@/lib/validation";
 import ResumePreviewSection from "./ResumePreviewSection";
+import { cn } from "@/lib/utils";
 
 const ResumeEditor = () => {
   const searchParams = useSearchParams();
 
   const [resumeData, setResumeData] = useState<ResumeValues>({});
+
+  const [showSmResumePreview, setShowSmResumePreview] = useState(false);
 
   const currentStep = searchParams.get("step") || steps[0].key;
   function setStep(key: string) {
@@ -36,7 +39,12 @@ const ResumeEditor = () => {
       </header>
       <main className="relative grow">
         <div className="bottom-0 top-0 flex w-full">
-          <div className="w-full overflow-y-auto p-3 md:w-1/2">
+          <div
+            className={cn(
+              "w-full overflow-y-auto p-3 md:block md:w-1/2",
+              showSmResumePreview && "hidden",
+            )}
+          >
             <Breadcrumbs currentStep={currentStep} setCurrentStep={setStep} />
             {FormComponent && (
               <FormComponent
@@ -49,10 +57,16 @@ const ResumeEditor = () => {
           <ResumePreviewSection
             resumeData={resumeData}
             setResumeData={setResumeData}
+            className={cn(showSmResumePreview && "flex")}
           />
         </div>
       </main>
-      <Footer currentStep={currentStep} setCurrentStep={setStep} />
+      <Footer
+        currentStep={currentStep}
+        setCurrentStep={setStep}
+        showSmResumePreview={showSmResumePreview}
+        setShowSmResumePreview={setShowSmResumePreview}
+      />
     </div>
   );
 };
