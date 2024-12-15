c723a1fa6070bd9a675abd2f41a5ef9f9cb1ab15
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-16 07:16:23 +0800

    - stylings

--- ai-resume/src/app/(main)/editor/ResumePreviewSection.tsx
+++ ai-resume/src/app/(main)/editor/ResumePreviewSection.tsx
@@ -2,19 +2,24 @@ import ResumePreview from "@/components/ResumePreview";
 import { ResumeValues } from "@/lib/validation";
 import { ColorPicker } from "./ColorPicker";
 import BorderStyle from "./BorderStyle";
+import { cn } from "@/lib/utils";
 
 interface ResumePreviewSectionProps {
   resumeData: ResumeValues;
   setResumeData: (data: ResumeValues) => void;
+  className?: string;
 }
 
 export default function ResumePreviewSection({
   resumeData,
   setResumeData,
+  className,
 }: ResumePreviewSectionProps) {
   return (
-    <div className="group relative hidden w-1/2 md:flex">
-      <div className="absolute left-1 top-1 transition-opacity flex flex-none flex-col gap-1 opacity-50 group-hover:opacity-100 lg:left-3 lg:top-3 xl:opacity-100">
+    <div
+      className={cn("group relative hidden w-full md:flex md:w-1/2", className)}
+    >
+      <div className="absolute left-1 top-1 flex flex-none flex-col gap-1 opacity-50 transition-opacity group-hover:opacity-100 lg:left-3 lg:top-3 xl:opacity-100">
         <ColorPicker
           color={resumeData.colorHex}
           onChange={(color) =>
