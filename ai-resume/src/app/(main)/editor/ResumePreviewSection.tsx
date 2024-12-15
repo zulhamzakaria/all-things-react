1576296d7b2c6126d625e93302bf60f7085ac2a0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-15 21:10:35 +0800

    - apply ColotPicker

--- ai-resume/src/app/(main)/editor/ResumePreviewSection.tsx
+++ ai-resume/src/app/(main)/editor/ResumePreviewSection.tsx
@@ -1,5 +1,6 @@
 import ResumePreview from "@/components/ResumePreview";
 import { ResumeValues } from "@/lib/validation";
+import { ColorPicker } from "./ColorPicker";
 
 interface ResumePreviewSectionProps {
   resumeData: ResumeValues;
@@ -8,9 +9,18 @@ interface ResumePreviewSectionProps {
 
 export default function ResumePreviewSection({
   resumeData,
+  setResumeData,
 }: ResumePreviewSectionProps) {
   return (
-    <div className="hidden w-1/2 md:flex">
+    <div className="relative hidden w-1/2 md:flex">
+      <div>
+        <ColorPicker
+          color={resumeData.colorHex}
+          onChange={(color) =>
+            setResumeData({ ...resumeData, colorHex: color.hex })
+          }
+        />
+      </div>
       <div className="flex w-full justify-center overflow-y-auto bg-secondary p-3">
         <ResumePreview
           resumeData={resumeData}
