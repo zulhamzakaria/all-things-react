af978286ec98f1049b456b1cac7e9dab519effee
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-15 21:07:50 +0800

    - WIP

--- ai-resume/src/app/(main)/editor/ColorPicker.tsx
+++ ai-resume/src/app/(main)/editor/ColorPicker.tsx
@@ -1,3 +1,24 @@
-export function ColorPicker() {
-  return <div>red</div>;
+import { Button } from "@/components/ui/button";
+import { Popover } from "@/components/ui/popover";
+import { PopoverTrigger } from "@radix-ui/react-popover";
+import { PaletteIcon } from "lucide-react";
+import { useState } from "react";
+import { Color, ColorChangeHandler } from "react-color";
+
+interface ColorPickerProps {
+  color: Color | undefined;
+  onChange: ColorChangeHandler;
+}
+
+export function ColorPicker({ color, onChange }: ColorPickerProps) {
+  const [showPopover, setShowPopover] = useState(false);
+  return (
+    <Popover open={showPopover} onOpenChange={setShowPopover}>
+      <PopoverTrigger asChild>
+        <Button>
+          <PaletteIcon className="size-5" />
+        </Button>
+      </PopoverTrigger>
+    </Popover>
+  );
 }
