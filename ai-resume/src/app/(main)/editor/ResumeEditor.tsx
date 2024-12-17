0aef1418e8d9ad9e522c1e594318f9c0ad2e1cd9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-17 20:14:49 +0800

    - passing useAutoSaveResume hook isSaving to footer isSaving

--- ai-resume/src/app/(main)/editor/ResumeEditor.tsx
+++ ai-resume/src/app/(main)/editor/ResumeEditor.tsx
@@ -72,6 +72,7 @@ const ResumeEditor = () => {
         setCurrentStep={setStep}
         showSmResumePreview={showSmResumePreview}
         setShowSmResumePreview={setShowSmResumePreview}
+        isSaving={isSaving}
       />
     </div>
   );
