624f0b832d537724e0466de6496fa5247b0efa49
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-18 17:38:31 +0800

    - assign image metadata to an object manually instead of using spread oerator i.e ...value

--- ai-resume/src/lib/utils.ts
+++ ai-resume/src/lib/utils.ts
@@ -8,7 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 export function fileReplacer(key: unknown, value: unknown) {
   return value instanceof File
     ? {
-        ...value,
+        name: value.name,
+        size: value.size,
+        type: value.type,
+        lastModified: value.lastModified,
       }
     : value;
 }
