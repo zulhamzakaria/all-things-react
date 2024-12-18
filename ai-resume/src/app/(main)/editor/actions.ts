ce25c5d31dc061846e179ece8666094afc2f99e0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-18 09:08:53 +0800

    - server action for caving resume data

--- ai-resume/src/app/(main)/editor/actions.ts
+++ ai-resume/src/app/(main)/editor/actions.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import prisma from "@/lib/prisma";
 import { resumeSchema, ResumeValues } from "@/lib/validation";
 import { auth } from "@clerk/nextjs/server";
+import { del, put } from "@vercel/blob";
+import path from "path";
 
 export async function saveResume(values: ResumeValues) {
   const { id } = values;
@@ -13,4 +16,77 @@ export async function saveResume(values: ResumeValues) {
     resumeSchema.parse(values);
   const { userId } = await auth();
   if (!userId) throw new Error("Invalid action!");
+
+  const existingResume = id
+    ? await prisma.resume.findUnique({ where: { id, userId } })
+    : null;
+
+  if (id && !existingResume) throw new Error("Resume not found");
+
+  //save photo to blob storage then get the URL
+  let newPhotoURL: string | undefined | null = undefined;
+
+  if (photo instanceof File) {
+    // replace existing image with the latest one
+    if (existingResume?.photoUrl) await del(existingResume.photoUrl);
+    const blob = await put(`resume_photos/${path.extname(photo.name)}`, photo, {
+      access: "public",
+    });
+
+    newPhotoURL = blob.url;
+  } else {
+    if (photo === null) {
+      if (existingResume?.photoUrl) await del(existingResume.photoUrl);
+    }
+    newPhotoURL = null;
+  }
+
+  if (id) {
+    return prisma.resume.update({
+      where: { id },
+      data: {
+        ...resumeValues,
+        photoUrl: newPhotoURL,
+        workExperiences: {
+          deleteMany: {},
+          create: workExperiences?.map((exp) => ({
+            ...exp,
+            startDate: exp.startDate ? new Date(exp.startDate) : undefined,
+            endDate: exp.endDate ? new Date(exp.endDate) : undefined,
+          })),
+        },
+        educations: {
+          deleteMany: {},
+          create: educations?.map((edu) => ({
+            ...edu,
+            startDate: edu.startDate ? new Date(edu.startDate) : undefined,
+            endDate: edu.endDate ? new Date(edu.endDate) : undefined,
+          })),
+        },
+        updatedAt: new Date(),
+      },
+    });
+  } else {
+    return prisma.resume.create({
+      data: {
+        ...resumeValues,
+        userId,
+        photoUrl: newPhotoURL,
+        workExperiences: {
+          create: workExperiences?.map((exp) => ({
+            ...exp,
+            startDate: exp.startDate ? new Date(exp.startDate) : undefined,
+            endDate: exp.endDate ? new Date(exp.endDate) : undefined,
+          })),
+        },
+        educations: {
+          create: educations?.map((edu) => ({
+            ...edu,
+            startDate: edu.startDate ? new Date(edu.startDate) : undefined,
+            endDate: edu.endDate ? new Date(edu.endDate) : undefined,
+          })),
+        },
+      },
+    });
+  }
 }
