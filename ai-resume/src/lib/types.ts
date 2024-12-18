d6c2935ad6f061c5e2ebea130e4e0200cc092e1e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-18 17:53:02 +0800

    - external const for getting includes

--- ai-resume/src/lib/types.ts
+++ ai-resume/src/lib/types.ts
@@ -1,6 +1,12 @@
+import { Prisma } from "@prisma/client";
 import { ResumeValues } from "./validation";
 
 export interface EditorFormProps {
   resumeData: ResumeValues;
   setResumeData: (data: ResumeValues) => void;
 }
+
+export const resumeDataInclude = {
+  workExperiences: true,
+  educations: true,
+} satisfies Prisma.ResumeInclude
