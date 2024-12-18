1ea1ed23bf892e2281690165aa3f62d44665db70
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-18 18:13:37 +0800

    - data mapper between ResumeServerData and resumeValues

--- ai-resume/src/lib/utils.ts
+++ ai-resume/src/lib/utils.ts
@@ -1,5 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { ResumeServerData } from "./types";
+import { ResumeValues } from "./validation";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -14,4 +16,37 @@ export function fileReplacer(key: unknown, value: unknown) {
         lastModified: value.lastModified,
       }
     : value;
+}
+
+export function mapToResumeValues(data: ResumeServerData): ResumeValues {
+  return {
+    id: data.id,
+    title: data.title || undefined,
+    description: data.description || undefined,
+    photo: data.photoUrl || undefined,
+    firstName: data.firstName || undefined,
+    lastName: data.lastName || undefined,
+    jobTitle: data.jobTitle || undefined,
+    city: data.city || undefined,
+    country: data.country || undefined,
+    phone: data.phone || undefined,
+    email: data.email || undefined,
+    workExperiences: data.workExperiences.map((exp) => ({
+      position: exp.position || undefined,
+      company: exp.company || undefined,
+      startDate: exp.startDate?.toISOString().split("T")[0], //split at time since only data is needed
+      endDate: exp.endDate?.toISOString().split("T")[0], //split at time since only data is needed
+      description: exp.description || undefined,
+    })),
+    educations: data.educations.map((edu) => ({
+      degree: edu.degree || undefined,
+      school: edu.school || undefined,
+      startDate: edu.startDate?.toISOString().split("T")[0], //split at time since only data is needed
+      endDate: edu.endDate?.toISOString().split("T")[0], //split at time since only data is needed
+    })),
+    skills: data.skills,
+    borderStyle: data.border, //border from prisma schema
+    colorHex: data.colorHex,
+    summary: data.summary || undefined,
+  };
 }
