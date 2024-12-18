4e5a566e70511c29f1fdd3e8e325455366db10d4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-18 18:18:43 +0800

    image remotePatterns

--- ai-resume/next.config.ts
+++ ai-resume/next.config.ts
@@ -2,11 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental:{
-    serverActions:{
-      bodySizeLimit: '4mb'
-    }
-  }
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "4mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
