6991bfe5e37f672312e316e2e8bd3fffb3983c6c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-18 17:54:34 +0800

    - exprt type of resumeDataInclude

--- ai-resume/src/lib/types.ts
+++ ai-resume/src/lib/types.ts
@@ -10,3 +10,5 @@ export const resumeDataInclude = {
   workExperiences: true,
   educations: true,
 } satisfies Prisma.ResumeInclude
+
+export type ResumeServerData = Prisma.ResumeGetPayload<{include: typeof resumeDataInclude}>
