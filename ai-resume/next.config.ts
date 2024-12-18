acaaebc2304841ebc621bbd3eb5b1a9893ab3a8d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-18 09:10:22 +0800

    - for limiting the image size

--- ai-resume/next.config.ts
+++ ai-resume/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental:{
+    serverActions:{
+      bodySizeLimit: '4mb'
+    }
+  }
 };
 
 export default nextConfig;
