8100384b94dc37e5c6797833affaa3c4e2797baa
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-18 17:51:04 +0800

    - when refreshed, reload the resume - searchParams should be used with Promise<> - wip

--- ai-resume/src/app/(main)/editor/page.tsx
+++ ai-resume/src/app/(main)/editor/page.tsx
@@ -1,11 +1,30 @@
 import { Metadata } from "next";
 import ResumeEditor from "./ResumeEditor";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+interface PageProps {
+  searchParams: Promise<{ resumeId?: string }>;
+}
 
 export const metadata: Metadata = {
   title: "design your resume",
 };
 
-const Editor = () => {
+const Editor = async ({ searchParams }: PageProps) => {
+  const { resumeId } = await searchParams;
+  const { userId } = await auth();
+  if (!userId) return null;
+  const resumeToEdit = resumeId
+    ? await prisma.resume.findUnique({
+        where: { id: resumeId, userId },
+        include: {
+          workExperiences: true,
+          educations: true,
+        },
+      })
+    : null;
+
   return (
     <div>
       <ResumeEditor />
