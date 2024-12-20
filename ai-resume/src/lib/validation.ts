15173fbe65fef9c5d2b75a01ec24dd53a2c53f69
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-20 20:22:00 +0800

    - summary shema and type

--- ai-resume/src/lib/validation.ts
+++ ai-resume/src/lib/validation.ts
@@ -86,3 +86,12 @@ export type ResumeValues = Omit<z.infer<typeof resumeSchema>, "photo"> & {
   id?: string;
   photo?: File | string | undefined;
 };
+
+export const generateSummarySchema = z.object({
+  jobTitle: optionalString,
+  ...workExperienceSchema.shape,
+  ...educationSchema.shape,
+  ...skillsSchema.shape,
+});
+
+export type GenerateSummaryInput = z.infer<typeof generateSummarySchema>;
