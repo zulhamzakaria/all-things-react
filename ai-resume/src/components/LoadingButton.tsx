dd72a8599211affa7c1379359b3ddd7c529a9d56
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-12-24 15:24:03 +0800

    - extends ButtonProps - WIP

--- ai-resume/src/components/LoadingButton.tsx
+++ ai-resume/src/components/LoadingButton.tsx
@@ -1,1 +1,17 @@
-export default function LoadingButton() {}
+import { Loader2 } from "lucide-react";
+import { Button, ButtonProps } from "./ui/button";
+
+interface LoadingButtonProps extends ButtonProps {
+  loading: boolean;
+}
+
+export default function LoadingButton({
+  loading,
+  disabled,
+  className,
+  ...props
+}: LoadingButtonProps) {
+  return (
+    <Button>{loading && <Loader2 className="size-5 animate-spin" />}</Button>
+  );
+}
