09c381b850e2b1bd1e8b56020534cca4d8007bd3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-19 23:28:14 +0800

    - applied theme-toggle

--- lms-course-platform/app/page.tsx
+++ lms-course-platform/app/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello, World</h1>
+      <ModeToggle />
     </div>
   );
 }
