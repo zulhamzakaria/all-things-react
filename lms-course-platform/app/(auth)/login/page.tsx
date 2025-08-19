9a57ae9dab397c00a14ff74cca850cef78a37b81
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-19 23:19:05 +0800

    - stylings

--- lms-course-platform/app/(auth)/login/page.tsx
+++ lms-course-platform/app/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { GithubIcon } from "lucide-react";
 
 export default function LoginPage() {
   return (
@@ -12,6 +15,12 @@ export default function LoginPage() {
         <CardTitle className="text-xl">Welcome back!</CardTitle>
         <CardDescription>Login with your Github Email Account</CardDescription>
       </CardHeader>
+      <CardContent>
+        <Button className="w-full" variant={"outline"}>
+          <GithubIcon className="size-4" />
+          Sign in with Github
+        </Button>
+      </CardContent>
     </Card>
   );
 }
