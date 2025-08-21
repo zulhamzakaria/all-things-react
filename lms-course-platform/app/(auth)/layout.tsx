259b44e71ae47ba9ae4eeee0e3fd90336f485e2a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-21 22:45:58 +0800

    - Link: prefetching, client side navigation (no refreshing) - className={buttonVariants()} : inherits all of button props

--- lms-course-platform/app/(auth)/layout.tsx
+++ lms-course-platform/app/(auth)/layout.tsx
@@ -1,8 +1,21 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
+      <Link
+        href={"/"}
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute top-4 left-4",
+        })}
+      >
+        <ArrowLeft className="size-4" />
+        Back
+      </Link>
       <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
     </div>
   );
