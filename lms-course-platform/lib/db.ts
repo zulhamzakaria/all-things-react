1ae85e4ee78af8f60b421b54bc232b2f56dd1234
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-22 00:13:18 +0800

    - to import prisma client, create a model first (can be a dummy), push it to db (i.e Neon) - then add import (remove default import > quick fix)

--- lms-course-platform/lib/db.ts
+++ lms-course-platform/lib/db.ts
@@ -1,3 +1,5 @@
+import { PrismaClient } from "./generated/prisma";
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
