3ff52d406ca405a5f55f8a80396bacb00a98dcfd
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-22 17:58:01 +0800

    - imported toaster from component

--- lms-course-platform/app/layout.tsx
+++ lms-course-platform/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
