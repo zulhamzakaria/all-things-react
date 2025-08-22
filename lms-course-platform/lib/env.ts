42842cbfd8db87b782d11b53512e95a84a23a529
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-22 17:29:10 +0800

    - validator file for env params

---
+++ lms-course-platform/lib/env.ts
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    BETTER_AUTH_URL: z.string().url(),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1),
+  },
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  experimental__runtimeEnv: {},
+});
