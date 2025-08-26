073dc0cdc8b09476a8a55494107c770342f04e04
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-26 19:35:15 +0800

    - user session from auth.api.session()

--- lms-course-platform/app/page.tsx
+++ lms-course-platform/app/page.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/theme-toggle";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   return (
     <div>
       <h1>Hello, World</h1>
       <ModeToggle />
+      {session ? <p>{session.user.name}</p> : <Button>Sign in</Button>}
     </div>
   );
 }
