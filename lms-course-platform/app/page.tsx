5b11b6b40ac66557eed31628188e6a2a74a2b265
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-28 21:54:53 +0800

    - signout button

--- lms-course-platform/app/page.tsx
+++ lms-course-platform/app/page.tsx
@@ -3,15 +3,35 @@
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/theme-toggle";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { data: session } = authClient.useSession();
+
+  const router = useRouter();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+      },
+    });
+  }
 
   return (
     <div>
       <h1>Hello, World</h1>
       <ModeToggle />
-      {session ? <p>{session.user.name}</p> : <Button>Sign in</Button>}
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Sign out</Button>
+        </div>
+      ) : (
+        <Button>Sign in</Button>
+      )}
     </div>
   );
 }
