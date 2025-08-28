f8ff366b348fd70d2564bc41d7133084e11fb8f0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-28 22:29:10 +0800

    - authclient for otp

--- lms-course-platform/lib/auth-client.ts
+++ lms-course-platform/lib/auth-client.ts
@@ -1,3 +1,3 @@
 import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient({});
+import { emailOTPClient } from "better-auth/client/plugins";
+export const authClient = createAuthClient({ plugins: [emailOTPClient()] });
