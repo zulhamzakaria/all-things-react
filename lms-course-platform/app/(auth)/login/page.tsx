b411895ac2f65e1377b8a6e78a1aac295084e146
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-28 18:58:16 +0800

    - checker for signed-in user. - gets redirected to the hompage

--- lms-course-platform/app/(auth)/login/page.tsx
+++ lms-course-platform/app/(auth)/login/page.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { auth } from "@/lib/auth";
 import LoginForm from "./_components/LoginForm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    return redirect("");
+  }
+
   return <LoginForm />;
 }
