0bc2d5340268a7c96679c09d00a170bebb4124c4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-08-29 11:36:08 +0800

    - arcjet rules

---
+++ lms-course-platform/lib/arcjet.ts
@@ -0,0 +1,29 @@
+import arcjet, {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+} from "@arcjet/next";
+import { env } from "./env";
+
+export {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+};
+
+export default arcjet({
+  key: env.ARCJET_KEY,
+  characteristics: ["fingerprint"],
+  // empty = no rules
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+  ],
+});
