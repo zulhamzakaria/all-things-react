061e68800ba26b08e63d7a732abd977697165021
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-01 23:22:04 +0800

    - too much

--- lms-course-platform/app/api/auth/[...all]/route.ts
+++ lms-course-platform/app/api/auth/[...all]/route.ts
@@ -1,6 +1,8 @@
+import arcjet from "@/lib/arcjet";
+import { auth } from "@/lib/auth";
 import { env } from "@/lib/env";
 import ip from "@arcjet/ip";
-import arcjet, {
+import {
   type ArcjetDecision,
   type BotOptions,
   type EmailOptions,
@@ -29,3 +31,67 @@ const ratelimitOptions = {
   interval: "2m",
   max: 5,
 } satisfies SlidingWindowRateLimitOptions<[]>;
+
+const signupOptions = {
+  email: emailOptions,
+  bots: botOptions,
+  rateLimit: ratelimitOptions,
+} satisfies ProtectSignupOptions<[]>;
+
+async function protect(req: NextRequest): Promise<ArcjetDecision> {
+  const session = await auth.api.getSession({
+    headers: req.headers,
+  });
+  let userId: string;
+  if (session?.user.id) {
+    userId = session.user.id;
+  } else {
+    userId = ip(req) || "127.0.0.1";
+  }
+  if (req.nextUrl.pathname.startsWith("/api/auth/sign-up")) {
+    const body = await req.clone().json();
+    if (typeof body.email === "string") {
+      return arcjet
+        .withRule(protectSignup(signupOptions))
+        .protect(req, { email: body.email, fingerprint: userId });
+    } else {
+      return arcjet
+        .withRule(detectBot(botOptions))
+        .withRule(slidingWindow(ratelimitOptions))
+        .protect(req, { fingerprint: userId });
+    }
+  } else {
+    return arcjet
+      .withRule(detectBot(botOptions))
+      .protect(req, { fingerprint: userId });
+  }
+}
+
+const authHandlers = toNextJsHandler(auth.handler);
+
+export const { GET } = authHandlers;
+
+export const POST = async (req: NextRequest) => {
+  const decision = await protect(req);
+  console.log("ArcJet Decision", decision);
+  if (decision.isDenied()) {
+    if (decision.reason.isRateLimit()) {
+      return new Response(null, { status: 429 });
+    } else if (decision.reason.isEmail()) {
+      let message: string;
+      if (decision.reason.emailTypes.includes("INVALID")) {
+        message = "Email address format is invalid. Check for typo";
+      } else if (decision.reason.emailTypes.includes("DISPOSABLE")) {
+        message = "Disposable email is not allowed";
+      } else if (decision.reason.emailTypes.includes("NO_MX_RECORDS")) {
+        message = "Email domain does not have an MX record. Check for typo";
+      } else {
+        message = "Invalid email";
+      }
+      return Response.json({ message }, { status: 400 });
+    } else {
+      return new Response(null, { status: 403 });
+    }
+  }
+  return authHandlers.POST(req);
+};
