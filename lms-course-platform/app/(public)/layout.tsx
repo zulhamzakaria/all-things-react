9ca152603e7228cb5fa5d1f239d95361b567ca58
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-02 22:56:59 +0800

    - basic Navbar component - import Navbar component from Home page

--- lms-course-platform/app/(public)/layout.tsx
+++ lms-course-platform/app/(public)/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "./_components/Navbar";
+
 export default function LayoutPublic({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function LayoutPublic({
 }) {
   return (
     <div>
-      <h1>Navbar</h1>
+      <Navbar />
       <main className="container mx-auto px-4 md:px-6 lg:px-8">{children}</main>
     </div>
   );
