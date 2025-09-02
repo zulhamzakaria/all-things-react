c79423656f9d2c0e33d947c0f31a4a6f3ec3ee05
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-02 18:50:38 +0800

    - WIP

--- lms-course-platform/app/(public)/page.tsx
+++ lms-course-platform/app/(public)/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { Badge } from "@/components/ui/badge";
 import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
@@ -19,12 +21,20 @@ export default function Home() {
   return (
     <>
       <section className="relative py-20">
-        <div>
-          <Badge>The Future of Online Learning</Badge>
+        <div className="flex flex-col items-center text-center space-y-8">
+          <Badge variant={"outline"}>The Future of Online Learning</Badge>
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            Elevate your learning experience
+          </h1>
+          <p className="text-muted-foreground max-w-[700px] md:text-xl">
+            Discover a new way of learning with our state-of-the-art learning
+            management system. Access high-quality courses anytime, anywhere
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-8">
+            <Link href="/courses">Explore courses</Link>
+          </div>
         </div>
       </section>
     </>
   );
 }
-
-("use client");
