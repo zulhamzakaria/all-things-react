d71ab8d058462db71ea8b856729f159084563f8a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-04 17:40:14 +0800

    - render children and moved dashboard components to admin index page

--- lms-course-platform/app/admin/page.tsx
+++ lms-course-platform/app/admin/page.tsx
@@ -1,7 +1,16 @@
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import data from "./data.json";
+
 export default function AdminIndexPage() {
   return (
-    <div>
-      <h1>hi from the admin index page</h1>
-    </div>
+    <>
+      <SectionCards />
+      <div className="px-4 lg:px-6">
+        <ChartAreaInteractive />
+      </div>
+      <DataTable data={data} />
+    </>
   );
 }
