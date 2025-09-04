217711507bc537008fc773c2fada477251310430
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-04 17:53:19 +0800

    - stylings

--- lms-course-platform/app/admin/courses/page.tsx
+++ lms-course-platform/app/admin/courses/page.tsx
@@ -1,7 +1,19 @@
-export default function Courses() {
+import { buttonVariants } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function CoursesPage() {
   return (
-    <div>
-      <h1>hi from courses</h1>
-    </div>
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Your Courses</h1>
+        <Link href={"/admin/courses/create"} className={buttonVariants()}>
+          <PlusIcon /> <p>Create Course</p>
+        </Link>
+      </div>
+      <div>
+        <h1></h1>
+      </div>
+    </>
   );
 }
