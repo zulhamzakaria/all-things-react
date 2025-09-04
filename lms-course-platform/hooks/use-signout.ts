bc8531c588dce5f1f2896cd3261fb7f4142ebc15
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-04 17:04:18 +0800

    - custon hook for signout

---
+++ lms-course-platform/hooks/use-signout.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export function useSignOut() {
+  const router = useRouter();
+  const handleSignout = async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+          toast.success("Signed out successfully");
+        },
+        onError: () => {
+          toast.error("Failed to sign out");
+        },
+      },
+    });
+  };
+  return handleSignout;
+}
