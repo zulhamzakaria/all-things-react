bbb7181baff77402be93b063e3a445dfa7fba967
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-04 17:44:26 +0800

    -stylings

--- lms-course-platform/app/admin/layout.tsx
+++ lms-course-platform/app/admin/layout.tsx
@@ -18,7 +18,7 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
         <SiteHeader />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
               {children}
             </div>
           </div>
