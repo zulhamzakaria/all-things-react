aaaf7249d5d2ef03225697fc3e7d7e0d717868df
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-05 14:52:18 +0800

    - wip

--- lms-course-platform/lib/zod-schemas.ts
+++ lms-course-platform/lib/zod-schemas.ts
@@ -2,4 +2,7 @@ import { z } from "zod";
 
 export const courseSchema = z.object({
   title: z.string().min(3).max(100),
+  description: z.string().min(3).max(2500),
+  fileKey: z.string().min(3).max(1),
+  price: z.coerce.number().min(1),
 });
