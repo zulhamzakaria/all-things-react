76be64c2f639508a4abeee580771417ede18544c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-05 12:12:07 +0800

    - stylings (WIP)

--- lms-course-platform/app/admin/courses/create/page.tsx
+++ lms-course-platform/app/admin/courses/create/page.tsx
@@ -1,7 +1,22 @@
-export default function CreateCoursePage(){
-    return <div>
-        <h1>
-            hi from course creation page
-        </h1>
-    </div>
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function CreateCoursePage() {
+  return (
+    <>
+      <div className="flex items-center gap-4">
+        <Link
+          href={"/admin/courses"}
+          className={buttonVariants({
+            variant: "outline",
+            size: "icon",
+          })}
+        >
+          <ArrowLeft className="size-4" />
+        </Link>
+        <h1 className="text-2xl font-bold">Create Courses</h1>
+      </div>
+    </>
+  );
 }
