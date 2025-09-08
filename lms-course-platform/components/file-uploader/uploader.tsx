fdc049a02310eeb547a201d1d4867af65c808906
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-08 21:48:35 +0800

    - validations for file upload

--- lms-course-platform/components/file-uploader/uploader.tsx
+++ lms-course-platform/components/file-uploader/uploader.tsx
@@ -1,16 +1,41 @@
 "use client";
 
 import { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+import { FileRejection, useDropzone } from "react-dropzone";
 import { Card, CardContent } from "../ui/card";
 import { cn } from "@/lib/utils";
 import { RenderEmptyState, RenderErrorState } from "./render-state";
+import { toast } from "sonner";
 
 export function Uploader() {
+  function rejectedFiles(fileRejection: FileRejection[]) {
+    if (fileRejection.length) {
+      const tooManyFiles = fileRejection.find((rejection) => {
+        rejection.errors[0].code === "too-many-files";
+      });
+      if (tooManyFiles) {
+        toast.error("Too many file being uploaded");
+      }
+      const largeSizeFiles = fileRejection.find(
+        (rejection) => rejection.errors[0].code === "file-too-large"
+      );
+      if (largeSizeFiles) {
+        toast.error("File size exceeded the limit (5mb)");
+      }
+    }
+  }
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
     // Do something with the files
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: { "image/*": [] },
+    maxFiles: 1,
+    multiple: false,
+    maxSize: 5 * 1024 * 1024, //5mb
+    onDropRejected: rejectedFiles,
+  });
   return (
     <Card
       {...getRootProps()}
