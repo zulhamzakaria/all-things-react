0eeae716f2a1fa92cf41a0acad6291c57083b20a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-08 17:52:32 +0800

    - empty state styles

---
+++ lms-course-platform/components/file-uploader/render-state.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import { CloudUploadIcon } from "lucide-react";
+
+export function RenderEmptyState({ isDragActive }: { isDragActive: boolean }) {
+  return (
+    <div className="text-center">
+      <div className="flex items-center mx-auto justify-center size-12 rounded-full bg-muted mb-4">
+        <CloudUploadIcon
+          className={cn(
+            "size-6 text-muted-foreground ",
+            isDragActive && "text-primary"
+          )}
+        />
+      </div>
+    </div>
+  );
+}
