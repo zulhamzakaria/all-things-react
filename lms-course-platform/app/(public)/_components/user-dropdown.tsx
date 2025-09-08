af4d7afe6367b14e74657193722cbc11cd22d69c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-08 16:28:18 +0800

    - stylings

--- lms-course-platform/app/(public)/_components/user-dropdown.tsx
+++ lms-course-platform/app/(public)/_components/user-dropdown.tsx
@@ -59,7 +59,7 @@ export default function UserDropdown({ name, email, image }: iAppProps) {
           />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="max-w-64">
+      <DropdownMenuContent align="end" className="min-w-48">
         <DropdownMenuLabel className="flex min-w-0 flex-col">
           <span className="text-foreground truncate text-sm font-medium">
             {name}
