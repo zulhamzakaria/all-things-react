708734946cfea28425dc052223a6455388762fd5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-08 12:03:34 +0800

    - tiptap textalign configuration

--- lms-course-platform/components/rich-text-editor/editor.tsx
+++ lms-course-platform/components/rich-text-editor/editor.tsx
@@ -3,10 +3,14 @@
 import { useEditor } from "@tiptap/react";
 import { StarterKit } from "@tiptap/starter-kit";
 import { Menubar } from "./menubar";
+import TextAlign from "@tiptap/extension-text-align";
 
 export function RichTextEditor() {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      TextAlign.configure({ types: ["heading", "paragraph"] }),
+    ],
     immediatelyRender: false,
   });
   return (
