e4cdc24a5f0ee20ae4055c3c21848608fe885286
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-08 16:01:01 +0800

    - code for converting the rich text editor value to string

--- lms-course-platform/components/rich-text-editor/editor.tsx
+++ lms-course-platform/components/rich-text-editor/editor.tsx
@@ -5,7 +5,7 @@ import { StarterKit } from "@tiptap/starter-kit";
 import { Menubar } from "./menubar";
 import TextAlign from "@tiptap/extension-text-align";
 
-export function RichTextEditor() {
+export function RichTextEditor({ field }: { field: any }) {
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -18,6 +18,10 @@ export function RichTextEditor() {
           "min-h-[300px] p-4 focus:outline-none prose prose-sm sm:prose lg:prose-lg xl:prose-x dark:prose-invert !w-full !max-w-none",
       },
     },
+    onUpdate: ({ editor }) => {
+      field.onChange(JSON.stringify(editor.getJSON()));
+    },
+    content: field.value ? JSON.parse(field.value) : "<p>Hello</p>",
   });
   return (
     <div className="border w-full border-input rounded-lg overflow-hidden dark:bg-input/30">
