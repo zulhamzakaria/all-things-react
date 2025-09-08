36a4d220588f7191d99aad96f7bb4e3fb9856612
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-08 17:33:47 +0800

    - boilerplate code

--- lms-course-platform/components/file-uploader/uploader.tsx
+++ lms-course-platform/components/file-uploader/uploader.tsx
@@ -1,5 +1,21 @@
 "use client";
 
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
 export function Uploader() {
-  return <div>hi from uploader component</div>;
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag and drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 }
