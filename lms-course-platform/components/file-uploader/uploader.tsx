a547ac1889080f41a1da9105de47ede063c59d5e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-08 18:10:18 +0800

    - WIP

--- lms-course-platform/components/file-uploader/uploader.tsx
+++ lms-course-platform/components/file-uploader/uploader.tsx
@@ -24,7 +24,7 @@ export function Uploader() {
       <CardContent className="flex items-center justify-center h-full w-full p-4">
         <input {...getInputProps()} />
         <RenderEmptyState isDragActive={isDragActive} />
-        <RenderErrorState />
+        {/* <RenderErrorState /> */}
       </CardContent>
     </Card>
   );
