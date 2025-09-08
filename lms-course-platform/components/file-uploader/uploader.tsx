c3b2b26cb664dd564e69deca66f6a39562fe810c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-08 17:42:23 +0800

    - dnd file uppload (WIP)

--- lms-course-platform/components/file-uploader/uploader.tsx
+++ lms-course-platform/components/file-uploader/uploader.tsx
@@ -2,6 +2,8 @@
 
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import { Card, CardContent } from "../ui/card";
+import { cn } from "@/lib/utils";
 
 export function Uploader() {
   const onDrop = useCallback((acceptedFiles: File[]) => {
@@ -9,13 +11,19 @@ export function Uploader() {
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag and drop some files here, or click to select files</p>
+    <Card
+      {...getRootProps()}
+      className={cn(
+        " relative border-2 border-dashed transition-colors duration-200 ease-in-out w-full h-64 ",
+        isDragActive
+          ? "border-primary bg-primary/10 border-solid"
+          : "border-border hover:border-primary"
       )}
-    </div>
+    >
+      <CardContent className="flex items-center justify-center h-full w-full p-4">
+        <input {...getInputProps()} />
+        <p></p>
+      </CardContent>
+    </Card>
   );
 }
