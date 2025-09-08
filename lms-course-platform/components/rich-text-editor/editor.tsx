62e77c443d956de32adfd014abc94a6e781fb7c3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-08 15:34:27 +0800

    - stylings

--- lms-course-platform/components/rich-text-editor/editor.tsx
+++ lms-course-platform/components/rich-text-editor/editor.tsx
@@ -14,7 +14,7 @@ export function RichTextEditor() {
     immediatelyRender: false,
     editorProps: {
       attributes: {
-        class: "min-h-[300px] p-4  ",
+        class: "min-h-[300px] p-4 focus:outline-none ",
       },
     },
   });
