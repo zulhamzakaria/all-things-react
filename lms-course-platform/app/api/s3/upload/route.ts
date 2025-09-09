3773d7501224e41caa836a2205de1eb675b31c56
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-09 20:02:02 +0800

    - wip

--- lms-course-platform/app/api/s3/upload/route.ts
+++ lms-course-platform/app/api/s3/upload/route.ts
@@ -1,5 +1,23 @@
+import { NextResponse } from "next/server";
+import z from "zod";
+
+export const fileUploadSchema = z.object({
+  fileName: z.string().min(1, { message: "Filename is required" }),
+  contentType: z.string().min(1, { message: "Content type is required" }),
+  size: z.number().min(1, { message: "Size is required" }),
+  isImage: z.boolean(),
+});
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+    const validation = fileUploadSchema.safeParse(body);
+    if (!validation.success) {
+      return NextResponse.json(
+        { error: "Invalid request body" },
+        { status: 400 }
+      );
+    }
+    const { fileName, contentType, size } = validation.data;
   } catch (error) {}
 }
