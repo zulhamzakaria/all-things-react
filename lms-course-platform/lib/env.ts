5a9fa968d17d612f52812714a5a75687d7594ec9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-10 17:56:05 +0800

    - t3 validator for aws bucket variables

--- lms-course-platform/lib/env.ts
+++ lms-course-platform/lib/env.ts
@@ -10,6 +10,11 @@ export const env = createEnv({
     GITHUB_CLIENT_SECRET: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
     ARCJET_KEY: z.string().min(1),
+    AWS_ACCESS_KEY_ID: z.string().min(1),
+    AWS_SECRET_ACCESS_KEY: z.string().min(1),
+    AWS_ENDPOINT_URL_S3: z.string().min(1),
+    AWS_ENDPOINT_URL_IAM: z.string().min(1),
+    AWS_REGION: z.string().min(1),
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {},
