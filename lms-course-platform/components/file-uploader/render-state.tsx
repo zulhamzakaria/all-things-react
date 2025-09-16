a2ed4bfc1e50f0f05f06c4db4a6a4e8693ebdb7c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-16 18:26:30 +0800

    - renderuploadedstate

--- lms-course-platform/components/file-uploader/render-state.tsx
+++ lms-course-platform/components/file-uploader/render-state.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
-import { CloudUploadIcon, Image, ImageIcon } from "lucide-react";
+import { CloudUploadIcon, ImageIcon, XIcon } from "lucide-react";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 export function RenderEmptyState({ isDragActive }: { isDragActive: boolean }) {
   return (
@@ -38,6 +39,26 @@ export function RenderErrorState() {
       <p className="text-muted-foreground text-xs mt-1">Something went wrong</p>
       <Button type="button" className="mt-4">
         Retry Upload
+      </Button>
+    </div>
+  );
+}
+
+export function RenderUploadedState({ previewUrl }: { previewUrl: string }) {
+  return (
+    <div>
+      <Image
+        src={previewUrl}
+        alt="uploaded-file"
+        fill
+        className="object-contain p-2"
+      />
+      <Button
+        variant={"destructive"}
+        size={"icon"}
+        className={cn("absolute top-4 right-4")}
+      >
+        <XIcon className="size-4" />
       </Button>
     </div>
   );
