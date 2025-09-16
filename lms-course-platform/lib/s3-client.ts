aa3c4bf9ce0bf65562c1b20597bff5086c3862a6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-16 19:56:46 +0800

    - missing keys?

--- lms-course-platform/lib/s3-client.ts
+++ lms-course-platform/lib/s3-client.ts
@@ -7,4 +7,8 @@ export const S3 = new S3Client({
   region: "auto",
   endpoint: env.AWS_ENDPOINT_URL_S3,
   forcePathStyle: false,
+  credentials: {
+    accessKeyId: env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+  },
 });
